6138e3e4022fe300b45394dd883a498ed34502bb
Christopher J. White <c.white.astro@gmail.com>
2014-06-10 12:12:13 -0400

    added relativistic shock tube problem generator

---
+++ src/pgen/shock_tube_sr.cpp
@@ -0,0 +1,133 @@
+// Special relativistic shock tube generator
+
+// Primary header
+#include "../fluid.hpp"
+
+// C++ headers
+#include <iostream>   // endl
+#include <cmath>      // sqrt()
+#include <sstream>    // stringstream
+#include <stdexcept>  // runtime_error
+#include <string>     // c_str()
+
+// Athena headers
+#include "../athena.hpp"           // enums, Real
+#include "../athena_arrays.hpp"    // AthenaArray
+#include "../mesh.hpp"             // Block, Domain, Mesh
+#include "../parameter_input.hpp"  // ParameterInput
+
+// Declarations
+static void set_state(AthenaArray<Real> &cons, int i, int j, int k, Real rho, Real pgas,
+    Real vx, Real vy, Real vz, Real gamma_adi, Real gamma_adi_red);
+
+// Function for setting initial conditions
+// Inputs:
+//   pin: parameters
+// Outputs: (none)
+// Notes:
+//   sets conserved variables according to input primitives
+void Fluid::InitProblem(ParameterInput *pin)
+{
+  // Prepare index bounds
+  Block *pb = pparent_block;
+  int is = pb->is;
+  int js = pb->js;
+  int ks = pb->ks;
+  int ie = pb->ie;
+  int je = pb->je;
+  int ke = pb->ke;
+
+  // Read and set ratio of specific heats
+  gamma_ = pin->GetReal("fluid", "gamma");
+  Real gamma_adi = GetGamma();
+  Real gamma_adi_red = gamma_adi / (gamma_adi - 1.0);
+
+  // Read and check shock direction and position
+  int shock_dir = pin->GetInteger("problem", "shock_dir"); 
+  Real shock_pos = pin->GetReal("problem", "xshock"); 
+  Real min_bound, max_bound;
+  std::stringstream msg;
+  switch (shock_dir)
+  {
+    case 1:
+      min_bound = pb->pparent_domain->pparent_mesh->mesh_size.x1min;
+      max_bound = pb->pparent_domain->pparent_mesh->mesh_size.x1max;
+      break;
+    case 2:
+      min_bound = pb->pparent_domain->pparent_mesh->mesh_size.x2min;
+      max_bound = pb->pparent_domain->pparent_mesh->mesh_size.x2max;
+      break;
+    case 3:
+      min_bound = pb->pparent_domain->pparent_mesh->mesh_size.x3min;
+      max_bound = pb->pparent_domain->pparent_mesh->mesh_size.x3max;
+      break;
+    default:
+      msg << "### FATAL ERROR in Problem Generator" << std::endl
+          << "shock_dir=" << shock_dir << " must be either 1, 2, or 3" << std::endl;
+      throw std::runtime_error(msg.str().c_str());
+  }
+  if (shock_pos < min_bound || shock_pos > max_bound)
+  {
+    msg << "### FATAL ERROR in Problem Generator" << std::endl
+        << "xshock=" << shock_pos << " lies outside x" << shock_dir
+        << " domain for shkdir=" << shock_dir << std::endl;
+    throw std::runtime_error(msg.str().c_str());
+  }
+
+  // Read left state
+  Real rho_left = pin->GetReal("problem", "dl");
+  Real pgas_left = pin->GetReal("problem", "pl");
+  Real vx_left = pin->GetReal("problem", "ul");
+  Real vy_left = pin->GetReal("problem", "vl");
+  Real vz_left = pin->GetReal("problem", "wl");
+
+  // Read right state
+  Real rho_right = pin->GetReal("problem", "dr");
+  Real pgas_right = pin->GetReal("problem", "pr");
+  Real vx_right = pin->GetReal("problem", "ur");
+  Real vy_right = pin->GetReal("problem", "vr");
+  Real vz_right = pin->GetReal("problem", "wr");
+
+  // Initialize the discontinuity
+  for (int k = ks; k <= ke; k++)
+    for (int j = js; j <= je; j++)
+      for (int i = is; i <= ie; i++)
+      {
+        bool left_side = false;
+        switch(shock_dir)
+        {
+          case 1:
+            if (pb->x1v(i) < shock_pos)
+              left_side = true;
+            break;
+          case 2:
+            if (pb->x2v(j) < shock_pos)
+              left_side = true;
+            break;
+          case 3:
+            if (pb->x3v(k) < shock_pos)
+              left_side = true;
+        }
+        if (left_side)
+          set_state(u, i, j, k, rho_left, pgas_left, vx_left, vy_left, vz_left,
+              gamma_adi, gamma_adi_red);
+        else
+          set_state(u, i, j, k, rho_right, pgas_right, vx_right, vy_right, vz_right,
+              gamma_adi, gamma_adi_red);
+      }
+  return;
+}
+
+// Function for setting conserved variables in a cell given the primitives
+static void set_state(AthenaArray<Real> &cons, int i, int j, int k, Real rho, Real pgas,
+    Real vx, Real vy, Real vz, Real gamma_adi, Real gamma_adi_red)
+{
+  Real gamma_lor_sq = 1.0 / (1.0 - (vx*vx + vy*vy + vz*vz));
+  Real gamma_lor_sq_rho_h = gamma_lor_sq * (rho + gamma_adi_red * pgas);
+  cons(IDN,k,j,i) = sqrt(gamma_lor_sq) * rho;
+  cons(IEN,k,j,i) = gamma_lor_sq_rho_h - pgas;
+  cons(IM1,k,j,i) = gamma_lor_sq_rho_h * vx;
+  cons(IM2,k,j,i) = gamma_lor_sq_rho_h * vy;
+  cons(IM3,k,j,i) = gamma_lor_sq_rho_h * vz;
+  return;
+}
