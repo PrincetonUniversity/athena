646265fe672b4848c7d0eaf06ecb134be8e8aebf
Christopher J. White <c.white.astro@gmail.com>
2014-06-10 12:27:39 -0400

    added test directory for unit/regression tests; can only be used with Google Test framework

---
+++ test/src/test.hpp
@@ -0,0 +1,16 @@
+// Test suite for Athena++
+
+#ifndef TEST_HPP
+#define TEST_HPP
+
+// Test headers
+#include "gtest/gtest.h"
+
+namespace {
+
+// General test class
+class GeneralTest : public testing::Test {};
+
+}
+
+#endif  // TEST_HPP
