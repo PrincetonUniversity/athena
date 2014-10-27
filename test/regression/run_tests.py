88e8902197c0fa3e65753413c11fa577859e82cc
Christopher J. White <c.white.astro@gmail.com>
2014-10-27 02:42:09 -0400

    removed everything in test directory; replaced with regression test framework; unit tests to be reimplemented later

---
+++ test/regression/run_tests.py
@@ -0,0 +1,60 @@
+# Regression test script
+
+# Modules
+import argparse
+import os
+import pkgutil
+import sys
+
+# Prevent generation of .pyc files
+sys.dont_write_bytecode = True
+
+# Global variables
+import scripts.utils.athena as athena
+
+# Main function
+def main(**kwargs):
+
+  # Save existing files
+  athena.save_files()
+
+  # Make list of tests to run
+  if kwargs['single'] is None:
+    test_names = [name for _,name,_ in pkgutil.iter_modules(['scripts/tests'])]
+  else:
+    test_names = [kwargs['single']]
+
+  # Run tests
+  test_results = []
+  try:
+    for name in test_names:
+      name_full = 'scripts.tests.' + name
+      module = __import__(name_full, globals(), locals(), fromlist=['run_test'])
+      test_results.append(module.run_test())
+
+  # Restore any previously-existing files once runs are complete
+  finally:
+    athena.restore_files()
+
+  # Report test results
+  print('')
+  print('Results:')
+  for name,result in zip(test_names,test_results):
+    result_string = 'passed' if result else 'failed'
+    print('    {0}: {1}'.format(name,result_string))
+  print('')
+  num_tests = len(test_results)
+  num_passed = test_results.count(True)
+  test_string = 'test' if num_tests == 1 else 'tests'
+  print('Summary: {0} out of {1} {2} passed'.format(num_passed,num_tests,test_string))
+  print('')
+
+# Execute main function
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-s', '--single',
+      type=str,
+      default=None,
+      help='name of individual test to run')
+  args = parser.parse_args()
+  main(**vars(args))
---
+++ test/regression/scripts/tests/gr_shock_tube.py
@@ -0,0 +1,34 @@
+# Test script for relativistic shock tube using GR framework
+
+# Modules
+import numpy as np
+import scripts.utils.athena as athena
+import scripts.utils.comparison as comparison
+
+# Primary function
+def run_test():
+  athena.configure('g',
+      prob='shock_tube_gr',
+      coord='minkowski_cartesian')
+  athena.make()
+  parameters = [
+      'job/problem_id=gr_shock_tube',
+      'output1/file_type=tab',
+      'output1/variable=cons',
+      'output1/data_format=%24.16e',
+      'output1/dt=0.4',
+      'time/cfl_number=0.4',
+      'time/tlim=0.4',
+      'mesh/nx1=400']
+  athena.run('hydro_sr/athinput.mb_1', *parameters)
+  headings = ['x', 'D', 'E', 'M1', 'M2', 'M3']
+  data_ref = athena.read('data/gr_shock_tube.tab', headings)
+  data_new = athena.read('bin/gr_shock_tube.out1.0001.tab', headings)
+  faces_ref = np.linspace(-0.5, 0.5, len(data_ref['x'])+1)
+  faces_new = np.linspace(-0.5, 0.5, len(data_new['x'])+1)
+  eps_d = comparison.l1_diff(faces_ref, data_ref['D'], faces_new, data_new['D'])
+  eps_d /= comparison.l1_norm(faces_ref, data_ref['D'])
+  if eps_d < 0.02:
+    return True
+  else:
+    return False
