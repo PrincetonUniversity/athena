309735a6ed58b347f17a9e97e74825dc02cee6c4
jim stone <jmstone@princeton.edu>
2015-12-17 07:21:28 -0500

    Updated all gr/sr input files to use strings to set boundary flags.  All regression tests pass except Newtonian linear wave convergence.  This needs to be tracked down.

--- src/bvals/get_boundary_flag.cpp
+++ src/bvals/get_boundary_flag.cpp
@@ -41,6 +41,8 @@ enum BoundaryFlag GetBoundaryFlag(std::string input_string)
     return PERIODIC_BNDRY;
   } else if (input_string == "polar") {
     return POLAR_BNDRY;
+  } else if (input_string == "none") {
+    return BNDRY_UNDEF;
   } else {
     std::stringstream msg;
     msg << "### FATAL ERROR in GetBoundaryFlag" << std::endl
