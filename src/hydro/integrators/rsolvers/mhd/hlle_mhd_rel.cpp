5fa005e3a58ea38c352435582859d325a4a29dbb
Christopher J. White <c.white.astro@gmail.com>
2015-12-31 23:52:57 -0500

    fixed missed name change

--- src/hydro/integrators/rsolvers/mhd/hlle_mhd_rel.cpp
+++ src/hydro/integrators/rsolvers/mhd/hlle_mhd_rel.cpp
@@ -309,7 +309,7 @@ static void NonTransformingHLLE(const int k, const int j, const int i,
   int ivz = IVX + ((ivx-IVX)+2)%3;
 
   // Extract ratio of specific heats
-  const Real gamma_adi = pmy_hydro->pf_eos->GetGamma();
+  const Real gamma_adi = pmy_hydro->peos->GetGamma();
 
   // Extract metric
   const Real
@@ -409,13 +409,13 @@ static void NonTransformingHLLE(const int k, const int j, const int i,
   // Calculate wavespeeds in left state
   Real lambda_p_l, lambda_m_l;
   Real wgas_l = rho_l + gamma_adi/(gamma_adi-1.0) * pgas_l;
-  pmy_hydro->pf_eos->FastMagnetosonicSpeedsGR(wgas_l, pgas_l, ucon_l[0], ucon_l[ivx],
+  pmy_hydro->peos->FastMagnetosonicSpeedsGR(wgas_l, pgas_l, ucon_l[0], ucon_l[ivx],
       b_sq_l, g00, g02, g22, &lambda_p_l, &lambda_m_l);
 
   // Calculate wavespeeds in right state
   Real lambda_p_r, lambda_m_r;
   Real wgas_r = rho_r + gamma_adi/(gamma_adi-1.0) * pgas_r;
-  pmy_hydro->pf_eos->FastMagnetosonicSpeedsGR(wgas_r, pgas_r, ucon_r[0], ucon_r[ivx],
+  pmy_hydro->peos->FastMagnetosonicSpeedsGR(wgas_r, pgas_r, ucon_r[0], ucon_r[ivx],
       b_sq_r, g00, g02, g22, &lambda_p_r, &lambda_m_r);
 
   // Calculate extremal wavespeeds
