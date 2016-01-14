7753ac9c1390014d9208d9d72a1ade86c667e138
Christopher J. White <c.white.astro@gmail.com>
2016-01-14 15:27:15 -0500

    fixed geodesic_infall problem to be in line with new boundary function interface

--- src/pgen/geodesic_infall.cpp
+++ src/pgen/geodesic_infall.cpp
@@ -18,10 +18,8 @@
 #include "../field/field.hpp"              // Field
 
 // Declarations
-void OutflowInnerHydro(MeshBlock *pmb, AthenaArray<Real> &prim, FaceField &bb, 
-    int is, int ie, int js, int je, int ks, int ke);
-void FixedOuter(MeshBlock *pmb, AthenaArray<Real> &prim, FaceField &bb, 
-    int is, int ie, int js, int je, int ks, int ke);
+void FixedOuter(MeshBlock *pmb, AthenaArray<Real> &prim, FaceField &bb, int is, int ie,
+    int js, int je, int ks, int ke);
 
 // Function for setting initial conditions
 // Inputs:
@@ -74,36 +72,16 @@ void Mesh::ProblemGenerator(Hydro *phyd, Field *pfld, ParameterInput *pin)
   gi.NewAthenaArray(NMETRIC, iu+1);
   for (int j = jl; j <= ju; j++)
   {
-    pmb->pcoord->CellMetric(kl, j, il, iu, g, gi);
     for (int i = il; i <= iu; i++)
     {
       // Get Boyer-Lindquist coordinates of cell
       Real r, theta, phi;
       pmb->pcoord->GetBoyerLindquistCoordinates(pmb->pcoord->x1v(i),
           pmb->pcoord->x2v(j), pmb->pcoord->x3v(kl), &r, &theta, &phi);
-      /*Real r2 = SQR(r);
-      Real sin = std::sin(theta);
-      Real sin2 = SQR(sin);
-      Real cos2 = 1.0 - sin2;
-      Real sigma = r2 + a2 * cos2;
-      Real delta = r2 - 2.0*m*r + a2;
-      Real pp = e * (r2 + a2) - lz * a;
-      Real kk = SQR(a * e * sin - lz / sin) + a2 * cos2;
-      Real rr = std::sqrt(SQR(pp) - delta * (r2 + kk));*/
 
       // Calculate primitives depending on location
       Real rho = rho_min * std::pow(r, rho_pow);
       Real pgas = (gamma_adi-1.0) * u_min * std::pow(r, u_pow);
-      /*Real u0_bl = 1.0/sigma * (-a * (a * e * sin2 - lz) + (r2+a2)/delta * (rr - pp));
-      Real u1_bl = rr/sigma;
-      Real u2_bl = 0.0;
-      Real u3_bl = 1.0/sigma * (-(a * e - lz / sin2) + a/delta * (rr - pp));
-      Real u0, u1, u2, u3;
-      pmb->pcoord->TransformVectorCell(u0_bl, u1_bl, u2_bl, u3_bl, pmb->ks, j, i,
-          &u0, &u1, &u2, &u3);
-      Real v1 = u1 / u0;
-      Real v2 = u2 / u0;
-      Real v3 = u3 / u0;*/
       Real uu1 = 0.0;
       Real uu2 = 0.0;
       Real uu3 = 0.0;
@@ -119,110 +97,30 @@ void Mesh::ProblemGenerator(Hydro *phyd, Field *pfld, ParameterInput *pin)
       }
     }
   }
-  g.DeleteAthenaArray();
-  gi.DeleteAthenaArray();
 
   // Initialize conserved values
   AthenaArray<Real> bb;
   bb.NewAthenaArray(3, ku+1, ju+1, iu+1);
-  pmb->phydro->peos->PrimitiveToConserved(phyd->w, bb, phyd->u, pmb->pcoord,
-                                          il, iu, jl, ju, kl, ku);
+  pmb->phydro->peos->PrimitiveToConserved(phyd->w, bb, phyd->u, pmb->pcoord, il, iu, jl,
+      ju, kl, ku);
   bb.DeleteAthenaArray();
 
-  // Enroll boundary functions
-  pmb->pbval->EnrollUserBoundaryFunction(INNER_X1, OutflowInnerHydro);
-  pmb->pbval->EnrollUserBoundaryFunction(OUTER_X1, FixedOuterHydro);
-  return;
-}
-
-// Inner boundary condition
-// Inputs:
-//   pmb: pointer to block
-// Outputs:
-//   prim: primitive quantities set along inner x1-boundary
-// Notes:
-//   TODO: remove prim hack
-//   TODO: note hack is wrong (assumes wrong primitives)
-void OutflowInnerHydro(MeshBlock *pmb, AthenaArray<Real> &prim, FaceField &bb,
-    int is, int ie, int js, int je, int ks, int ke)
-{
-  int il = is - NGHOST;
-  int iu = is;
-  int jl = js;
-  int ju = je;
-  int kl = ks;
-  int ku = ke;
-  AthenaArray<Real> *pprim;
-  if (&cons == &pmb->phydro->u)
-    pprim = &pmb->phydro->w;
-  else if (&cons == &pmb->phydro->u1)
-    pprim = &pmb->phydro->w1;
-  else
-    assert(0);
-  AthenaArray<Real> g, gi;
-  g.NewAthenaArray(NMETRIC,iu+1);
-  gi.NewAthenaArray(NMETRIC,iu+1);
-  for (int k = kl; k <= ku; ++k)
-    for (int j = jl; j <= ju; ++j)
-    {
-      pco->CellMetric(k, j, il, iu, g, gi);
-      Real alpha = std::sqrt(-1.0/gi(I00,is));
-      Real v1 = (*pprim)(IVX,k,j,is);
-      Real v2 = (*pprim)(IVY,k,j,is);
-      Real v3 = (*pprim)(IVZ,k,j,is);
-      Real tmp = g(I00,is) + 2.0 * (g(I01,is)*v1 + g(I02,is)*v2 + g(I03,is)*v3)
-          + g(I11,is)*v1*v1 + 2.0*g(I12,is)*v1*v2 + 2.0*g(I13,is)*v1*v3
-          + g(I22,is)*v2*v2 + 2.0*g(I23,is)*v2*v3
-          + g(I33,is)*v3*v3;
-      Real u0 = std::sqrt(-1.0/tmp);
-      Real u1 = u0 * v1;
-      Real u2 = u0 * v2;
-      Real u3 = u0 * v3;
-      Real gamma = alpha * u0;
-      Real utilde1 = u1 + alpha * gamma * gi(I01,is);
-      Real utilde2 = u2 + alpha * gamma * gi(I02,is);
-      Real utilde3 = u3 + alpha * gamma * gi(I03,is);
-      for (int i = il; i <= iu; ++i)
-      {
-        Real &rho = (*pprim)(IDN,k,j,i);
-        Real &pgas = (*pprim)(IEN,k,j,i);
-        rho = (*pprim)(IDN,k,j,is);
-        pgas = (*pprim)(IEN,k,j,is);
-        Real alpha_new = std::sqrt(-1.0/gi(I00,i));
-        Real u0_new = gamma / alpha_new;
-        Real u1_new = utilde1 - alpha_new * gamma * gi(I01,i);
-        Real u2_new = utilde2 - alpha_new * gamma * gi(I02,i);
-        Real u3_new = utilde3 - alpha_new * gamma * gi(I03,i);
-        (*pprim)(IVX,k,j,i) = u1_new / u0_new;
-        (*pprim)(IVY,k,j,i) = u2_new / u0_new;
-        (*pprim)(IVZ,k,j,i) = u3_new / u0_new;
-        Real u_0_new, u_1_new, u_2_new, u_3_new;
-        pco->LowerVectorCell(u0_new, u1_new, u2_new, u3_new, k, j, i, &u_0_new,
-            &u_1_new, &u_2_new, &u_3_new);
-        Real gamma_adi = pmb->phydro->peos->GetGamma();
-        Real gamma_prime = gamma_adi/(gamma_adi-1.0);
-        Real wgas = rho + gamma_prime * pgas;
-        cons(IDN,k,j,i) = rho * u0_new;
-        cons(IEN,k,j,i) = wgas * u0_new * u_0_new + pgas;
-        cons(IM1,k,j,i) = wgas * u0_new * u_1_new;
-        cons(IM2,k,j,i) = wgas * u0_new * u_2_new;
-        cons(IM3,k,j,i) = wgas * u0_new * u_3_new;
-      }
-    }
-  g.DeleteAthenaArray();
-  gi.DeleteAthenaArray();
+  // Enroll boundary function
+  pmb->pbval->EnrollUserBoundaryFunction(OUTER_X1, FixedOuter);
   return;
 }
 
 // Outer boundary condition
 // Inputs:
 //   pmb: pointer to block
+//   is,ie,js,je,ks,ke: index boundaries of active zone
 // Outputs:
 //   prim: primitive quantities set along outer x1-boundary
+//   bb: magnetic fields set along outer x1-boundary
 // Notes:
 //   remains unchanged
-void FixedOuter(MeshBlock *pmb, AthenaArray<Real> &prim, FaceField &bb,
-    int is, int ie, int js, int je, int ks, int ke)
+void FixedOuter(MeshBlock *pmb, AthenaArray<Real> &prim, FaceField &bb, int is, int ie,
+    int js, int je, int ks, int ke)
 {
   return;
 }
