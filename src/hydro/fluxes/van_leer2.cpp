0f7c2c8834ab7007037b8e76035ee2e8d6f6acbe
James Stone <jmstone@princeton.edu>
2016-05-26 10:10:14 -0400

    van_leer2 function now only contains flux-divergence function.  Also re-wrote for clarity and speed.

--- src/hydro/fluxes/van_leer2.cpp
+++ src/hydro/fluxes/van_leer2.cpp
@@ -65,68 +65,57 @@ void HydroFluxes::FluxDivergence(MeshBlock *pmb,AthenaArray<Real> &u,
 #ifdef OPENMP_PARALLEL
   tid=omp_get_thread_num();
 #endif
-  AthenaArray<Real> x1area, x2area, x2area_p1, x3area, x3area_p1, vol;
+  AthenaArray<Real> x1area, x2area, x2area_p1, x3area, x3area_p1, vol, dflx;
   x1area.InitWithShallowSlice(x1face_area_,2,tid,1);
   x2area.InitWithShallowSlice(x2face_area_,2,tid,1);
   x2area_p1.InitWithShallowSlice(x2face_area_p1_,2,tid,1);
   x3area.InitWithShallowSlice(x3face_area_,2,tid,1);
   x3area_p1.InitWithShallowSlice(x3face_area_p1_,2,tid,1);
   vol.InitWithShallowSlice(cell_volume_,2,tid,1);
+  dflx.InitWithShallowSlice(flx_,2,tid,1);
 
-  // update conserved hydro variables
-  if (pmb->block_size.nx3 > 1) {
 #pragma omp for schedule(static)
-    for (int k=ks; k<=ke; ++k) { 
-      for (int j=js; j<=je; ++j) {
-        pmb->pcoord->CellVolume(k,j,is,ie,vol);
-        pmb->pcoord->Face1Area(k,j,is,ie+1,x1area);
+  for (int k=ks; k<=ke; ++k) { 
+    for (int j=js; j<=je; ++j) {
+
+      // calculate x1-flux divergence
+      pmb->pcoord->Face1Area(k,j,is,ie+1,x1area);
+      for (int n=0; n<NHYDRO; ++n) {
+        for (int i=is; i<=ie; ++i) {
+          dflx(n,i) = (x1area(i+1) *x1flux(n,k,j,i+1) - x1area(i)*x1flux(n,k,j,i));
+        }
+      }
+
+      // calculate x2-flux divergence
+      if (pmb->block_size.nx2 > 1) {
         pmb->pcoord->Face2Area(k,j  ,is,ie,x2area   );
         pmb->pcoord->Face2Area(k,j+1,is,ie,x2area_p1);
-        pmb->pcoord->Face3Area(k  ,j,is,ie,x3area   );
-        pmb->pcoord->Face3Area(k+1,j,is,ie,x3area_p1);
         for (int n=0; n<NHYDRO; ++n) {
-//#pragma simd
+#pragma simd
           for (int i=is; i<=ie; ++i) {
-            u(n,k,j,i) -= dt*(x1area(i+1) *x1flux(n,k,j,i+1) 
-                            - x1area(i)   *x1flux(n,k,j,i)
-                            + x2area_p1(i)*x2flux(n,k,j+1,i)
-                            - x2area(i)   *x2flux(n,k,j,i)
-                            + x3area_p1(i)*x3flux(n,k+1,j,i)
-                            - x3area(i)   *x3flux(n,k,j,i))/vol(i);
+            dflx(n,i) += (x2area_p1(i)*x2flux(n,k,j+1,i) - x2area(i)*x2flux(n,k,j,i));
           }
         }
       }
-    }
-  }
-  else if (pmb->block_size.nx2 > 1) {
-    int k=pmb->ks;
-#pragma omp for schedule(static)
-    for (int j=js; j<=je; ++j) {
-      pmb->pcoord->CellVolume(k,j,is,ie,vol);
-      pmb->pcoord->Face1Area(k,j,is,ie+1,x1area);
-      pmb->pcoord->Face2Area(k,j  ,is,ie,x2area   );
-      pmb->pcoord->Face2Area(k,j+1,is,ie,x2area_p1);
-      for (int n=0; n<NHYDRO; ++n) {
+
+      // calculate x3-flux divergence
+      if (pmb->block_size.nx3 > 1) {
+        pmb->pcoord->Face3Area(k  ,j,is,ie,x3area   );
+        pmb->pcoord->Face3Area(k+1,j,is,ie,x3area_p1);
+        for (int n=0; n<NHYDRO; ++n) {
 #pragma simd
-        for (int i=is; i<=ie; ++i) {
-          u(n,k,j,i) -= dt*(x1area(i+1) *x1flux(n,k,j,i+1) 
-                          - x1area(i)   *x1flux(n,k,j,i)
-                          + x2area_p1(i)*x2flux(n,k,j+1,i)
-                          - x2area(i)   *x2flux(n,k,j,i))/vol(i);
+          for (int i=is; i<=ie; ++i) {
+            dflx(n,i) += (x3area_p1(i)*x3flux(n,k+1,j,i) - x3area(i)*x3flux(n,k,j,i));
+          }
         }
       }
-    }
-  }
-  else {
-    int j=pmb->js, k=pmb->ks;
-    pmb->pcoord->CellVolume(k,j,is,ie,vol);
-    pmb->pcoord->Face1Area(k,j,is,ie+1,x1area);
-#pragma omp for schedule(static)
-    for (int n=0; n<NHYDRO; ++n) {
-#pragma simd
-      for (int i=is; i<=ie; ++i) {
-        u(n,k,j,i) -= dt*(x1area(i+1)*x1flux(n,k,j,i+1) 
-                        - x1area(i)*  x1flux(n,k,j,i))/vol(i);
+
+      // update conserved variables
+      pmb->pcoord->CellVolume(k,j,is,ie,vol);
+      for (int n=0; n<NHYDRO; ++n) {
+        for (int i=is; i<=ie; ++i) {
+          u(n,k,j,i) -= dt*dflx(n,i)/vol(i);
+        }
       }
     }
   }
