63855762859026cb62f039e014ee87a3e36a3f1b
James Stone <jmstone@princeton.edu>
2016-08-15 17:05:16 -0400

    Updated linear wave convergence regressions test to check maximum error in each variable, scaled to the RMS error in that variable.  This error criterion is more sensitive for finding bugs at AMR block boundaries.  For example, it is possible that the error in a small number of grid zones at fine/coarse boundaries is fixed, but the total RMS error is still converging (because the error at the block boundaries is a small contribution to the total).

--- tst/regression/scripts/tests/mhd/mhd_linwave.py
+++ tst/regression/scripts/tests/mhd/mhd_linwave.py
@@ -10,7 +10,6 @@ import math
 import sys
 import scripts.utils.athena as athena
 import scripts.utils.comparison as comparison
-sys.path.insert(0, '../../vis/python')
 
 # Prepare Athena++
 def prepare():
@@ -63,30 +62,44 @@ def analyze():
         continue
       data.append([float(val) for val in line.split()])
 
-  # check absolute error and convergence of all three waves
+  # check largest maximum error scaled to RMS is within bounds at each highest res
+  if data[1][13] > 6.0:
+    print "maximum error in L-going fast wave too large",data[1][13]
+    return False
+  if data[3][13] > 6.0:
+    print "maximum error in L-going Alfven wave too large",data[3][13]
+    return False
+  if data[5][13] > 6.0:
+    print "maximum error in L-going slow wave too large",data[5][13]
+    return False
+  if data[7][13] > 6.0:
+    print "maximum error in L-going entropy wave too large",data[7][13]
+    return False
+
+  # check RMS error and convergence of all three waves
   if data[1][4] > 4.0e-8:
-    print "error in L-going fast wave too large",data[1][4]
+    print "RMS error in L-going fast wave too large",data[1][4]
     return False
   if data[1][4]/data[0][4] > 0.4:
     print "not converging for L-going fast wave",data[0][4],data[1][4]
     return False
 
   if data[3][4] > 4.0e-8:
-    print "error in L-going Alfven wave too large",data[3][4]
+    print "RMS error in L-going Alfven wave too large",data[3][4]
     return False
   if data[3][4]/data[2][4] > 0.4:
     print "not converging for L-going Alfven wave",data[2][4],data[3][4]
     return False
 
   if data[5][4] > 5.0e-8:
-    print "error in L-going slow wave too large",data[5][4]
+    print "RMS error in L-going slow wave too large",data[5][4]
     return False
   if data[5][4]/data[4][4] > 0.4:
     print "not converging for L-going slow wave",data[4][4],data[5][4]
     return False
 
   if data[7][4] > 2.5e-8:
-    print "error in entropy wave too large",data[7][4]
+    print "RMS error in entropy wave too large",data[7][4]
     return False
   if data[7][4]/data[6][4] > 0.4:
     print "not converging for entropy wave",data[6][4],data[7][4]
