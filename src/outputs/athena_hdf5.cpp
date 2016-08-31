096c3ea7d1ae672f23d509e17ccfee24e8c3ff1e
Jiming Shi <jmshi@astro.princeton.edu>
2016-08-31 16:52:30 -0400

    fix conflict with master for output and athena_hdf5

--- src/outputs/athena_hdf5.cpp
+++ src/outputs/athena_hdf5.cpp
@@ -388,6 +388,9 @@ void ATHDF5Output::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag)
       H5P_DEFAULT);
   H5Awrite(attribute, H5T_NATIVE_DOUBLE, &time);
   H5Aclose(attribute);
+  //[JMSHI
+  code_time = (float)time;
+  //JMSHI]
 
   // Write coordinate system
   if (std::strlen(COORDINATE_SYSTEM) > max_name_length) {
@@ -689,6 +692,9 @@ void ATHDF5Output::MakeXDMF()
   xdmf << "<Xdmf Version=\"2.0\">\n";
   xdmf << "<Domain>\n";
   xdmf << "<Grid Name=\"Mesh\" GridType=\"Collection\">\n";
+  //[JMSHI
+  xdmf << " <Time Value=\"" << code_time << "\"/>\n";
+  //JMSHI]
 
   // Go through all MeshBlocks
   for (int n_block = 0; n_block < num_blocks_global; ++n_block) {
