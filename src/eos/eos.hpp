e5c8ece42c50a5230cf4241514ae3c1b9930572d
Christopher J. White <c.white.astro@gmail.com>
2016-11-12 10:15:31 -0800

    changed GR MHD algorithm to a more robust one

--- src/eos/eos.hpp
+++ src/eos/eos.hpp
@@ -110,7 +110,11 @@ private:
   Real rho_min_, rho_pow_;               // variables to control power-law denity floor
   Real u_min_, u_pow_;                   // variables to control power-law energy floor
   AthenaArray<Real> g_, g_inv_;          // metric and its inverse, used in GR
-  AthenaArray<bool> fixed_;              // array for flagging fixed cells, used in GR
+  AthenaArray<Real> normal_dd_;          // normal-frame densities, used in GR
+  AthenaArray<Real> normal_ee_;          // normal-frame energies, used in GR
+  AthenaArray<Real> normal_mm_;          // normal-frame momenta, used in GR
+  AthenaArray<Real> normal_bb_;          // normal-frame fields, used in GR
+  AthenaArray<Real> normal_tt_;          // normal-frame M.B, used in GR
 };
 
 #endif
