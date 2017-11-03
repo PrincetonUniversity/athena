8b0d5f5423284d9a4e470c753e8aa113e1012dd7
Christopher J. White <c.white.astro@gmail.com>
2017-11-02 23:40:35 -0700

    generalized mesh plotting script to take alternate input and output

--- vis/python/plot_mesh.py
+++ vis/python/plot_mesh.py
@@ -1,23 +1,61 @@
-# python script to plot mesh structure in 'mesh_structure.dat' file produced
-# by '-m #np' on command line
+#! /usr/bin/env python
 
-import matplotlib.pyplot as plt
-from mpl_toolkits.mplot3d import Axes3D
-fig = plt.figure()
-ax = fig.add_subplot(111, projection='3d')
-x=[]
-y=[]
-z=[]
-with open("mesh_structure.dat") as f:
+"""
+Script for plotting mesh structure in mesh_structure.dat (default) file produced
+by running Athena++ with "-m <np>" argument.
+
+Can optionally specify "-i <input_file>" and/or "-o <output_file>". Output
+defaults to using "show()" command rather than saving to file.
+"""
+
+# Python modules
+import argparse
+
+# Main function
+def main(**kwargs):
+
+  # Extract inputs
+  input_file = kwargs['input']
+  output_file = kwargs['output']
+
+  # Load Python plotting modules
+  if output_file != 'show':
+    import matplotlib
+    matplotlib.use('agg')
+  import matplotlib.pyplot as plt
+  from mpl_toolkits.mplot3d import Axes3D
+
+  # Read and plot block edges
+  fig = plt.figure()
+  ax = fig.gca(projection='3d')
+  x = []
+  y = []
+  z = []
+  with open(input_file) as f:
     for line in f:
-        if line[0]!='\n' and line[0]!='#':
-                numbers_str = line.split()
-                x.append(float(numbers_str[0]))
-                y.append(float(numbers_str[1]))
-                z.append(float(numbers_str[2]))
-        if line[0]=='\n' and len(x)!=0:
-	        plt.plot(x,y,z,'k-')
-		x=[]
-		y=[]
-		z=[]
-plt.show()
+      if line[0] != '\n' and line[0] != '#':
+        numbers_str = line.split()
+        x.append(float(numbers_str[0]))
+        y.append(float(numbers_str[1]))
+        z.append(float(numbers_str[2]))
+      if line[0] == '\n' and len(x) != 0:
+        plt.plot(x, y, z, 'k-')
+        x = []
+        y = []
+        z = []
+  if output_file == 'show':
+    plt.show()
+  else:
+    plt.savefig(output_file, bbox_inches='tight')
+
+# Execute main function
+if __name__ == '__main__':
+  parser = argparse.ArgumentParser()
+  parser.add_argument('-i', '--input',
+      default='mesh_structure.dat',
+      help='name of mesh structure file')
+  parser.add_argument('-o', '--output',
+      default='show',
+      help='name of output image file to create; omit to display rather than save image')
+  args = parser.parse_args()
+  main(**vars(args))
