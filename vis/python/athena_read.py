bef903805bc1d9485b55a073ce129f1c39b257fe
Matt Coleman <mcoleman@ias.edu>
2018-01-22 15:39:41 -0500

    Added athinput reader to athena_read.py

--- vis/python/athena_read.py
+++ vis/python/athena_read.py
@@ -812,6 +812,47 @@ def restrict_like(vals, levels, vols=None):
     vals_restricted = np.where(levels == level, vals_level, vals_restricted)
   return vals_restricted
 
+
+#=========================================================================================
+
+def athinput(filename):
+  """Reads athinput file and returns a dictionary of dictionaries."""
+  # Read data
+  with open(filename, 'r') as athinput:
+    # Remove comments, extra whitespace and empty lines
+    lines = filter(None, [i.split('#')[0].strip() for i in athinput.readlines()])
+  data = {}
+  # split into blocks, first element will be empty
+  blocks = ('\n'.join(lines)).split('<')[1:]
+
+  def typecast(x):
+    try:
+      return int(x)
+    except ValueError:
+      pass
+    try:
+      return float(x)
+    except ValueError:
+      pass
+    try:
+      return complex(x)
+    except ValueError:
+      pass
+    return x
+
+  def parse_line(line):
+    out = [i.strip() for i in line.split('=')]
+    out[1] = '='.join(out[1:])
+    out[1] = typecast(out[1])
+    return out[:2]
+
+  for block in blocks:
+    info = filter(None, block.split('\n'))
+    key = info.pop(0)[:-1] # last character is '>'
+    data[key] = dict(map(parse_line, info))
+
+  return data
+
 #=========================================================================================
 
 class AthenaError(RuntimeError):
