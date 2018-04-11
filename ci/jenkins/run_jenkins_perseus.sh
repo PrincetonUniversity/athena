5825cace99d83412c1afa7f448aae90f4f12527f
Kyle Gerard Felker <kfelker@princeton.edu>
2018-04-11 19:57:36 -0400

    Use --mpirun=srun for all Jenkins regression tests

--- ci/jenkins/run_jenkins_perseus.sh
+++ ci/jenkins/run_jenkins_perseus.sh
@@ -15,42 +15,43 @@ cd tst/regression
 # Build step #1: GNU compiler and OpenMPI library
 module load rh # latest GNU compiler
 module load openmpi/gcc # /1.10.2/64
-
-# Run regression test sets. Need to specify Slurm mpirun wrapper, srun
-python ./run_tests.py mpi --mpirun=srun
-python ./run_tests.py hydro
-# MHD is currenlty the longest regression test set:
-python ./run_tests.py mhd
-python ./run_tests.py amr
-python ./run_tests.py outputs
-python ./run_tests.py pgen
-python ./run_tests.py sr
-python ./run_tests.py gr
-python ./run_tests.py curvilinear
-#python ./run_tests.py shearingbox
-
 # grav/ regression tests require MPI and FFTW
 module load fftw/gcc/3.3.4
+module list
+
+# Run regression test sets. Need to specify Slurm mpirun wrapper, srun
 python ./run_tests.py grav --mpirun=srun
+python ./run_tests.py mpi --mpirun=srun
+python ./run_tests.py hydro --mpirun=srun
+# MHD is currenlty the longest regression test set:
+python ./run_tests.py mhd --mpirun=srun
+python ./run_tests.py amr --mpirun=srun
+python ./run_tests.py outputs --mpirun=srun
+python ./run_tests.py pgen --mpirun=srun
+python ./run_tests.py sr --mpirun=srun
+python ./run_tests.py gr --mpirun=srun
+python ./run_tests.py curvilinear --mpirun=srun
+#python ./run_tests.py shearingbox
+
 
 # Build step #2: Intel compiler and MPI library
 module purge
 module load intel
 module load intel-mpi
-
-python ./run_tests.py mpi --cxx=icc --mpirun=srun
-python ./run_tests.py hydro --cxx=icc
-python ./run_tests.py mhd --cxx=icc
-python ./run_tests.py amr --cxx=icc
-python ./run_tests.py outputs --cxx=icc
-python ./run_tests.py pgen --cxx=icc
-python ./run_tests.py sr --cxx=icc
-python ./run_tests.py gr --cxx=icc
-python ./run_tests.py curvilinear --cxx=icc
-#python ./run_tests.py shearingbox --cxx=icc
-
 module load fftw/gcc/3.3.4
+module list
+
 python ./run_tests.py grav --cxx=icc --mpirun=srun
+python ./run_tests.py mpi --cxx=icc --mpirun=srun
+python ./run_tests.py hydro --cxx=icc --mpirun=srun
+python ./run_tests.py mhd --cxx=icc --mpirun=srun
+python ./run_tests.py amr --cxx=icc --mpirun=srun
+python ./run_tests.py outputs --cxx=icc --mpirun=srun
+python ./run_tests.py pgen --cxx=icc --mpirun=srun
+python ./run_tests.py sr --cxx=icc --mpirun=srun
+python ./run_tests.py gr --cxx=icc --mpirun=srun
+python ./run_tests.py curvilinear --cxx=icc --mpirun=srun
+#python ./run_tests.py shearingbox --cxx=icc
 
 set +e
 # end regression tests
