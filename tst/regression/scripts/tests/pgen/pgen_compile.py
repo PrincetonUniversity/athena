355f8c211fcef6abe090b1423882dea0e06c1c6f
Kyle Gerard Felker <kfelker@princeton.edu>
2018-04-11 22:35:18 -0400

    Make pgen_compile.py comply with PEP8

--- tst/regression/scripts/tests/pgen/pgen_compile.py
+++ tst/regression/scripts/tests/pgen/pgen_compile.py
@@ -6,32 +6,36 @@ import glob
 import os
 
 current_dir = os.getcwd()
-test = current_dir[0:len(current_dir)-14]
+test = current_dir[0:(len(current_dir) - 14)]
 pgen_directory = test + 'src/pgen/'
 
 # set pgen_choices to list of .cpp files in src/pgen/
 pgen_choices = glob.glob(pgen_directory + '*.cpp')
-# remove 'src/pgen/' prefix and '.cpp' extension from each filename 
+# remove 'src/pgen/' prefix and '.cpp' extension from each filename
 pgen_choices = [choice[len(pgen_directory):-4] for choice in pgen_choices]
+
 
 # Prepare Athena++
 def prepare(**kwargs):
-  for pgen in pgen_choices:
-    if pgen[0:3] == 'gr_':
-      print(pgen)
-#      athena.configure('g',coord='minkowski',flux='hlle',prob=pgen, **kwargs)
-#      athena.make()
-    elif pgen=='cpaw' or pgen=='field_loop' or pgen=='orszag_tang' or pgen=='rotor':
-      athena.configure('b',prob=pgen, **kwargs)
-      athena.make()
-    else:
-      athena.configure(prob=pgen, **kwargs)
-      athena.make()
+    for pgen in pgen_choices:
+        if pgen[0:3] == 'gr_':
+            print(pgen)
+            # athena.configure('g', coord='minkowski', flux='hlle', prob=pgen)
+            # athena.make()
+        elif (pgen == 'cpaw' or pgen == 'field_loop' or
+              pgen == 'orszag_tang' or pgen == 'rotor'):
+            athena.configure('b', prob=pgen, **kwargs)
+            athena.make()
+        else:
+            athena.configure(prob=pgen, **kwargs)
+            athena.make()
+
 
 # Run Athena++
 def run(**kwargs):
-  pass
+    pass
+
 
 # Analyze outputs
 def analyze():
-  return True
+    return True
