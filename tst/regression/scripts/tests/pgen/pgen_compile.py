f0977068ce3405c73b24552e7d9ae606cad0875b
Kyle Gerard Felker <kfelker@princeton.edu>
2018-04-17 13:55:22 -0400

    Group pgen_compile tests by configure flags and avoid make clean

--- tst/regression/scripts/tests/pgen/pgen_compile.py
+++ tst/regression/scripts/tests/pgen/pgen_compile.py
@@ -12,41 +12,46 @@ pgen_directory = test + 'src/pgen/'
 # set pgen_choices to list of .cpp files in src/pgen/
 pgen_choices = glob.glob(pgen_directory + '*.cpp')
 # remove 'src/pgen/' prefix and '.cpp' extension from each filename
-pgen_choices = [choice[len(pgen_directory):-4] for choice in pgen_choices]
+pgen_choices = set([choice[len(pgen_directory):-4] for choice in pgen_choices])
+
+# Skip --prob=default_pgen; weak attr. may not compile with Intel C++ compiler
+pgen_choices.remove('default_pgen')
+# Currently testing GR Hydro, not MHD:
+# coord='minkowski', #'-t'
+gr_probs = set([pgen for pgen in pgen_choices if pgen[0:3] == 'gr_'])
+# MHD-required problems: --rsolver=hlld by default
+mhd_probs = set(['cpaw', 'field_loop', 'orszag_tang', 'rotor'])
+# Shearing box MHD problems
+shear_probs = set(['hb3', 'hgb', 'ssheet', 'strat'])
+# Newtonian Hydro-only or MHD-optional problems are all leftover pgen/ files
+# --rsolver=hllc by default
+hydro_probs = pgen_choices - gr_probs - mhd_probs - shear_probs
+# Curvilinear problems
+
+# Define configure flags for each set:
+gr_args = ['g']
+mhd_args = ['b']
+shear_args = ['b', 'shear']
+hydro_args = []
 
 
 # Prepare Athena++
 def prepare(**kwargs):
     # Check that code compiles all pgen files in single or double precision
     for single_precision in [True, False]:
-        for pgen in pgen_choices:
-            args = []
+        # Skipping GR Hydro tests for now
+        for pgen_set, args in zip([mhd_probs, shear_probs, hydro_probs],
+                                  [mhd_args, shear_args, hydro_args]):
             if single_precision:
-                args = ['float']
-            # Skip GR and default_pgen problems by default
-            # (default_pgen may not compile with Intel C++ compiler)
-            if (pgen[0:3] == 'gr_' or pgen == 'default_pgen'):
-                print(pgen)
-                args.extend(['g'])
-                # athena.configure(args, coord='minkowski',
-                #                  flux='hlle', prob=pgen)
-                # athena.make()
-            # MHD-required problems
-            elif (pgen == 'cpaw' or pgen == 'field_loop' or
-                  pgen == 'orszag_tang' or pgen == 'rotor'):
-                args.extend(['b'])
+                args.extend(['float'])
+            # "make clean" only for the first problem in the set that shares
+            # ./configure.py flags (except --pgen)
+            pgen = pgen_set.pop()
+            athena.configure(*args, prob=pgen, **kwargs)
+            athena.make(clean_first=True)
+            for pgen in pgen_set:
                 athena.configure(*args, prob=pgen, **kwargs)
-                athena.make()
-            # Shearinbox MHD problems
-            elif (pgen == 'hb3' or pgen == 'hgb' or pgen == 'ssheet' or
-                  pgen == 'strat'):
-                args.extend(['b', 'shear'])
-                athena.configure(*args, prob=pgen, **kwargs)
-                athena.make()
-            # Hydro-only and MHD-optional problems
-            else:
-                athena.configure(*args, prob=pgen, **kwargs)
-                athena.make()
+                athena.make(clean_first=False)
 
 
 # Run Athena++
