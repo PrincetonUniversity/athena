0ef42ed7082865721ddcda284f47f57f7a8a0db3
Kyle Gerard Felker <kfelker@princeton.edu>
2018-05-01 14:35:03 -0400

    Separate inner/outer x1/x2 outflow BC fns into B/U/Bcc

--- src/bvals/outflow.cpp
+++ src/bvals/outflow.cpp
@@ -305,3 +305,232 @@ void OutflowOuterX3(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim,
 
   return;
 }
+
+
+
+
+// begin fourth-order MHD outflow BC functions-------------------------
+
+
+void OutflowCellFieldInnerX1(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &bc,
+							 int is, int ie, int js, int je, int ks, int ke) {
+  for (int n=0; n<(NFIELD); ++n) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=js; j<=je; ++j) {
+        for (int i=1; i<=(NGHOST); ++i) {
+          bc(n,k,j,is-i) = bc(n,k,j,is);
+        }
+      }
+    }
+  }
+  return;
+}
+
+//----------------------------------------------------------------------------------------
+//! \fn void OutflowFieldInnerX1(MeshBlock *pmb, Coordinates *pco, FaceField &b,
+//                          int is, int ie, int js, int je, int ks, int ke)
+//  \brief OUTFLOW boundary conditions, inner x1 boundary. Applied to arbitrary FaceField
+//         variable
+
+void OutflowFaceFieldInnerX1(MeshBlock *pmb, Coordinates *pco, FaceField &b, int is,
+                             int ie, int js, int je, int ks, int ke) {
+  // copy face-centered magnetic fields into ghost zones
+  if (MAGNETIC_FIELDS_ENABLED) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=js; j<=je; ++j) {
+#pragma omp simd
+        for (int i=1; i<=(NGHOST); ++i) {
+          b.x1f(k,j,(is-i)) = b.x1f(k,j,is);
+        }
+      }
+    }
+
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=js; j<=je+1; ++j) {
+#pragma omp simd
+        for (int i=1; i<=(NGHOST); ++i) {
+          b.x2f(k,j,(is-i)) = b.x2f(k,j,is);
+        }
+      }
+    }
+
+    for (int k=ks; k<=ke+1; ++k) {
+      for (int j=js; j<=je; ++j) {
+#pragma omp simd
+        for (int i=1; i<=(NGHOST); ++i) {
+          b.x3f(k,j,(is-i)) = b.x3f(k,j,is);
+        }
+      }
+    }
+  }
+  return;
+}
+
+
+void OutflowCellFieldOuterX1(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &bc,
+							 int is, int ie, int js, int je, int ks, int ke) {
+  for (int n=0; n<(NFIELD); ++n) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=js; j<=je; ++j) {
+        for (int i=1; i<=(NGHOST); ++i) {
+          bc(n,k,j,ie+i) = bc(n,k,j,ie);
+        }
+      }
+    }
+  }
+
+  return;
+}
+//----------------------------------------------------------------------------------------
+//! \fn void OutflowFieldOuterX1(MeshBlock *pmb, Coordinates *pco, FaceField &b,
+//                         int is, int ie, int js, int je, int ks, int ke)
+//  \brief OUTFLOW boundary conditions, outer x1 boundary. Applied to arbitrary
+//         FaceField variable
+
+void OutflowFaceFieldOuterX1(MeshBlock *pmb, Coordinates *pco,FaceField &b, int is,
+                             int ie, int js, int je, int ks, int ke) {
+  // copy face-centered magnetic fields into ghost zones
+  if (MAGNETIC_FIELDS_ENABLED) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=js; j<=je; ++j) {
+#pragma omp simd
+        for (int i=1; i<=(NGHOST); ++i) {
+          b.x1f(k,j,(ie+i+1)) = b.x1f(k,j,(ie+1));
+        }
+      }
+    }
+
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=js; j<=je+1; ++j) {
+#pragma omp simd
+        for (int i=1; i<=(NGHOST); ++i) {
+          b.x2f(k,j,(ie+i)) = b.x2f(k,j,ie);
+        }
+      }
+    }
+
+    for (int k=ks; k<=ke+1; ++k) {
+      for (int j=js; j<=je; ++j) {
+#pragma omp simd
+        for (int i=1; i<=(NGHOST); ++i) {
+          b.x3f(k,j,(ie+i)) = b.x3f(k,j,ie);
+        }
+      }
+    }
+  }
+
+  return;
+}
+
+void OutflowCellFieldInnerX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &bc,
+							 int is, int ie, int js, int je, int ks, int ke) {
+  for (int n=0; n<(NFIELD); ++n) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=1; j<=(NGHOST); ++j) {
+        for (int i=is; i<=ie; ++i) {
+          bc(n,k,js-j,i) = bc(n,k,js,i);
+        }
+      }
+    }
+  }
+  return;
+}
+
+//----------------------------------------------------------------------------------------
+//! \fn void OutflowFieldInnerX2(MeshBlock *pmb, Coordinates *pco, FaceField &b,
+//                          int is, int ie, int js, int je, int ks, int ke)
+//  \brief OUTFLOW boundary conditions, inner x2 boundary. Applied to arbitrary FaceField
+//         variable
+
+void OutflowFaceFieldInnerX2(MeshBlock *pmb, Coordinates *pco, FaceField &b, int is,
+                             int ie, int js, int je, int ks, int ke) {
+  // copy face-centered magnetic fields into ghost zones
+  if (MAGNETIC_FIELDS_ENABLED) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=1; j<=(NGHOST); ++j) {
+#pragma omp simd
+        for (int i=is; i<=ie+1; ++i) {
+          b.x1f(k,(js-j),i) = b.x1f(k,js,i);
+        }
+      }
+    }
+
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=1; j<=(NGHOST); ++j) {
+#pragma omp simd
+        for (int i=is; i<=ie; ++i) {
+          b.x2f(k,(js-j),i) = b.x2f(k,js,i);
+        }
+      }
+    }
+
+    for (int k=ks; k<=ke+1; ++k) {
+      for (int j=1; j<=(NGHOST); ++j) {
+#pragma omp simd
+        for (int i=is; i<=ie; ++i) {
+          b.x3f(k,(js-j),i) = b.x3f(k,js,i);
+        }
+      }
+    }
+  }
+  return;
+}
+
+
+void OutflowCellFieldOuterX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &bc,
+							 int is, int ie, int js, int je, int ks, int ke) {
+  for (int n=0; n<(NFIELD); ++n) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=1; j<=(NGHOST); ++j) {
+        for (int i=is; i<=ie; ++i) {
+          bc(n,k,je+j,i) = bc(n,k,je,i);
+        }
+      }
+    }
+  }
+
+  return;
+}
+//----------------------------------------------------------------------------------------
+//! \fn void OutflowFieldOuterX2(MeshBlock *pmb, Coordinates *pco, FaceField &b,
+//                         int is, int ie, int js, int je, int ks, int ke)
+//  \brief OUTFLOW boundary conditions, outer x2 boundary. Applied to arbitrary
+//         FaceField variable
+
+void OutflowFaceFieldOuterX2(MeshBlock *pmb, Coordinates *pco,FaceField &b, int is,
+                             int ie, int js, int je, int ks, int ke) {
+  // copy face-centered magnetic fields into ghost zones
+  if (MAGNETIC_FIELDS_ENABLED) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=1; j<=(NGHOST); ++j) {
+#pragma omp simd
+        for (int i=is; i<=ie; ++i) {
+          b.x1f(k,(je+1+j),i) = b.x1f(k,(je+1),i);
+        }
+      }
+    }
+
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=1; j<=(NGHOST); ++j) {
+#pragma omp simd
+        for (int i=is; i<=ie; ++i) {
+          b.x2f(k,(je+1+j),i) = b.x2f(k,(je+1),i);
+        }
+      }
+    }
+
+    for (int k=ks; k<=ke+1; ++k) {
+      for (int j=1; j<=(NGHOST); ++j) {
+#pragma omp simd
+        for (int i=is; i<=ie; ++i) {
+          b.x3f(k,(je+1+j),i) = b.x3f(k,(je+1),i);
+        }
+      }
+    }
+  }
+
+  return;
+}
+
+
+// -------- end fourth-order MHD outflow BC functions
