0ceec565a11a6ffad12b396e5dc9db2724b68160
Matt Coleman <mcoleman@ias.edu>
2018-05-03 21:36:07 -0400

    Argument order change

--- eos_test_config.sh
+++ eos_test_config.sh
@@ -2,5 +2,5 @@
 
 # Default configuration and look-up tables for EOS tests
 
-python configure.py --eos general --prob shock_tube
+python configure.py --prob shock_tube --eos general
 python mk_eos_test_table.py
