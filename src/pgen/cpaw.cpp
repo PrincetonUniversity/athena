32d958ae5a7fc007d17e31133426350872e0c4cd
Kyle Gerard Felker <kfelker@princeton.edu>
2018-05-08 00:45:03 -0400

    Extend average vec potential initialization to 3D for CPAW
    Use this for non-refined meshes; comment out original Ai()

--- src/pgen/cpaw.cpp
+++ src/pgen/cpaw.cpp
@@ -365,7 +365,11 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
             a1(k,j,i) = 0.5*(A1(x1l, pcoord->x2f(j), pcoord->x3f(k)) +
                              A1(x1r, pcoord->x2f(j), pcoord->x3f(k)));
           } else {
-            a1(k,j,i) = A1(pcoord->x1v(i), pcoord->x2f(j), pcoord->x3f(k));
+            // hardcode ang_2=0 initialization of <A>
+            if (i != ie+1)
+              a1(k,j,i) = A1_ave(pcoord->x1f(i), pcoord-> x1f(i+1), pcoord->x2f(j),
+                                 pcoord->x3f(k));
+            //a1(k,j,i) = A1(pcoord->x1v(i), pcoord->x2f(j), pcoord->x3f(k));
           }
 
           if ((pbval->nblevel[1][1][0]>level && i==is)
@@ -381,7 +385,11 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
             a2(k,j,i) = 0.5*(A2(pcoord->x1f(i), x2l, pcoord->x3f(k)) +
                              A2(pcoord->x1f(i), x2r, pcoord->x3f(k)));
           } else {
-            a2(k,j,i) = A2(pcoord->x1f(i), pcoord->x2v(j), pcoord->x3f(k));
+            // hardcode ang_2=0 initialization of <A>
+            if (j != je+1)
+              a2(k,j,i) = A2_ave(pcoord->x1f(i), pcoord->x2f(j), pcoord->x2f(j+1),
+                                 pcoord->x3f(k));
+            //a2(k,j,i) = A2(pcoord->x1f(i), pcoord->x2v(j), pcoord->x3f(k));
           }
 
           if ((pbval->nblevel[1][1][0]>level && i==is)
@@ -397,7 +405,11 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
             a3(k,j,i) = 0.5*(A3(pcoord->x1f(i), pcoord->x2f(j), x3l) +
                              A3(pcoord->x1f(i), pcoord->x2f(j), x3r));
           } else {
-            a3(k,j,i) = A3(pcoord->x1f(i), pcoord->x2f(j), pcoord->x3v(k));
+            // hardcode ang_2=0 initialization of <A>
+            if (k != ke+1)
+              a3(k,j,i) = A3_ave(pcoord->x1f(i), pcoord->x2f(j), pcoord->x3f(k),
+                                 pcoord->x3f(k+1));
+            //a3(k,j,i) = A3(pcoord->x1f(i), pcoord->x2f(j), pcoord->x3v(k));
           }
         }
       }
@@ -524,19 +536,27 @@ static Real A3(const Real x1, const Real x2, const Real x3) {
 //----------------------------------------------------------------------------------------
 //! \fn static Real A1_ave(const Real x1,const Real x2,const Real x3)
 //  \brief A1_ave: 1-component of vector potential averaged along x1 edge
-//         For 2D, assume that cos_a2=1, sin_a2=0
-// TODO(kfelker): extend to 3D
+// TODO(kfelker): consider switching dx1f to pco->dx1f, etc
 
 static Real A1_ave(const Real x1f, const Real x1f_ip1, const Real x2, const Real x3) {
-  Real x =  x1f*cos_a3 + x2*sin_a3;
-  Real x_ip1 =  x1f_ip1*cos_a3 + x2*sin_a3;
+  Real x =  cos_a2*cos_a3*x1f + cos_a2*sin_a3*x2 + sin_a2*x3;
+  Real x_ip1 =  cos_a2*cos_a3*x1f_ip1 + cos_a2*sin_a3*x2 + sin_a2*x3;
   Real dx1f = x1f_ip1 - x1f;
-  Real Ay;
-  if (cos_a3 != 0.0)
-	Ay =  (fac*b_perp/(SQR(k_par)*cos_a3))*(-cos(k_par*(x_ip1)) + cos(k_par*(x)));
-  else // vertical coordinate aligned wave--- uniform on the x1 edge
+  Real Ay, Az;
+
+  if (cos_a3 != 0.0) {
+	Ay =  (fac*b_perp/(SQR(k_par)*cos_a3))*(-cos(k_par*x_ip1) + cos(k_par*x));
+  } else { // vertical (+x2) coordinate aligned wave--- Ay uniform on the x1 edge
 	Ay = dx1f*((fac*b_perp/k_par)*sin(k_par*(x))); // x  = x_ip1
-  return -Ay*sin_a3/dx1f;
+  }
+  if (cos_a2 != 0.0) {
+    // cancelling cos_a3 factor
+    Az = b_perp/(SQR(k_par)*cos_a2)*(sin(k_par*x_ip1) - sin(k_par*x));
+  } else { // polar (+x3) coordinate aligned wave: only linear term in Az changes along x1
+    Az = dx1f*((cos_a3*b_perp/k_par)*cos(k_par*x)); // x = x3
+  }
+  Az += b_par*cos_a3*(-sin_a3*0.5*(SQR(x1f_ip1) - SQR(x1f)) + dx1f*cos_a3*x2);
+  return (-Ay*sin_a3 - Az*sin_a2)/dx1f;
 }
 
 //----------------------------------------------------------------------------------------
@@ -544,16 +564,24 @@ static Real A1_ave(const Real x1f, const Real x1f_ip1, const Real x2, const Real
 //  \brief A2_ave: 2-component of vector potential averaged along x2 edge
 
 static Real A2_ave(const Real x1, const Real x2f, const Real x2f_jp1, const Real x3) {
-  Real x =  x1*cos_a3 + x2f*sin_a3;
-  Real x_jp1 =  x1*cos_a3 + x2f_jp1*sin_a3;
+  Real x =  cos_a2*cos_a3*x1 + cos_a2*sin_a3*x2f + sin_a2*x3;
+  Real x_jp1 =  cos_a2*cos_a3*x1 + cos_a2*sin_a3*x2f_jp1 + sin_a2*x3;
   Real dx2f = x2f_jp1 - x2f;
-  Real Ay;
-  if (sin_a3 != 0.0)
-	Ay =  (fac*b_perp/(SQR(k_par)*sin_a3))*(-cos(k_par*(x_jp1)) + cos(k_par*(x)));
-  else // horizontal coordinate aligned wave--- uniform on the x2 edge
-	Ay = dx2f*((fac*b_perp/k_par)*sin(k_par*(x))); // x = x_jp1
+  Real Ay, Az;
 
-  return Ay*cos_a3/dx2f;
+  if (sin_a3 != 0.0) {
+	Ay =  (fac*b_perp/(SQR(k_par)*sin_a3))*(-cos(k_par*x_jp1) + cos(k_par*x));
+  } else { // horizontal (+x1) coordinate aligned wave--- Ay uniform on the x2 edge
+	Ay = dx2f*((fac*b_perp/k_par)*sin(k_par*(x))); // x = x_jp1
+  }
+  if (cos_a2 != 0.0) {
+    // cancelling sin_a3 factor
+    Az = b_perp/(SQR(k_par)*cos_a2)*(sin(k_par*x_jp1) - sin(k_par*x));
+  } else { // polar (+x3) coordinate aligned wave: only linear term in Az changes along x2
+    Az = dx2f*((sin_a3*b_perp/k_par)*cos(k_par*x)); // x = x3
+  }
+  Az += b_par*cos_a3*(-dx2f*sin_a3*x1 + cos_a3*0.5*(SQR(x2f_jp1) - SQR(x2f)));
+  return (Ay*cos_a3 - Az*sin_a2)/dx2f;
 }
 
 //----------------------------------------------------------------------------------------
@@ -561,10 +589,16 @@ static Real A2_ave(const Real x1, const Real x2f, const Real x2f_jp1, const Real
 //  \brief A3_ave: 3-component of vector potential averaged along x3 edge
 
 static Real A3_ave(const Real x1, const Real x2, const Real x3f, const Real x3f_kp1) {
-  Real x =  x1*cos_a3 + x2*sin_a3;
+  Real x =  cos_a2*cos_a3*x1 + cos_a2*sin_a3*x2 + sin_a2*x3f;
+  Real x_kp1 =  cos_a2*cos_a3*x1 + cos_a2*sin_a3*x2 + sin_a2*x3f_kp1;
   Real y = -x1*sin_a3        + x2*cos_a3;
   Real dx3f = x3f_kp1 - x3f;
-  Real Az = (b_perp/k_par)*cos(k_par*(x)) + b_par*y;
+  Real Az;
 
-  return Az;
+  if (sin_a2 != 0.0) {
+    Az = cos_a2*b_perp/(SQR(k_par)*sin_a2)*(sin(k_par*x_kp1) - sin(k_par*x));
+  } else { // wave propagates in x1-x2 plane
+    Az = dx3f*((b_perp/k_par)*cos(k_par*(x)) + b_par*y); // x, y do not depend on x3
+  }
+  return Az/dx3f;
 }
