2b4ee513a5fe280161e11714c7ff401519a6b2f0
Kyle Gerard Felker <kfelker@princeton.edu>
2018-05-31 13:42:57 -0400

    Disable parallel HDF5 for Travis CI for now

--- tst/ci/travis/install_hdf5.sh
+++ tst/ci/travis/install_hdf5.sh
@@ -14,7 +14,7 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
         echo "Installing HDF5 with Homebrew"
 	HOMEBREW_TEMP=$TRAVIS_BUILD_DIR/hdf5
 	brew update
-        brew install hdf5 --with-mpi
+        brew install hdf5 # --with-mpi
 	brew unlink hdf5
 	# /usr/local/opt symlinks to Cellar are preserved, use these:
 	# (note, /usr/local/bin/bin -> /usr/local/bin = circular symlink)
@@ -36,7 +36,8 @@ else
 	tar zxf hdf5-1.10.1.tar.gz
 	echo "Configuring and building HDF5"
 	cd hdf5-1.10.1
-	./configure --prefix=$TRAVIS_BUILD_DIR/hdf5 --with-mpi CC=$C_COMPILER CXX=$CXX_COMPILER &> hdf5.configure
+	./configure --prefix=$TRAVIS_BUILD_DIR/hdf5 CC=$C_COMPILER CXX=$CXX_COMPILER &> hdf5.configure
+	# CC=/Users/kfelker/mpich-install/bin/mpicc ./configure --enable-parallel
 	make -j4 &> hdf5.make
 	#make check &> hdf5.makecheck
 	make install &> hdf5.install
