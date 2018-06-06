5bd94e989c89fa83812bf7d46fa5c86809222289
Bei Wang <beiwang@princeton.edu>
2018-06-06 11:59:12 -0400

    optimize hydro SR with HLLC

--- src/hydro/rsolvers/hydro/hllc_rel.cpp
+++ src/hydro/rsolvers/hydro/hllc_rel.cpp
@@ -18,6 +18,16 @@
 #include "../../../eos/eos.hpp"                  // EquationOfState
 #include "../../../mesh/mesh.hpp"                // MeshBlock
 
+#if defined(__AVX512F__)
+#define SIMD_WIDTH 8
+#elif defined(__AVX__)
+#define SIMD_WIDTH 4
+#elif defined(__SSE2__)
+#define SIMD_WIDTH 2
+#else
+#define SIMD_WIDTH 4
+#endif
+
 // Declarations
 static void HLLCTransforming(MeshBlock *pmb, const int k, const int j, const int il,
     const int iu, const int ivx, const AthenaArray<Real> &bb,
@@ -135,56 +145,62 @@ static void HLLCTransforming(MeshBlock *pmb, const int k, const int j, const int
   const Real gamma_adi = pmb->peos->GetGamma();
 
   // Go through each interface
-  #pragma omp simd
+#pragma omp simd simdlen(SIMD_WIDTH)
   for (int i = il; i <= iu; ++i) {
 
     // Extract left primitives
-    const Real &rho_l = prim_l(IDN,k,j,i);
-    const Real &pgas_l = prim_l(IPR,k,j,i);
+    Real rho_l = prim_l(IDN,k,j,i);
+    Real pgas_l = prim_l(IPR,k,j,i);
+    Real vx_l = prim_l(ivx,k,j,i);
+    Real vy_l = prim_l(ivy,k,j,i);
+    Real vz_l = prim_l(ivz,k,j,i);
+
+    // Extract right primitives
+    Real rho_r = prim_r(IDN,k,j,i);
+    Real pgas_r = prim_r(IPR,k,j,i);
+    Real vx_r = prim_r(ivx,k,j,i);
+    Real vy_r = prim_r(ivy,k,j,i);
+    Real vz_r = prim_r(ivz,k,j,i);
+
+    Real sqr_ml = SQR(vx_l) + SQR(vy_l) + SQR(vz_l);
+    Real sqr_mr = SQR(vx_r) + SQR(vy_r) + SQR(vz_r);
+
     Real u_l[4];
     if (GENERAL_RELATIVITY) {
-      u_l[1] = prim_l(ivx,k,j,i);
-      u_l[2] = prim_l(ivy,k,j,i);
-      u_l[3] = prim_l(ivz,k,j,i);
-      u_l[0] = std::sqrt(1.0 + SQR(u_l[1]) + SQR(u_l[2]) + SQR(u_l[3]));
+      u_l[0] = std::sqrt(1.0 + sqr_ml);
+      u_l[1] = vx_l;
+      u_l[2] = vy_l;
+      u_l[3] = vz_l;
     } else {  // SR
-      const Real &vx_l = prim_l(ivx,k,j,i);
-      const Real &vy_l = prim_l(ivy,k,j,i);
-      const Real &vz_l = prim_l(ivz,k,j,i);
-      u_l[0] = std::sqrt(1.0 / (1.0 - SQR(vx_l) - SQR(vy_l) - SQR(vz_l)));
+      u_l[0] = std::sqrt(1.0 / (1.0 - sqr_ml));
       u_l[1] = u_l[0] * vx_l;
       u_l[2] = u_l[0] * vy_l;
       u_l[3] = u_l[0] * vz_l;
     }
 
-    // Extract right primitives
-    const Real &rho_r = prim_r(IDN,k,j,i);
-    const Real &pgas_r = prim_r(IPR,k,j,i);
     Real u_r[4];
     if (GENERAL_RELATIVITY) {
-      u_r[1] = prim_r(ivx,k,j,i);
-      u_r[2] = prim_r(ivy,k,j,i);
-      u_r[3] = prim_r(ivz,k,j,i);
-      u_r[0] = std::sqrt(1.0 + SQR(u_r[1]) + SQR(u_r[2]) + SQR(u_r[3]));
+      u_r[0] = std::sqrt(1.0 + sqr_mr);
+      u_r[1] = vx_r;
+      u_r[2] = vy_r;
+      u_r[3] = vz_r;
     } else {  // SR
-      const Real &vx_r = prim_r(ivx,k,j,i);
-      const Real &vy_r = prim_r(ivy,k,j,i);
-      const Real &vz_r = prim_r(ivz,k,j,i);
-      u_r[0] = std::sqrt(1.0 / (1.0 - SQR(vx_r) - SQR(vy_r) - SQR(vz_r)));
+      u_r[0] = std::sqrt(1.0 / (1.0 - sqr_mr));
       u_r[1] = u_r[0] * vx_r;
       u_r[2] = u_r[0] * vy_r;
       u_r[3] = u_r[0] * vz_r;
     }
 
+    Real gamma_tmp = gamma_adi/(gamma_adi - 1.0);
     // Calculate wavespeeds in left state (MB2005 23)
     Real lambda_p_l, lambda_m_l;
-    Real wgas_l = rho_l + gamma_adi/(gamma_adi-1.0) * pgas_l;
+    Real wgas_l = rho_l + gamma_tmp * pgas_l;
     pmb->peos->SoundSpeedsSR(wgas_l, pgas_l, u_l[1]/u_l[0], SQR(u_l[0]), &lambda_p_l,
         &lambda_m_l);
 
     // Calculate wavespeeds in right state (MB2005 23)
     Real lambda_p_r, lambda_m_r;
-    Real wgas_r = rho_r + gamma_adi/(gamma_adi-1.0) * pgas_r;
+    Real wgas_r = rho_r + gamma_tmp * pgas_r;
     pmb->peos->SoundSpeedsSR(wgas_r, pgas_r, u_r[1]/u_r[0], SQR(u_r[0]), &lambda_p_r,
         &lambda_m_r);
 
@@ -194,58 +210,63 @@ static void HLLCTransforming(MeshBlock *pmb, const int k, const int j, const int
 
     // Calculate conserved quantities in L region (MB2005 3)
     Real cons_l[NWAVE];
+    Real wgas_u0_l = wgas_l * u_l[0];
+
     cons_l[IDN] = rho_l * u_l[0];
-    cons_l[IEN] = wgas_l * u_l[0] * u_l[0] - pgas_l;
-    cons_l[ivx] = wgas_l * u_l[1] * u_l[0];
-    cons_l[ivy] = wgas_l * u_l[2] * u_l[0];
-    cons_l[ivz] = wgas_l * u_l[3] * u_l[0];
+    cons_l[IEN] = wgas_u0_l * u_l[0] - pgas_l;
+    cons_l[ivx] = wgas_u0_l * u_l[1];
+    cons_l[ivy] = wgas_u0_l * u_l[2];
+    cons_l[ivz] = wgas_u0_l * u_l[3];
 
     // Calculate fluxes in L region (MB2005 2,3)
     Real flux_l[NWAVE];
+    Real wgas_u1_l = wgas_l * u_l[1];
     flux_l[IDN] = rho_l * u_l[1];
-    flux_l[IEN] = wgas_l * u_l[0] * u_l[1];
-    flux_l[ivx] = wgas_l * u_l[1] * u_l[1] + pgas_l;
-    flux_l[ivy] = wgas_l * u_l[2] * u_l[1];
-    flux_l[ivz] = wgas_l * u_l[3] * u_l[1];
+    flux_l[IEN] = wgas_u1_l * u_l[0];
+    flux_l[ivx] = wgas_u1_l * u_l[1] + pgas_l;
+    flux_l[ivy] = wgas_u1_l * u_l[2];
+    flux_l[ivz] = wgas_u1_l * u_l[3];
 
     // Calculate conserved quantities in R region (MB2005 3)
     Real cons_r[NWAVE];
+    Real wgas_u0_r = wgas_r * u_r[0];
     cons_r[IDN] = rho_r * u_r[0];
-    cons_r[IEN] = wgas_r * u_r[0] * u_r[0] - pgas_r;
-    cons_r[ivx] = wgas_r * u_r[1] * u_r[0];
-    cons_r[ivy] = wgas_r * u_r[2] * u_r[0];
-    cons_r[ivz] = wgas_r * u_r[3] * u_r[0];
+    cons_r[IEN] = wgas_u0_r * u_r[0] - pgas_r;
+    cons_r[ivx] = wgas_u0_r * u_r[1];
+    cons_r[ivy] = wgas_u0_r * u_r[2];
+    cons_r[ivz] = wgas_u0_r * u_r[3];
 
     // Calculate fluxes in R region (MB2005 2,3)
     Real flux_r[NWAVE];
+    Real wgas_u1_r = wgas_r * u_r[1];
     flux_r[IDN] = rho_r * u_r[1];
-    flux_r[IEN] = wgas_r * u_r[0] * u_r[1];
-    flux_r[ivx] = wgas_r * u_r[1] * u_r[1] + pgas_r;
-    flux_r[ivy] = wgas_r * u_r[2] * u_r[1];
-    flux_r[ivz] = wgas_r * u_r[3] * u_r[1];
+    flux_r[IEN] = wgas_u1_r * u_r[0];
+    flux_r[ivx] = wgas_u1_r * u_r[1] + pgas_r;
+    flux_r[ivy] = wgas_u1_r * u_r[2];
+    flux_r[ivz] = wgas_u1_r * u_r[3];
 
+    Real lambda_diff_inverse = 1.0 / (lambda_r-lambda_l);
     // Calculate conserved quantities in HLL region in GR (MB2005 9)
     Real cons_hll[NWAVE];
     for (int n = 0; n < NWAVE; ++n) {
-      cons_hll[n] = (lambda_r*cons_r[n] - lambda_l*cons_l[n] + flux_l[n] - flux_r[n])
-          / (lambda_r-lambda_l);
+      cons_hll[n] = (lambda_r*cons_r[n] - lambda_l*cons_l[n] + flux_l[n] - flux_r[n]) * lambda_diff_inverse;
     }
 
     // Calculate fluxes in HLL region (MB2005 11)
     Real flux_hll[NWAVE];
     for (int n = 0; n < NWAVE; ++n) {
       flux_hll[n] = (lambda_r*flux_l[n] - lambda_l*flux_r[n]
-          + lambda_l*lambda_r * (cons_r[n] - cons_l[n])) / (lambda_r-lambda_l);
+          + lambda_l*lambda_r * (cons_r[n] - cons_l[n])) * lambda_diff_inverse;
     }
 
     // Calculate contact wavespeed (MB2005 18)
     Real lambda_star;
+    Real a = flux_hll[IEN];
+    Real b = -(cons_hll[IEN] + flux_hll[ivx]);
+    Real c = cons_hll[ivx];
+    Real q = -0.5 * (b - std::sqrt(SQR(b) - 4.0*a*c));
     if (std::abs(flux_hll[IEN]) > TINY_NUMBER) {  // use quadratic formula
       // Follows algorithm in Numerical Recipes (section 5.6) for avoiding cancellations
-      Real a = flux_hll[IEN];
-      Real b = -(cons_hll[IEN] + flux_hll[ivx]);
-      Real c = cons_hll[ivx];
-      Real q = -0.5 * (b - std::sqrt(SQR(b) - 4.0*a*c));
       lambda_star = c / q;
     } else {  // no quadratic term
       lambda_star = cons_hll[ivx] / (cons_hll[IEN] + flux_hll[ivx]);
@@ -257,11 +278,11 @@ static void HLLCTransforming(MeshBlock *pmb, const int k, const int j, const int
 
     // Calculate conserved quantities in L* region (MB2005 16)
     Real cons_lstar[NWAVE];
-    Real vx_l = u_l[1] / u_l[0];
+    Real vx_l_ratio = u_l[1] / u_l[0];
     for (int n = 0; n < NWAVE; ++n) {
-      cons_lstar[n] = cons_l[n] * (lambda_l-vx_l);
+      cons_lstar[n] = cons_l[n] * (lambda_l-vx_l_ratio);
     }
-    cons_lstar[IEN] += pgas_star*lambda_star - pgas_l*vx_l;
+    cons_lstar[IEN] += pgas_star*lambda_star - pgas_l*vx_l_ratio;
     cons_lstar[ivx] += pgas_star - pgas_l;
     for (int n = 0; n < NWAVE; ++n) {
       cons_lstar[n] /= lambda_l - lambda_star;
@@ -275,11 +296,11 @@ static void HLLCTransforming(MeshBlock *pmb, const int k, const int j, const int
 
     // Calculate conserved quantities in R* region (MB2005 16)
     Real cons_rstar[NWAVE];
-    Real vx_r = u_r[1] / u_r[0];
+    Real vx_r_ratio = u_r[1] / u_r[0];
     for (int n = 0; n < NWAVE; ++n) {
-      cons_rstar[n] = cons_r[n] * (lambda_r-vx_r);
+      cons_rstar[n] = cons_r[n] * (lambda_r-vx_r_ratio);
     }
-    cons_rstar[IEN] += pgas_star*lambda_star - pgas_r*vx_r;
+    cons_rstar[IEN] += pgas_star*lambda_star - pgas_r*vx_r_ratio;
     cons_rstar[ivx] += pgas_star - pgas_r;
     for (int n = 0; n < NWAVE; ++n) {
       cons_rstar[n] /= lambda_r - lambda_star;
