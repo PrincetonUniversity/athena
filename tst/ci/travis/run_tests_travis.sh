f86443491f3a4c34eaff00964afd06fe9c9d3243
Kyle Gerard Felker <kfelker@princeton.edu>
2018-06-06 15:13:24 -0400

    Use --oversubscribe flag for mpi/ and grav/ tests on Travis CI
    Need to differentiate MPICH and OpenMPI jobs

--- tst/ci/travis/run_tests_travis.sh
+++ tst/ci/travis/run_tests_travis.sh
@@ -8,8 +8,10 @@ set -ev
 # (Could alternatively group sets of tests with && operator)
 cd regression/
 # python3 run_tests.py pgen --config=--cxx=$TEMP_CXX --config=--cflag="$(./ci/set_warning_cflag.sh $TEMP_CXX)"
-python3 run_tests.py mpi --config=--cxx=$TEMP_CXX # --silent
-python3 run_tests.py grav/jeans_3d --config=--cxx=$TEMP_CXX # requires FFTW library
+
+python3 run_tests.py mpi --config=--cxx=$TEMP_CXX --mpirun='mpirun --oversubscribe' # --silent
+python3 run_tests.py grav/jeans_3d --config=--cxx=$TEMP_CXX --mpirun='mpirun --oversubscribe' # requires FFTW library
+
 # python3 run_tests.py grav/unstable_jeans_3d_fft grav/unstable_jeans_3d_mg --config=--cxx=$TEMP_CXX --silent # requires FFTW library
 # python3 run_tests.py amr --config=--cxx=$TEMP_CXX --silent
 
