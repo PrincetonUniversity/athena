a5401e2add961c92c4850da8faf97b0e7ed8eaaa
Kyle Gerard Felker <kfelker@princeton.edu>
2018-06-07 12:54:32 -0400

    Re-enable most regression tests on Travis CI

--- tst/ci/travis/run_tests_travis.sh
+++ tst/ci/travis/run_tests_travis.sh
@@ -7,7 +7,7 @@ set -ev
 # - Script fails after first broken set of tests
 # (Could alternatively group sets of tests with && operator)
 cd regression/
-# python3 run_tests.py pgen --config=--cxx=$TEMP_CXX --config=--cflag="$(./ci/set_warning_cflag.sh $TEMP_CXX)"
+
 if [ "$MPI_CHOICE" == "openmpi" ]; then
     PATH=$TRAVIS_BUILD_DIR/openmpi/bin/:$PATH
     MPI_OPTS=--oversubscribe
@@ -17,19 +17,20 @@ else
     PATH=$TRAVIS_BUILD_DIR/mpich/bin/:$PATH
 fi
 
-python3 run_tests.py mpi --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS # --silent
-python3 run_tests.py grav/jeans_3d --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS # requires FFTW library
+python3 run_tests.py pgen --config=--cxx=$TEMP_CXX --config=--cflag="$(./ci/set_warning_cflag.sh $TEMP_CXX)"
+python3 run_tests.py mpi --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS --silent
+python3 run_tests.py grav/unstable_jeans_3d_fft grav/unstable_jeans_3d_mg --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS # requires FFTW library
+python3 run_tests.py amr --config=--cxx=$TEMP_CXX --silent
+python3 run_tests.py hydro --config=--cxx=$TEMP_CXX --silent
+python3 run_tests.py outputs --config=--cxx=$TEMP_CXX --silent
+python3 run_tests.py curvilinear --config=--cxx=$TEMP_CXX --silent
+python3 run_tests.py gr --config=--cxx=$TEMP_CXX --silent
+python3 run_tests.py sr --config=--cxx=$TEMP_CXX --silent
+python3 run_tests.py shearingbox --config=--cxx=$TEMP_CXX --silent
+python3 run_tests.py diffusion --config=--cxx=$TEMP_CXX --silent
 
-# python3 run_tests.py grav/unstable_jeans_3d_fft grav/unstable_jeans_3d_mg --config=--cxx=$TEMP_CXX --silent # requires FFTW library
-# python3 run_tests.py amr --config=--cxx=$TEMP_CXX --silent
+# Test has memory error on Travis CI builds with MPICH (usually with clang++)
+# python3 run_tests.py grav/jeans_3d --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS # requires FFTW library
 
-# mhd/ contains the longest set of tests. Timeout after 10 m on Travis CI
-# python3 run_tests.py mhd
-
-# python3 run_tests.py hydro --config=--cxx=$TEMP_CXX --silent
-# python3 run_tests.py outputs --config=--cxx=$TEMP_CXX --silent
-# python3 run_tests.py curvilinear --config=--cxx=$TEMP_CXX --silent
-# python3 run_tests.py gr --config=--cxx=$TEMP_CXX --silent
-# python3 run_tests.py sr --config=--cxx=$TEMP_CXX --silent
-# python3 run_tests.py shearingbox --config=--cxx=$TEMP_CXX --silent
-# python3 run_tests.py diffusion --config=--cxx=$TEMP_CXX --silent
+# mhd/ currently contains the longest set of tests. Command times-out after 10 m on Travis CI
+# python3 run_tests.py mhd --config=--cxx=$TEMP_CXX --silent
