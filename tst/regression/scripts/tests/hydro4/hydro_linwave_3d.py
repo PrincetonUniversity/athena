26dd1523c90d2ff9d88263746a1b1a8837aab15b
Kyle Gerard Felker <kfelker@princeton.edu>
2018-06-20 15:22:04 -0400

    Add convergence test for 3D hydro linear wave for 4th order algorithm
    Currently, not applying midpoint IC or error correction

--- tst/regression/scripts/tests/hydro4/hydro_linwave_3d.py
+++ tst/regression/scripts/tests/hydro4/hydro_linwave_3d.py
@@ -1,9 +1,7 @@
 # Regression test based on Newtonian hydro linear wave convergence problem
 
-# Check errors of reconstruction and time integrator options other than default VL2+PLM
-# primitive reconstruction. In particular, confirm fourth-order convergence rate for
-# semidiscrete integration with RK4 + PPM + Laplacian flux correction terms. 2D uniform
-# square grid, no SMR.
+# Confirm fourth-order convergence rate for semidiscrete integration with RK4 + PPM (char.
+# and primitive) + Laplacian flux correction terms. 3D uniform square grid, no SMR.
 
 # Modules
 import scripts.utils.athena as athena
@@ -11,25 +9,19 @@ from math import log
 import numpy as np
 
 # List of time/integrator and time/xorder combinations to test:
-solvers = [('vl2', '2c'), ('vl2', '3'), ('rk2', '3c'),
-           ('rk3', '4'), ('rk4', '4c'), ('ssprk5_4', '4')]
+solvers = [('rk4', '4c'), ('ssprk5_4', '4')]
 # Matching above list of solver configurations, provide bounds on error metrics:
-# for each tested resolution (excluding lowest Nx1=16) and wave_flag
+# for each tested resolution (excluding lowest Nx1=16) and wave_flag.
 # Upper bound on RMS-L1 errors:
-error_tols = [((1.4e-7, 4.6e-8, 1.1e-8, 2.5e-9), (1.1e-7, 3.7e-8, 9.3e-9, 2.2e-9)),
-              ((9.6e-8, 2.4e-8, 5.8e-9, 1.5e-9), (4.5e-8, 1.1e-8, 2.6e-9, 6.4e-10)),
-              ((3.7e-8, 1.1e-8, 2.7e-9, 6.7e-10), (4.8e-9, 2.0e-9, 5.3e-10, 1.4e-10)),
-              ((5.5e-9, 4.0e-10, 3.6e-11, 6.2e-12), (3.7e-9, 2.5e-10, 1.6e-11, 1.1e-12)),
-              ((5.2e-9, 3.4e-10, 2.2e-11, 5.6e-12), (3.8e-9, 2.4e-10, 1.6e-11, 1.7e-12)),
-              ((5.2e-9, 3.4e-10, 2.1e-11, 5.6e-12), (3.8e-9, 2.4e-10, 1.6e-11, 1.1e-12))
+error_tols = [((5.7e-9, 3.7e-10), (4.1e-9, 2.7e-10)),
+              ((5.7e-9, 3.7e-10), (4.1e-9, 2.7e-10))
               ]
-# for each wave_flag, lower bound on convergence rate at Nx1=128 asymptotic convergence
+# for each wave_flag, lower bound on convergence rate at 64x32x32 asymptotic convergence
 # regime. Linear hydro waves stop converging around RMS-L1 error 1e-11 to 1e-12
-rate_tols = [(2.0, 1.9), (2.0, 2.0), (1.95, 1.85),
-             (3.4, 3.95), (3.95, 3.95),  (3.95, 3.95)]
+rate_tols = [(3.95, 3.94),  (3.95, 3.94)]
 # this metric is redundant with above error_tols, but it is simpler...
 
-resolution_range = [16, 32, 64, 128, 256]  # , 512]
+resolution_range = [16, 32, 64]
 num_nx1 = len(resolution_range)
 # Number of times Athena++ is run for each above configuration:
 nrows_per_solver = 2*num_nx1 + 2
@@ -53,18 +45,24 @@ def run(**kwargs):
                          'time/xorder=' + xorder, 'time/integrator=' + torder,
                          'problem/wave_flag=0', 'problem/vflow=0.0',
                          'mesh/nx1=' + repr(i), 'mesh/nx2=' + repr(i/2),
+                         'mesh/nx3=' + repr(i/2),
+                         'meshblock/nx1=' + repr(i), 'meshblock/nx2=' + repr(i/2),
+                         'meshblock/nx3=' + repr(i/2),
                          'output2/dt=-1', 'time/tlim=1.0',
                          'problem/compute_error=true']
-            athena.run('hydro/athinput.linear_wave2d', arguments)
+            athena.run('hydro/athinput.linear_wave3d', arguments)
         # L-going entropy wave
         for i in resolution_range:
             arguments = ['time/ncycle_out=0',
                          'time/xorder=' + xorder, 'time/integrator=' + torder,
                          'problem/wave_flag=3', 'problem/vflow=1.0',
                          'mesh/nx1=' + repr(i), 'mesh/nx2=' + repr(i/2),
+                         'mesh/nx3=' + repr(i/2),
+                         'meshblock/nx1=' + repr(i), 'meshblock/nx2=' + repr(i/2),
+                         'meshblock/nx3=' + repr(i/2),
                          'output2/dt=-1', 'time/tlim=1.0',
                          'problem/compute_error=true']
-            athena.run('hydro/athinput.linear_wave2d', arguments)
+            athena.run('hydro/athinput.linear_wave3d', arguments)
         # L/R-going sound wave, no SMR
         for w in (0, 4):
             arguments = ['time/ncycle_out=0',
@@ -72,7 +70,7 @@ def run(**kwargs):
                          'problem/wave_flag=' + repr(w),
                          'output2/dt=-1', 'time/tlim=1.0',
                          'problem/compute_error=true']
-            athena.run('hydro/athinput.linear_wave2d', arguments)
+            athena.run('hydro/athinput.linear_wave3d', arguments)
 
 
 # Analyze outputs
@@ -133,22 +131,17 @@ def analyze():
                     rms_errs[i], err_tol[1][i-1]))
                 return False
 
-        # print(solver_results[-2, 4] - solver_results[-1, 4])
-        # print(solver_results[-2, 4], solver_results[-1, 4])
+        print(solver_results[-2, 4] - solver_results[-1, 4])
+        print(solver_results[-2, 4], solver_results[-1, 4])
         # print("numpy bound = {}".format(5e-16 + 1e-5*abs(solver_results[-1, 4])))
 
-        # Check that errors are identical for sound waves in each direction at Nx1=256
+        # Check that errors are identical for sound waves in each direction at default
+        # 64x32x32 resolution
         if (not np.allclose(solver_results[-2, 4], solver_results[-1, 4],
-                            atol=5e-16, rtol=1e-5) and xorder != '3c'):
+                            atol=5e-16, rtol=1e-5)):
             print(("L/R-going sound wave errors, {} and {}".format(solver_results[-2, 4],
                                                                    solver_results[-1, 4]),
                    ", have a difference that is not close to round-off"))
             return False
-        # Unlike comparison for VL2+PLM, the errors become small enough in the high-order
-        # solver that round-off needs to be accounted for. Assuming true solution A is
-        # O(1), then the difference betwee L/R-going RMS-L1 errors approach:
-        # Diff = sum(A - sol1) - sum(A - sol2) ---> 1 + epsilon
 
-        # Currently, RK2 + 3c produces the largest directional asymmetry w/
-        # diff=-2.22e-14. Characteristic projection procedure may be introducing asymmetry
     return True
