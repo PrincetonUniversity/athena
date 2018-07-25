21b0c54b83c5774d18bb4598e7563db7101e613d
Alwin <alwin@princeton.edu>
2018-07-24 22:15:58 -0400

    add self gravity to from_array

--- src/pgen/from_array.cpp
+++ src/pgen/from_array.cpp
@@ -32,6 +32,17 @@
 //     MeshBlock/nx2
 //     MeshBlock/nx1
 
+void Mesh::InitUserMeshData(ParameterInput *pin) {
+  if (SELF_GRAVITY_ENABLED) {
+    Real four_pi_G = pin->GetReal("problem","four_pi_G");
+    Real eps = pin->GetOrAddReal("problem","grav_eps", 0.0);
+    Real d0 = pin->GetReal("problem","d0");
+    SetFourPiG(four_pi_G);
+    SetGravityThreshold(eps);
+    SetMeanDensity(d0);
+  }
+}
+
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 
   // Determine locations of initial values
