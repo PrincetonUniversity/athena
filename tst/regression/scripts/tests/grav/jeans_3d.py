b95518ed32cce538cd93aa5b5494dd18246f22e2
Kyle Gerard Felker <kfelker@princeton.edu>
2018-07-26 12:26:29 -0400

    Delete Multigrid from grav/ regression tests
    - Delete Multigrid version of unstable Jeans regression tst
    - Remove Multigrid tests (MPI and serial) from Jeans 3D regression test

--- tst/regression/scripts/tests/grav/jeans_3d.py
+++ tst/regression/scripts/tests/grav/jeans_3d.py
@@ -17,11 +17,11 @@ def prepare(**kwargs):
     athena.make()
     os.system('mv bin/athena bin/athena_mpi_fft')
 
-    athena.configure('mpi',
-                     prob='jeans',
-                     grav='mg', **kwargs)
-    athena.make()
-    os.system('mv bin/athena bin/athena_mpi_mg')
+    # athena.configure('mpi',
+    #                  prob='jeans',
+    #                  grav='mg', **kwargs)
+    # athena.make()
+    # os.system('mv bin/athena bin/athena_mpi_mg')
 
     athena.configure('fft',
                      prob='jeans',
@@ -30,10 +30,10 @@ def prepare(**kwargs):
     athena.make()
     os.system('mv bin/athena bin/athena_fft')
 
-    athena.configure(prob='jeans',
-                     grav='mg',
-                     **kwargs)
-    athena.make()
+    # athena.configure(prob='jeans',
+    #                  grav='mg',
+    #                  **kwargs)
+    # athena.make()
 
 
 # Run Athena++
@@ -44,18 +44,18 @@ def run(**kwargs):
                  'meshblock/nx2=16',
                  'meshblock/nx3=16',
                  'output2/dt=-1', 'time/tlim=1.0', 'problem/compute_error=true']
-    athena.run('hydro/athinput.jeans_3d', arguments)
+    # athena.run('hydro/athinput.jeans_3d', arguments)
 
     os.system('mv bin/athena_fft bin/athena')
     athena.run('hydro/athinput.jeans_3d', arguments)
 
-    os.system('mv bin/athena_mpi_mg bin/athena')
-    athena.mpirun(kwargs['mpirun_cmd'], kwargs['mpirun_opts'],
-                  1, 'hydro/athinput.jeans_3d', arguments)
-    athena.mpirun(kwargs['mpirun_cmd'], kwargs['mpirun_opts'],
-                  2, 'hydro/athinput.jeans_3d', arguments)
-    athena.mpirun(kwargs['mpirun_cmd'], kwargs['mpirun_opts'],
-                  4, 'hydro/athinput.jeans_3d', arguments)
+    # os.system('mv bin/athena_mpi_mg bin/athena')
+    # athena.mpirun(kwargs['mpirun_cmd'], kwargs['mpirun_opts'],
+    #               1, 'hydro/athinput.jeans_3d', arguments)
+    # athena.mpirun(kwargs['mpirun_cmd'], kwargs['mpirun_opts'],
+    #               2, 'hydro/athinput.jeans_3d', arguments)
+    # athena.mpirun(kwargs['mpirun_cmd'], kwargs['mpirun_opts'],
+    #               4, 'hydro/athinput.jeans_3d', arguments)
 
     os.system('mv bin/athena_mpi_fft bin/athena')
     athena.mpirun(kwargs['mpirun_cmd'], kwargs['mpirun_opts'],
@@ -79,51 +79,51 @@ def analyze():
             data.append([float(val) for val in line.split()])
 
     print(data[0][4], data[1][4])
-    print(data[2][4], data[3][4], data[4][4])
-    print(data[5][4], data[6][4], data[7][4])
+    print(data[2][4], data[3][4])  # , data[4][4])
+    # print(data[5][4], data[6][4], data[7][4])
 
     # check errors between runs w/wo MPI and different numbers of cores
-    if data[0][4] != data[2][4]:
-        print(
-            "Linear wave error with one core w/wo MPI not identical for MG gravity",
-            data[0][4],
-            data[2][4])
-        return False
+    # if data[0][4] != data[2][4]:
+    #     print(
+    #         "Linear wave error with one core w/wo MPI not identical for MG gravity",
+    #         data[0][4],
+    #         data[2][4])
+    #     return False
+    # if data[0][4] > 1.e-7:
+    #     print("Linear wave error is too large for MG gravity", data[0][4])
+    #     return False
     if data[0][4] > 1.e-7:
-        print("Linear wave error is too large for MG gravity", data[0][4])
+        print("Linear wave error is too large for FFT gravity", data[0][4])
         return False
-    if data[1][4] > 1.e-7:
-        print("Linear wave error is too large for FFT gravity", data[1][4])
-        return False
-    if data[1][4] != data[5][4]:
+    if data[0][4] != data[1][4]:
         print(
             "Linear wave error with one core w/wo MPI not identical for FFT gravity",
-            data[1][4],
-            data[5][4])
+            data[0][4],
+            data[1][4])
+        return False
+    # if abs(data[3][4]-data[0][4]) > 5.0e-4:
+    #     print(
+    #         "Linear wave error between 2 and 1 cores too large for MG gravity",
+    #         data[3][4],
+    #         data[0][4])
+    #     return False
+    # if abs(data[4][4]-data[0][4]) > 5.0e-4:
+    #     print(
+    #         "Linear wave error between 4 and 1 cores too large for MG gravity",
+    #         data[4][4],
+    #         data[0][4])
+    #     return False
+    if abs(data[2][4]-data[0][4]) > 5.0e-4:
+        print(
+            "Linear wave error between 2 and 1 cores too large for FFT gravity",
+            data[2][4],
+            data[0][4])
         return False
     if abs(data[3][4]-data[0][4]) > 5.0e-4:
         print(
-            "Linear wave error between 2 and 1 cores too large for MG gravity",
+            "Linear wave error between 4 and 1 cores too large for FFT gravity",
             data[3][4],
             data[0][4])
-        return False
-    if abs(data[4][4]-data[0][4]) > 5.0e-4:
-        print(
-            "Linear wave error between 4 and 1 cores too large for MG gravity",
-            data[4][4],
-            data[0][4])
-        return False
-    if abs(data[6][4]-data[1][4]) > 5.0e-4:
-        print(
-            "Linear wave error between 2 and 1 cores too large for FFT gravity",
-            data[6][4],
-            data[1][4])
-        return False
-    if abs(data[7][4]-data[1][4]) > 5.0e-4:
-        print(
-            "Linear wave error between 4 and 1 cores too large for FFT gravity",
-            data[7][4],
-            data[1][4])
         return False
 
     return True
