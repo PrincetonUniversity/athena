dac977aff9066bcab740817cdb12fb5878bbc87c
Kyle Gerard Felker <kfelker@princeton.edu>
2018-09-15 17:27:54 -0400

    Use time utility on command line to measure Travis CI regression tsts

--- tst/ci/travis/run_tests_travis.sh
+++ tst/ci/travis/run_tests_travis.sh
@@ -3,7 +3,7 @@
 # Terminate script at first cmd w/ non-zero exit status & echo commands before executing (-v) as lines are read
 set -ev # -x # for tracing/debugging commands after variable expansion
 # Do not use "python run_tests.py" to run all tests, so that:
-# - Each set/directory of tests are timed separately
+# - Each set/directory of tests are timed separately (although entire script is timed as one unit in Travis CI)
 # - Script fails after first broken set of tests
 # (Could alternatively group sets of tests with && operator)
 cd regression/
@@ -18,19 +18,19 @@ else
 fi
 
 # --silent option refers only to stdout of Makefile calls for condensed build logs. Don't use with pgen_compile.py
-python3 run_tests.py pgen --config=--cxx=$TEMP_CXX --config=--cflag="$(../ci/set_warning_cflag.sh $TEMP_CXX)"
-python3 run_tests.py mpi --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS --silent
-python3 run_tests.py grav --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS --silent # requires FFTW library
-python3 run_tests.py amr --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py hydro --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py outputs --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py curvilinear --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py gr --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py sr --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py shearingbox --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py diffusion --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py hydro4 --config=--cxx=$TEMP_CXX --silent
-python3 run_tests.py symmetry --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py pgen --config=--cxx=$TEMP_CXX --config=--cflag="$(../ci/set_warning_cflag.sh $TEMP_CXX)"
+time python3 run_tests.py mpi --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS --silent
+time python3 run_tests.py grav --config=--cxx=$TEMP_CXX --mpirun_opts=$MPI_OPTS --silent # requires FFTW library
+time python3 run_tests.py amr --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py hydro --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py outputs --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py curvilinear --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py gr --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py sr --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py shearingbox --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py diffusion --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py hydro4 --config=--cxx=$TEMP_CXX --silent
+time python3 run_tests.py symmetry --config=--cxx=$TEMP_CXX --silent
 
-# mhd/ currently contains the longest set of tests. Command times-out after 10 m on Travis CI
-# python3 run_tests.py mhd --config=--cxx=$TEMP_CXX --silent
+# mhd/ currently contains the longest set of tests. The following command often times-out after 10 m on Travis CI
+# time python3 run_tests.py mhd --config=--cxx=$TEMP_CXX --silent
