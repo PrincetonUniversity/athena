0d6560cd369c2a259847161b921518a8c0a49125
Kyle Gerard Felker <kfelker@princeton.edu>
2018-10-30 23:55:11 -0400

    Revert back to 17.0 R5 Intel libraries on Perseus for Jenkins
    intel-mpi loads 2018 R3, unlike on Tiger 2.
    
    intel 19.0 may be misconfigured on Perseus. These modules (with
    intel-mpi 19) worked on Tiger 2.
    
    Also delete temporary duplicate omp/, hybrid/ tests on Jenkins

--- tst/ci/jenkins/run_jenkins_perseus.sh
+++ tst/ci/jenkins/run_jenkins_perseus.sh
@@ -68,16 +68,13 @@ time python ./run_tests.py pgen/hdf5_reader_parallel --mpirun=srun --config=--li
 # Build step #2: regression tests using Intel compiler and MPI library
 module purge
 # automatically use latest default version of these libraries as Princeton Research Computing updates them:
-module load intel # intel/17.0/64/17.0.5.239
-module load intel-mpi # intel-mpi/intel/2017.5/64
+module load intel/17.0/64/17.0.5.239 # intel ---intel/19.0/64/19.0.0.117
+module load intel-mpi/intel/2017.5/64 # intel-mpi --- intel-mpi/intel/2018.3/64
 # pinning these modules to a specific version, since new library versions are rarely compiled:
 module load fftw/gcc/3.3.4
 module load hdf5/intel-17.0/1.10.0 # hdf5/intel-17.0/intel-mpi/1.10.0
 # do not mix w/ "module load rh" to ensure that Intel shared libraries are used by the loader (especially OpenMP?)
 module list
-# temp:
-time python ./run_tests.py omp --config=--cxx=icc --silent
-time python ./run_tests.py hybrid --config=--cxx=icc --mpirun=srun --silent
 
 time python ./run_tests.py pgen/pgen_compile --config=--cxx=icc --config=--cflag="$(../ci/set_warning_cflag.sh icc)"
 time python ./run_tests.py pgen/hdf5_reader_serial --silent
