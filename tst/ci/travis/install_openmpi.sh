097245706dd485e4f51ad1a6b4d485a809509000
Kyle Gerard Felker <kfelker@princeton.edu>
2018-11-09 10:47:36 -0500

    No longer need to "brew update" to fix GCC specifically for MPICH
    Travis CI Homebrew repository is installed and updated every time the
    macOS VM images are updated:
    https://docs.travis-ci.com/user/reference/osx/#homebrew
    
    "brew update" is slow and dumps the names of many irrelevant updated
    packages to the Travis CI build log. Was only used in MPICH install
    script due to broken gfortran dependencies in older gcc formula on
    Travis CI image
    
    Redirect command output to /dev/null if ever uncommented in the future;
    see: https://github.com/Homebrew/legacy-homebrew/issues/35662

--- tst/ci/travis/install_openmpi.sh
+++ tst/ci/travis/install_openmpi.sh
@@ -9,9 +9,9 @@ if [ "$TRAVIS_OS_NAME" == "osx" ]; then
     cd openmpi
     brew unlink mpich || true
     # OpenMPI and dependencies:
-    # depending on macOS image, may need to install Xcode CLI (workaround for "missing stdio.h" header):
+    # Depending on macOS image, may need to install Xcode CLI (workaround for "missing stdio.h" header):
     #sudo softwareupdate -i "Command Line Tools (macOS High Sierra version 10.13) for Xcode-9.4"
-    # always install dependencies from pre-compiled bottles before attempting to build-from-source
+    # Always install dependencies from pre-compiled bottles before attempting to build-from-source:
     brew install gcc
     brew link libevent || true
     brew install libevent || true
