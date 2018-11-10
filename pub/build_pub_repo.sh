529b0d8102b61a266b6c03c43574ef37b2edd018
Kyle Gerard Felker <kfelker@princeton.edu>
2018-11-09 20:38:44 -0500

    For all Bash scripts: use lower-snake-case variable names

--- pub/build_pub_repo.sh
+++ pub/build_pub_repo.sh
@@ -6,34 +6,34 @@
 # the filename needs to include the path with respect
 # to the top of the Git repo
 # Here is an example for excluding multiple directories
-# PRIVATE="{pub,private,inputs}"
+# private_files="{pub,private,inputs}"
 # Note: for a single directory, you should not use the curly
 # brackets e.g.
-# incorrect: PRIVATE="{pub}"
-#   correct: PRIVATE="pub"
-PRIVATE="{pub,tst/ci,.travis.yml,.github,CONTRIBUTING.md,CHANGELOG.md}"
+# incorrect: private_files="{pub}"
+#   correct: private_files="pub"
+private_files="{pub,tst/ci,.travis.yml,.github,CONTRIBUTING.md,CHANGELOG.md}"
 
 # Repo HTTPS URLs
-PRIV_REPO_URL="https://github.com/PrincetonUniversity/athena.git"
-PUB_REPO_URL="https://github.com/PrincetonUniversity/athena-public-version.git"
+priv_repo_url="https://github.com/PrincetonUniversity/athena.git"
+pub_repo_url="https://github.com/PrincetonUniversity/athena-public-version.git"
 # Repo SSH URLs:
-# PRIV_REPO_URL="git@github.com:PrincetonUniversity/athena.git"
-# PUB_REPO_URL="git@github.com:PrincetonUniversity/athena-public-version.git"
+# priv_repo_url="git@github.com:PrincetonUniversity/athena.git"
+# pub_repo_url="git@github.com:PrincetonUniversity/athena-public-version.git"
 
 # data for this script: assumes we are rebasing private repo "master" on "last_public" tag
-PRIV_REPO_PUB_BRANCH="public"
+priv_repo_pub_branch="public"
 
-PUB_REMOTE_NAME="public_repo"
-PUB_REPO_BRANCH="master"
+pub_remote_name="public_repo"
+pub_repo_branch="master"
 
-PRE_PUSH_HOOK="pub/pre-push"
-POST_COMMIT_HOOK="pub/post-commit"
+pre_push_hook="pub/pre-push"
+post_commit_hook="pub/post-commit"
 
-LOCAL_REPO_NAME=$1
+local_repo_name=$1
 
 # clone private repo
-git clone $PRIV_REPO_URL $LOCAL_REPO_NAME
-cd $LOCAL_REPO_NAME
+git clone $priv_repo_url $local_repo_name
+cd $local_repo_name
 git pull --tags
 
 # Move the tag last_public
@@ -67,9 +67,9 @@ git tag last_public last_public_old
 git tag -d last_public_old
 
 # Copy the pre-push hook to the cloned repo
-if [ -e $PRE_PUSH_HOOK ]
+if [ -e $pre_push_hook ]
 then
-    cp $PRE_PUSH_HOOK .git/hooks/
+    cp $pre_push_hook .git/hooks/
     chmod 766 .git/hooks/pre-push
 else
     echo the pre-push hook does not exists
@@ -78,21 +78,21 @@ fi
 
 # clean up the files and directories we don't want to make public
 git filter-branch --force --index-filter \
-    "git rm -r --cached --ignore-unmatch $PRIVATE" \
+    "git rm -r --cached --ignore-unmatch $private_files" \
     --prune-empty --tag-name-filter cat -- --all
 
 # rebase to eliminate merge commit since last_public
 git rebase last_public
 
 # add the public_repo remote
-git remote add $PUB_REMOTE_NAME $PUB_REPO_URL
+git remote add $pub_remote_name $pub_repo_url
 git fetch --all
 
 # Create the public branch by pulling the master branch of
 # the public repo. The public branch of the private repo is
 # now tracking the master branch of the public repo so that
 # "git push" and "git pull" will be towards public_repo:master
-git checkout -b $PRIV_REPO_PUB_BRANCH $PUB_REMOTE_NAME/$PUB_REPO_BRANCH
+git checkout -b $priv_repo_pub_branch $pub_remote_name/$pub_repo_branch
 
 # configure git so it pushes from the public branch to public_repo:master
 git config push.default upstream
