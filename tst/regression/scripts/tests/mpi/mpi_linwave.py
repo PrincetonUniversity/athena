f327471604a3646e898d2be7147ac88c096fd64b
Kyle Gerard Felker <kfelker@princeton.edu>
2018-11-13 13:54:12 -0500

    For all mpi/, omp/, hybrid/ tests, conform to new Lcov framework

--- tst/regression/scripts/tests/mpi/mpi_linwave.py
+++ tst/regression/scripts/tests/mpi/mpi_linwave.py
@@ -15,6 +15,7 @@ def prepare(**kwargs):
                      flux='hlld', **kwargs)
     athena.make()
     os.system('mv bin/athena bin/athena_mpi')
+    os.system('mv obj obj_mpi')
 
     athena.configure('b', prob='linear_wave', coord='cartesian', flux='hlld', **kwargs)
     athena.make()
@@ -30,8 +31,10 @@ def run(**kwargs):
                  'meshblock/nx2=8',
                  'meshblock/nx3=8',
                  'output2/dt=-1', 'time/tlim=2.0', 'problem/compute_error=true']
-    athena.run('mhd/athinput.linear_wave3d', arguments)
+    athena.run('mhd/athinput.linear_wave3d', arguments, lcov_test_suffix='serial')
 
+    os.system('rm -rf obj')
+    os.system('mv obj_mpi obj')
     os.system('mv bin/athena_mpi bin/athena')
     athena.mpirun(kwargs['mpirun_cmd'], kwargs['mpirun_opts'], 1,
                   'mhd/athinput.linear_wave3d', arguments)
