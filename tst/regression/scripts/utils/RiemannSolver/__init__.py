8dd37fcfed8ef1d7bdeac869b3d15dbf6114e07f
Matt Coleman <mcoleman@ias.edu>
2019-01-01 17:50:31 -0500

    Removed commented code

--- tst/regression/scripts/utils/RiemannSolver/__init__.py
+++ tst/regression/scripts/utils/RiemannSolver/__init__.py
@@ -1,6 +1,2 @@
-#from numpy import finfo
-
-
 brent_opt = dict(full_output=True, disp=True)
-# , rtol=2 * finfo(float).eps, atol=2 * finfo(float).eps)
 ode_opt = dict(mxstep=5000000, rtol=1e-15, atol=1e-13)
