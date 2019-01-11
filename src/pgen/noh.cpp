d831ecc0defe82b01a210f1a99c6c02eca8c181d
Kyle Gerard Felker <kfelker@princeton.edu>
2019-01-11 15:38:04 -0500

    Use local il, iu, ... index variable names instead of is, ie ...
    Fix indentation

--- src/pgen/noh.cpp
+++ src/pgen/noh.cpp
@@ -32,11 +32,14 @@
 
 // BCs on outer edges of grid in each dimension
 void Noh3DOuterX1(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim, FaceField &b,
-       Real time, Real dt, int is, int ie, int js, int je, int ks, int ke, int ngh);
+                  Real time, Real dt,
+                  int il, int iu, int jl, int ju, int kl, int ku, int ngh);
 void Noh3DOuterX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim, FaceField &b,
-       Real time, Real dt, int is, int ie, int js, int je, int ks, int ke, int ngh);
+                  Real time, Real dt,
+                  int il, int iu, int jl, int ju, int kl, int ku, int ngh);
 void Noh3DOuterX3(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim, FaceField &b,
-       Real time, Real dt, int is, int ie, int js, int je, int ks, int ke, int ngh);
+                  Real time, Real dt,
+                  int il, int iu, int jl, int ju, int kl, int ku, int ngh);
 
 // made global to share with BC functions
 static Real gmma, gmma1;
@@ -69,23 +72,24 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 
   // Initialize the grid: d=1, v=-1.0 in radial direction, p=10^-6
   for (int k=ks; k<=ke; k++) {
-  for (int j=js; j<=je; j++) {
-  for (int i=is; i<=ie; i++) {
-    Real rad;
-    if (block_size.nx3 > 1) {
-      rad = std::sqrt(SQR(pcoord->x1v(i)) + SQR(pcoord->x2v(j)) + SQR(pcoord->x3v(k)));
-      phydro->u(IM3,k,j,i) = -pcoord->x3v(k)/rad;
-    } else {
-      rad = std::sqrt(SQR(pcoord->x1v(i)) + SQR(pcoord->x2v(j)));
-      phydro->u(IM3,k,j,i) = 0.0;
+    for (int j=js; j<=je; j++) {
+      for (int i=is; i<=ie; i++) {
+        Real rad;
+        if (block_size.nx3 > 1) {
+          rad = std::sqrt(SQR(pcoord->x1v(i)) + SQR(pcoord->x2v(j))
+                          + SQR(pcoord->x3v(k)));
+          phydro->u(IM3,k,j,i) = -pcoord->x3v(k)/rad;
+        } else {
+          rad = std::sqrt(SQR(pcoord->x1v(i)) + SQR(pcoord->x2v(j)));
+          phydro->u(IM3,k,j,i) = 0.0;
+        }
+        phydro->u(IDN,k,j,i) = 1.0;
+        phydro->u(IM1,k,j,i) = -pcoord->x1v(i)/rad;
+        phydro->u(IM2,k,j,i) = -pcoord->x2v(j)/rad;
+        phydro->u(IEN,k,j,i) = 1.0e-6/gmma1 + 0.5;
+      }
     }
-    phydro->u(IDN,k,j,i) = 1.0;
-    phydro->u(IM1,k,j,i) = -pcoord->x1v(i)/rad;
-    phydro->u(IM2,k,j,i) = -pcoord->x2v(j)/rad;
-    phydro->u(IEN,k,j,i) = 1.0e-6/gmma1 + 0.5;
   }
-}
-}
 }
 
 
@@ -96,34 +100,35 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 // Quantities at this boundary are held fixed at the time-dependent upstream state
 
 void Noh3DOuterX1(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim, FaceField &b,
-       Real time, Real dt, int is, int ie, int js, int je, int ks, int ke, int ngh) {
-  for (int k=ks; k<=ke; ++k) {
-  for (int j=js; j<=je; ++j) {
-    for (int i=1;  i<=ngh; ++i) {
-      Real rad,f_t;
-      if (pmb->block_size.nx3 > 1) {
-        rad = std::sqrt(SQR(pco->x1v(ie+i)) + SQR(pco->x2v(j))
-              + SQR(pco->x3v(k)));
-        f_t = SQR(1.0 + pmb->pmy_mesh->time/rad);
-      } else {
-        rad = std::sqrt(SQR(pco->x1v(ie+i)) + SQR(pco->x2v(j)));
-        f_t = (1.0 + pmb->pmy_mesh->time/rad);
-      }
-      Real d0 = 1.0*f_t;
+                  Real time, Real dt,
+                  int il, int iu, int jl, int ju, int kl, int ku, int ngh) {
+  for (int k=kl; k<=ku; ++k) {
+    for (int j=jl; j<=ju; ++j) {
+      for (int i=1;  i<=ngh; ++i) {
+        Real rad,f_t;
+        if (pmb->block_size.nx3 > 1) {
+          rad = std::sqrt(SQR(pco->x1v(iu+i)) + SQR(pco->x2v(j))
+                          + SQR(pco->x3v(k)));
+          f_t = SQR(1.0 + pmb->pmy_mesh->time/rad);
+        } else {
+          rad = std::sqrt(SQR(pco->x1v(iu+i)) + SQR(pco->x2v(j)));
+          f_t = (1.0 + pmb->pmy_mesh->time/rad);
+        }
+        Real d0 = 1.0*f_t;
 
-      prim(IDN,k,j,ie+i)  = d0;
-      prim(IVX,k,j,ie+i) = -pco->x1v(ie+i)/rad;
-      prim(IVY,k,j,ie+i) = -pco->x2v(j   )/rad;
-      if (pmb->block_size.nx3 > 1) {
-        prim(IVZ,k,j,ie+i) = -pco->x3v(k)/rad;
-        prim(IPR,k,j,ie+i) = 1.0e-6*std::pow(f_t,(1.0+gmma));
-      } else {
-        prim(IVZ,k,j,ie+i) = 0.0;
-        prim(IPR,k,j,ie+i)= 1.0e-6;
+        prim(IDN,k,j,iu+i)  = d0;
+        prim(IVX,k,j,iu+i) = -pco->x1v(iu+i)/rad;
+        prim(IVY,k,j,iu+i) = -pco->x2v(j   )/rad;
+        if (pmb->block_size.nx3 > 1) {
+          prim(IVZ,k,j,iu+i) = -pco->x3v(k)/rad;
+          prim(IPR,k,j,iu+i) = 1.0e-6*std::pow(f_t,(1.0+gmma));
+        } else {
+          prim(IVZ,k,j,iu+i) = 0.0;
+          prim(IPR,k,j,iu+i)= 1.0e-6;
+        }
       }
     }
   }
-}
 }
 
 //----------------------------------------------------------------------------------------
@@ -133,34 +138,35 @@ void Noh3DOuterX1(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim, Fac
 // Quantities at this boundary are held fixed at the time-dependent upstream state
 
 void Noh3DOuterX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim, FaceField &b,
-       Real time, Real dt, int is, int ie, int js, int je, int ks, int ke, int ngh) {
-  for (int k=ks; k<=ke; ++k) {
-  for (int j=1; j<=ngh; ++j) {
-    for (int i=is; i<=ie; ++i) {
-      Real rad,f_t;
-      if (pmb->block_size.nx3 > 1) {
-        rad = std::sqrt(SQR(pco->x1v(i)) + SQR(pco->x2v(je+j))
-              + SQR(pco->x3v(k)));
-        f_t = SQR(1.0 + pmb->pmy_mesh->time/rad);
-      } else {
-        rad = std::sqrt(SQR(pco->x1v(i)) + SQR(pco->x2v(je+j)));
-        f_t = (1.0 + pmb->pmy_mesh->time/rad);
-      }
-      Real d0 = 1.0*f_t;
+                  Real time, Real dt,
+                  int il, int iu, int jl, int ju, int kl, int ku, int ngh) {
+  for (int k=kl; k<=ku; ++k) {
+    for (int j=1; j<=ngh; ++j) {
+      for (int i=il; i<=iu; ++i) {
+        Real rad,f_t;
+        if (pmb->block_size.nx3 > 1) {
+          rad = std::sqrt(SQR(pco->x1v(i)) + SQR(pco->x2v(ju+j))
+                          + SQR(pco->x3v(k)));
+          f_t = SQR(1.0 + pmb->pmy_mesh->time/rad);
+        } else {
+          rad = std::sqrt(SQR(pco->x1v(i)) + SQR(pco->x2v(ju+j)));
+          f_t = (1.0 + pmb->pmy_mesh->time/rad);
+        }
+        Real d0 = 1.0*f_t;
 
-      prim(IDN,k,je+j,i)  = d0;
-      prim(IVX,k,je+j,i) = -pco->x1v(i)/rad;
-      prim(IVY,k,je+j,i) = -pco->x2v(je+j)/rad;
-      if (pmb->block_size.nx3 > 1) {
-        prim(IVZ,k,je+j,i) = -pco->x3v(k)/rad;
-        prim(IPR,k,je+j,i) = 1.0e-6*std::pow(f_t,(1.0+gmma));
-      } else {
-        prim(IVZ,k,je+j,i) = 0.0;
-        prim(IPR,k,je+j,i)= 1.0e-6;
+        prim(IDN,k,ju+j,i)  = d0;
+        prim(IVX,k,ju+j,i) = -pco->x1v(i)/rad;
+        prim(IVY,k,ju+j,i) = -pco->x2v(ju+j)/rad;
+        if (pmb->block_size.nx3 > 1) {
+          prim(IVZ,k,ju+j,i) = -pco->x3v(k)/rad;
+          prim(IPR,k,ju+j,i) = 1.0e-6*std::pow(f_t,(1.0+gmma));
+        } else {
+          prim(IVZ,k,ju+j,i) = 0.0;
+          prim(IPR,k,ju+j,i)= 1.0e-6;
+        }
       }
     }
   }
-}
 }
 
 //----------------------------------------------------------------------------------------
@@ -170,21 +176,22 @@ void Noh3DOuterX2(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim, Fac
 // Quantities at this boundary are held fixed at the time-dependent upstream state
 
 void Noh3DOuterX3(MeshBlock *pmb, Coordinates *pco, AthenaArray<Real> &prim, FaceField &b,
-       Real time, Real dt, int is, int ie, int js, int je, int ks, int ke, int ngh) {
+                  Real time, Real dt,
+                  int il, int iu, int jl, int ju, int kl, int ku, int ngh) {
   for (int k=1; k<=ngh; ++k) {
-  for (int j=js; j<=je; ++j) {
-    for (int i=is; i<=ie; ++i) {
-      Real rad = std::sqrt(SQR(pco->x1v(i)) + SQR(pco->x2v(j))
-              + SQR(pco->x3v(ke+k)));
-      Real f_t = SQR(1.0 + pmb->pmy_mesh->time/rad);
-      Real d0 = 1.0*f_t;
+    for (int j=jl; j<=ju; ++j) {
+      for (int i=il; i<=iu; ++i) {
+        Real rad = std::sqrt(SQR(pco->x1v(i)) + SQR(pco->x2v(j))
+                             + SQR(pco->x3v(ku+k)));
+        Real f_t = SQR(1.0 + pmb->pmy_mesh->time/rad);
+        Real d0 = 1.0*f_t;
 
-      prim(IDN,ke+k,j,i)  = d0;
-      prim(IVX,ke+k,j,i) = -pco->x1v(i)/rad;
-      prim(IVY,ke+k,j,i) = -pco->x2v(j)/rad;
-      prim(IVZ,ke+k,j,i) = -pco->x3v(ke+k)/rad;
-      prim(IPR,ke+k,j,i) = 1.0e-6*std::pow(f_t,(1.0+gmma));
+        prim(IDN,ku+k,j,i)  = d0;
+        prim(IVX,ku+k,j,i) = -pco->x1v(i)/rad;
+        prim(IVY,ku+k,j,i) = -pco->x2v(j)/rad;
+        prim(IVZ,ku+k,j,i) = -pco->x3v(ku+k)/rad;
+        prim(IPR,ku+k,j,i) = 1.0e-6*std::pow(f_t,(1.0+gmma));
+      }
     }
   }
-}
 }
