c184d5ecfb47e996dde7c953f92a54c8cf02eafc
Kyle Gerard Felker <kfelker@princeton.edu>
2019-01-24 00:54:42 -0500

    Disable sr/ regression tests on Travis CI
    After addition of 3x eos/ tests (which take around 8m total on Travis
    CI), macOS builds are often hitting the 2 hr limit and erroring out.

--- tst/ci/travis/run_tests_travis.sh
+++ tst/ci/travis/run_tests_travis.sh
@@ -46,7 +46,7 @@ time python3 run_tests.py hydro --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --
 time python3 run_tests.py outputs --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --silent
 time python3 run_tests.py curvilinear --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --silent
 time python3 run_tests.py gr --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --silent
-time python3 run_tests.py sr --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --silent
+#time python3 run_tests.py sr --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --silent  # 9x tests take about 11-15m on Travis CI
 time python3 run_tests.py shearingbox --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --silent
 time python3 run_tests.py diffusion --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --silent
 time python3 run_tests.py symmetry --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --silent
