08b0ca6ed56bea46dfe59a723e8945516c1e89f5
Kyle Gerard Felker <kfelker@princeton.edu>
2019-02-07 00:29:43 -0500

    Change location of bvals_mg.hpp in multigrid.hpp

--- src/multigrid/multigrid.hpp
+++ src/multigrid/multigrid.hpp
@@ -16,7 +16,7 @@
 // Athena++ headers
 #include "../athena.hpp"
 #include "../athena_arrays.hpp"
-#include "../bvals/bvals_mg.hpp"
+#include "../bvals/cc/mg/bvals_mg.hpp"
 #include "../globals.hpp"
 #include "../mesh/mesh.hpp"
 #include "../task_list/mg_task_list.hpp"
