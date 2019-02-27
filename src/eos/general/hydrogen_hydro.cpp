dece9df8a1688b5ef809decb85233aa0e4ff97be
Matt Coleman <mcoleman@ias.edu>
2019-02-27 10:20:27 -0500

    Corrected header/brief

--- src/eos/general/hydrogen_hydro.cpp
+++ src/eos/general/hydrogen_hydro.cpp
@@ -4,7 +4,7 @@
 // Licensed under the 3-clause BSD License, see LICENSE file for details
 //======================================================================================
 //! \file hydrogen_hydro.cpp
-//  \brief implements functions in class EquationOfState for an EOS lookup table
+//  \brief implements functions in class EquationOfState for simple hydrogen EOS
 //======================================================================================
 
 // C headers
