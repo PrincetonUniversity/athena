PrincetonUniversity/athena

commit ad5f132faafd412131849aa54a9d880b6a2238f6

Added a dependency for Receiving B boundary.
This also improved the bitwise reproducibility of MPI MHD calculations.

src/task_list/time_integrator.cpp
@@ -260,7 +260,7 @@ TimeIntegratorTaskList::TimeIntegratorTaskList(ParameterInput *pin, Mesh *pm)
       }
 
       AddTimeIntegratorTask(SEND_FLD,INT_FLD);
-      AddTimeIntegratorTask(RECV_FLD,START_ALLRECV);
+      AddTimeIntegratorTask(RECV_FLD,START_ALLRECV|INT_FLD);
       if (SHEARING_BOX) { // Shearingbox BC for Bfield
         AddTimeIntegratorTask(SEND_FLDSH,RECV_FLD);
         AddTimeIntegratorTask(RECV_FLDSH,RECV_FLD);
@@ -592,13 +592,11 @@ enum TaskStatus TimeIntegratorTaskList::HydroIntegrate(MeshBlock *pmb, int stage
     ave_wghts[0] = stage_wghts[stage-1].gamma_1;
     ave_wghts[1] = stage_wghts[stage-1].gamma_2;
     ave_wghts[2] = stage_wghts[stage-1].gamma_3;
-    if (ave_wghts[0] == 0.0 && ave_wghts[1] == 1.0 && ave_wghts[2] == 0.0) {
-      // Use swap instead of deep copy. Note that u1 will be replaced with u.
-      // If u1 needs to be kept, use WeightedAve although it costs more.
+    if (ave_wghts[0] == 0.0 && ave_wghts[0] == 1.0 && ave_wghts[2] == 0.0)
       ph->u.SwapAthenaArray(ph->u1);
-    } else {
+    else
       ph->WeightedAveU(ph->u, ph->u1, ph->u2, ave_wghts);
-    }
+
     ph->AddFluxDivergenceToAverage(ph->w, pf->bcc, stage_wghts[stage-1].beta, ph->u);
 
     // Hardcode an additional flux divergence weighted average for the penultimate
@@ -634,15 +632,14 @@ enum TaskStatus TimeIntegratorTaskList::FieldIntegrate(MeshBlock *pmb, int stage
     ave_wghts[0] = stage_wghts[stage-1].gamma_1;
     ave_wghts[1] = stage_wghts[stage-1].gamma_2;
     ave_wghts[2] = stage_wghts[stage-1].gamma_3;
-    if (ave_wghts[0] == 0.0 && ave_wghts[1] == 1.0 && ave_wghts[2] == 0.0) {
-      // Use swap instead of deep copy. Note that b1 will be replaced with b.
-      // If b1 needs to be kept, use WeightedAve although it costs more.
+    if (ave_wghts[0] == 0.0 && ave_wghts[0] == 1.0 && ave_wghts[2] == 0.0) {
       pf->b.x1f.SwapAthenaArray(pf->b1.x1f);
       pf->b.x2f.SwapAthenaArray(pf->b1.x2f);
       pf->b.x3f.SwapAthenaArray(pf->b1.x3f);
     } else {
       pf->WeightedAveB(pf->b,pf->b1,pf->b2,ave_wghts);
     }
+
     pf->CT(stage_wghts[stage-1].beta, pf->b);
 
     return TASK_NEXT;

src/field/ct.cpp
@@ -230,9 +230,32 @@ void Field::WeightedAveB(FaceField &b_out, FaceField &b_in1, FaceField &b_in2,
       }
     } else if (wght[1] == 1.0) {
       // just deep copy
-      b_out.x1f = b_in1.x1f;
-      b_out.x2f = b_in1.x2f;
-      b_out.x3f = b_in1.x3f;
+      //---- B1
+      for (int k=ks; k<=ke; ++k) {
+        for (int j=js; j<=je; ++j) {
+#pragma omp simd
+          for (int i=is; i<=ie+1; ++i) {
+            b_out.x1f(k,j,i) = b_in1.x1f(k,j,i);
+          }
+        }}
+      //---- B2
+      for (int k=ks; k<=ke; ++k) {
+        for (int j=jl; j<=ju; ++j) {
+#pragma omp simd
+          for (int i=is; i<=ie; ++i) {
+            b_out.x2f(k,j,i) = b_in1.x2f(k,j,i);
+          }
+        }
+      }
+      //---- B3
+      for (int k=ks; k<=ke+1; ++k) {
+        for (int j=js; j<=je; ++j) {
+#pragma omp simd
+          for (int i=is; i<=ie; ++i) {
+            b_out.x3f(k,j,i) = b_in1.x3f(k,j,i);
+          }
+        }
+      }
     } else {
       //---- B1
       for (int k=ks; k<=ke; ++k) {

src/hydro/add_flux_divergence.cpp
@@ -154,7 +154,16 @@ void Hydro::WeightedAveU(AthenaArray<Real> &u_out, AthenaArray<Real> &u_in1,
       }
     } else if (wght[1] == 1.0) {
       // just deep copy
-      u_out = u_in1;
+      for (int n=0; n<NHYDRO; ++n) {
+        for (int k=ks; k<=ke; ++k) {
+          for (int j=js; j<=je; ++j) {
+#pragma omp simd
+            for (int i=is; i<=ie; ++i) {
+              u_out(n,k,j,i) = u_in1(n,k,j,i);
+            }
+          }
+        }
+      }
     } else {
       for (int n=0; n<NHYDRO; ++n) {
         for (int k=ks; k<=ke; ++k) {

commit 4abd8885899bfa1606573cc4d55b68da052ae3fc

Correct subtle typo that likely causes a nasty performance bug
Introduced in ad5f132faafd412131849aa54a9d880b6a2238f6

Because of this bug, each stage involved unnecessary deep-copying of
FaceField b and AthenaArray u in MHD, since the conditional always
evaluated to false.

Detected on bvals_pscalars thanks to STS, which successfully swaps u,
u1, u3 data pointers and b, b1, b2 data pointers. s/o to @pdmullen

src/task_list/time_integrator.cpp
@@ -625,7 +625,7 @@ TaskStatus TimeIntegratorTaskList::HydroIntegrate(MeshBlock *pmb, int stage) {
     ave_wghts[0] = stage_wghts[stage-1].gamma_1;
     ave_wghts[1] = stage_wghts[stage-1].gamma_2;
     ave_wghts[2] = stage_wghts[stage-1].gamma_3;
-    if (ave_wghts[0] == 0.0 && ave_wghts[0] == 1.0 && ave_wghts[2] == 0.0)
+    if (ave_wghts[0] == 0.0 && ave_wghts[1] == 1.0 && ave_wghts[2] == 0.0)
       ph->u.SwapAthenaArray(ph->u1);
     else
       pmb->WeightedAve(ph->u, ph->u1, ph->u2, ave_wghts);
@@ -665,7 +665,7 @@ TaskStatus TimeIntegratorTaskList::FieldIntegrate(MeshBlock *pmb, int stage) {
     ave_wghts[0] = stage_wghts[stage-1].gamma_1;
     ave_wghts[1] = stage_wghts[stage-1].gamma_2;
     ave_wghts[2] = stage_wghts[stage-1].gamma_3;
-    if (ave_wghts[0] == 0.0 && ave_wghts[0] == 1.0 && ave_wghts[2] == 0.0) {
+    if (ave_wghts[0] == 0.0 && ave_wghts[1] == 1.0 && ave_wghts[2] == 0.0) {
       pf->b.x1f.SwapAthenaArray(pf->b1.x1f);
       pf->b.x2f.SwapAthenaArray(pf->b1.x2f);
       pf->b.x3f.SwapAthenaArray(pf->b1.x3f);