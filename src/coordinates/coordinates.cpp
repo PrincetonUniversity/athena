c5f5066ee09a419ddec0db757a39572336d1d4e7
Kyle Gerard Felker <kfelker@princeton.edu>
2019-04-13 00:43:18 -0400

    Fix typo

--- src/coordinates/coordinates.cpp
+++ src/coordinates/coordinates.cpp
@@ -1,4 +1,4 @@
-/========================================================================================
+//========================================================================================
 // Athena++ astrophysical MHD code
 // Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
 // Licensed under the 3-clause BSD License, see LICENSE file for details
