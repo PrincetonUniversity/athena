17342136f7f675c663ee2dd4b85818450c0a9aeb
Kyle Gerard Felker <kfelker@princeton.edu>
2019-04-16 14:14:01 -0400

    Conditionally allocate flux[3] and coarse_* arrays in Hydro init. list

--- src/hydro/hydro.cpp
+++ src/hydro/hydro.cpp
@@ -32,41 +32,42 @@ Hydro::Hydro(MeshBlock *pmb, ParameterInput *pin) :
     w(NHYDRO, pmb->ncells3, pmb->ncells2, pmb->ncells1),
     u1(NHYDRO, pmb->ncells3, pmb->ncells2, pmb->ncells1),
     w1(NHYDRO, pmb->ncells3, pmb->ncells2, pmb->ncells1),
+    flux{AthenaArray<Real>(NHYDRO, pmb->ncells3, pmb->ncells2, pmb->ncells1+1),
+         AthenaArray<Real>(
+             NHYDRO, pmb->ncells3, pmb->ncells2+1, pmb->ncells1,
+             (pmb->pmy_mesh->f2_ ? AthenaArray<Real>::DataStatus::allocated :
+              AthenaArray<Real>::DataStatus::empty)),
+         AthenaArray<Real>(
+             NHYDRO, pmb->ncells3+1, pmb->ncells2, pmb->ncells1,
+             (pmb->pmy_mesh->f3_ ? AthenaArray<Real>::DataStatus::allocated :
+              AthenaArray<Real>::DataStatus::empty)) },
+    coarse_cons_(NHYDRO, pmb->ncc3, pmb->ncc2, pmb->ncc1,
+                 (pmb->pmy_mesh->multilevel ? AthenaArray<Real>::DataStatus::allocated :
+                  AthenaArray<Real>::DataStatus::empty)),
+    coarse_prim_(NHYDRO, pmb->ncc3, pmb->ncc2, pmb->ncc1,
+                 (pmb->pmy_mesh->multilevel ? AthenaArray<Real>::DataStatus::allocated :
+                  AthenaArray<Real>::DataStatus::empty)),
     hbvar(pmb, &u, &coarse_cons_, flux, HydroBoundaryQuantity::cons),
     hsrc(this, pin),
     hdif(this, pin) {
-  int ncells1 = pmb->ncells1, ncells2 = pmb->ncells2, ncells3 = pmb->ncells3;
+  int nc1 = pmb->ncells1, nc2 = pmb->ncells2, nc3 = pmb->ncells3;
   Mesh *pm = pmy_block->pmy_mesh;
 
   // Allocate optional memory primitive/conserved variable registers for time-integrator
   if (pmb->precon->xorder == 4) {
     // fourth-order hydro cell-centered approximations
-    u_cc.NewAthenaArray(NHYDRO, ncells3, ncells2, ncells1);
-    w_cc.NewAthenaArray(NHYDRO, ncells3, ncells2, ncells1);
+    u_cc.NewAthenaArray(NHYDRO, nc3, nc2, nc1);
+    w_cc.NewAthenaArray(NHYDRO, nc3, nc2, nc1);
   }
 
   // If user-requested time integrator is type 3S*, allocate additional memory registers
   std::string integrator = pin->GetOrAddString("time", "integrator", "vl2");
   if (integrator == "ssprk5_4" || STS_ENABLED)
     // future extension may add "int nregister" to Hydro class
-    u2.NewAthenaArray(NHYDRO, ncells3, ncells2, ncells1);
+    u2.NewAthenaArray(NHYDRO, nc3, nc2, nc1);
 
-  flux[X1DIR].NewAthenaArray(NHYDRO, ncells3, ncells2, ncells1+1);
-  if (pm->f2_)
-    flux[X2DIR].NewAthenaArray(NHYDRO, ncells3, ncells2+1, ncells1);
-  if (pm->f3_)
-    flux[X3DIR].NewAthenaArray(NHYDRO, ncells3+1, ncells2, ncells1);
-
-  // allocate prolongation buffers
+  // "Enroll" in S/AMR by adding to vector of tuples of pointers in MeshRefinement class
   if (pm->multilevel == true) {
-    int ncc1 = pmb->block_size.nx1/2 + 2*NGHOST;
-    int ncc2 = 1;
-    if (pm->f2_) ncc2 = pmb->block_size.nx2/2 + 2*NGHOST;
-    int ncc3 = 1;
-    if (pm->f3_) ncc3 = pmb->block_size.nx3/2 + 2*NGHOST;
-    coarse_cons_.NewAthenaArray(NHYDRO, ncc3, ncc2, ncc1);
-    coarse_prim_.NewAthenaArray(NHYDRO, ncc3, ncc2, ncc1);
-    // "Enroll" in S/AMR by adding to vector of tuples of pointers in MeshRefinement class
     pmy_block->pmr->AddToRefinement(&u, &coarse_cons_);
   }
 
@@ -76,59 +77,59 @@ Hydro::Hydro(MeshBlock *pmb, ParameterInput *pin) :
   pmb->pbval->bvars_main_int.push_back(&hbvar);
 
   // Allocate memory for scratch arrays
-  dt1_.NewAthenaArray(ncells1);
-  dt2_.NewAthenaArray(ncells1);
-  dt3_.NewAthenaArray(ncells1);
-  dxw_.NewAthenaArray(ncells1);
-  wl_.NewAthenaArray(NWAVE, ncells1);
-  wr_.NewAthenaArray(NWAVE, ncells1);
-  wlb_.NewAthenaArray(NWAVE, ncells1);
-  x1face_area_.NewAthenaArray(ncells1+1);
+  dt1_.NewAthenaArray(nc1);
+  dt2_.NewAthenaArray(nc1);
+  dt3_.NewAthenaArray(nc1);
+  dxw_.NewAthenaArray(nc1);
+  wl_.NewAthenaArray(NWAVE, nc1);
+  wr_.NewAthenaArray(NWAVE, nc1);
+  wlb_.NewAthenaArray(NWAVE, nc1);
+  x1face_area_.NewAthenaArray(nc1+1);
   if (pm->f2_) {
-    x2face_area_.NewAthenaArray(ncells1);
-    x2face_area_p1_.NewAthenaArray(ncells1);
+    x2face_area_.NewAthenaArray(nc1);
+    x2face_area_p1_.NewAthenaArray(nc1);
   }
   if (pm->f3_) {
-    x3face_area_.NewAthenaArray(ncells1);
-    x3face_area_p1_.NewAthenaArray(ncells1);
+    x3face_area_.NewAthenaArray(nc1);
+    x3face_area_p1_.NewAthenaArray(nc1);
   }
-  cell_volume_.NewAthenaArray(ncells1);
-  dflx_.NewAthenaArray((NHYDRO), ncells1);
+  cell_volume_.NewAthenaArray(nc1);
+  dflx_.NewAthenaArray((NHYDRO), nc1);
   if (MAGNETIC_FIELDS_ENABLED && RELATIVISTIC_DYNAMICS) { // only used in (SR/GR)MHD
-    bb_normal_.NewAthenaArray(ncells1);
-    lambdas_p_l_.NewAthenaArray(ncells1);
-    lambdas_m_l_.NewAthenaArray(ncells1);
-    lambdas_p_r_.NewAthenaArray(ncells1);
-    lambdas_m_r_.NewAthenaArray(ncells1);
+    bb_normal_.NewAthenaArray(nc1);
+    lambdas_p_l_.NewAthenaArray(nc1);
+    lambdas_m_l_.NewAthenaArray(nc1);
+    lambdas_p_r_.NewAthenaArray(nc1);
+    lambdas_m_r_.NewAthenaArray(nc1);
   }
   if (GENERAL_RELATIVITY) { // only used in GR
-    g_.NewAthenaArray(NMETRIC, ncells1);
-    gi_.NewAthenaArray(NMETRIC, ncells1);
-    cons_.NewAthenaArray(NWAVE, ncells1);
+    g_.NewAthenaArray(NMETRIC, nc1);
+    gi_.NewAthenaArray(NMETRIC, nc1);
+    cons_.NewAthenaArray(NWAVE, nc1);
   }
   // for one-time potential calcuation and correction (old Athena)
   if (SELF_GRAVITY_ENABLED == 3) {
-    gflx[X1DIR].NewAthenaArray(NHYDRO, ncells3, ncells2, ncells1+1);
+    gflx[X1DIR].NewAthenaArray(NHYDRO, nc3, nc2, nc1+1);
     if (pm->f2_)
-      gflx[X2DIR].NewAthenaArray(NHYDRO, ncells3, ncells2+1, ncells1);
+      gflx[X2DIR].NewAthenaArray(NHYDRO, nc3, nc2+1, nc1);
     if (pm->f3_)
-      gflx[X3DIR].NewAthenaArray(NHYDRO, ncells3+1, ncells2, ncells1);
+      gflx[X3DIR].NewAthenaArray(NHYDRO, nc3+1, nc2, nc1);
 
-    gflx_old[X1DIR].NewAthenaArray(NHYDRO, ncells3, ncells2, ncells1+1);
+    gflx_old[X1DIR].NewAthenaArray(NHYDRO, nc3, nc2, nc1+1);
     if (pm->f2_)
-      gflx_old[X2DIR].NewAthenaArray(NHYDRO, ncells3, ncells2+1, ncells1);
+      gflx_old[X2DIR].NewAthenaArray(NHYDRO, nc3, nc2+1, nc1);
     if (pm->f3_)
-      gflx_old[X3DIR].NewAthenaArray(NHYDRO, ncells3+1, ncells2, ncells1);
+      gflx_old[X3DIR].NewAthenaArray(NHYDRO, nc3+1, nc2, nc1);
   }
 
   // fourth-order hydro
   // 4D scratch arrays
-  wl3d_.NewAthenaArray(NWAVE, ncells3, ncells2, ncells1);
-  wr3d_.NewAthenaArray(NWAVE, ncells3, ncells2, ncells1);
-  scr1_nkji_.NewAthenaArray(NHYDRO, ncells3, ncells2, ncells1);
-  scr2_nkji_.NewAthenaArray(NHYDRO, ncells3, ncells2, ncells1);
-  laplacian_l_fc_.NewAthenaArray(ncells1);
-  laplacian_r_fc_.NewAthenaArray(ncells1);
+  wl3d_.NewAthenaArray(NWAVE, nc3, nc2, nc1);
+  wr3d_.NewAthenaArray(NWAVE, nc3, nc2, nc1);
+  scr1_nkji_.NewAthenaArray(NHYDRO, nc3, nc2, nc1);
+  scr2_nkji_.NewAthenaArray(NHYDRO, nc3, nc2, nc1);
+  laplacian_l_fc_.NewAthenaArray(nc1);
+  laplacian_r_fc_.NewAthenaArray(nc1);
 
   UserTimeStep_ = pmb->pmy_mesh->UserTimeStep_;
 }
@@ -139,6 +140,6 @@ Hydro::Hydro(MeshBlock *pmb, ParameterInput *pin) :
 
 Real Hydro::GetWeightForCT(Real dflx, Real rhol, Real rhor, Real dx, Real dt) {
   Real v_over_c = (1024.0)* dt * dflx / (dx * (rhol + rhor));
-  Real tmp_min = std::min(static_cast<Real>(0.5),v_over_c);
-  return 0.5 + std::max(static_cast<Real>(-0.5),tmp_min);
+  Real tmp_min = std::min(static_cast<Real>(0.5), v_over_c);
+  return 0.5 + std::max(static_cast<Real>(-0.5), tmp_min);
 }
