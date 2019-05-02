cedf18967052cbb40b657ad00f145b492c88e2eb
Matt Coleman <mcoleman@ias.edu>
2019-05-02 10:29:53 -0400

    Updated eos_mhd test

--- tst/regression/scripts/tests/eos/eos_mhd.py
+++ tst/regression/scripts/tests/eos/eos_mhd.py
@@ -3,16 +3,25 @@ Regression test for general EOS 1D Riemann problems.
 """
 
 # Modules
-import sys                                     # standard Python module to change path
-import scripts.utils.athena as athena          # utilities for running Athena++
-import scripts.utils.comparison as comparison  # more utilities explicitly for testing
+import logging
+import sys
+import scripts.utils.athena as athena
+import scripts.utils.comparison as comparison
 from scripts.utils.RiemannSolver.riemann import riemann_problem
+sys.path.insert(0, '../../vis/python')
+import athena_read  # noqa
 from .eos_riemann import _tests, _thresh, _states
-sys.path.insert(0, '../../vis/python')         # insert path to Python read scripts
-import athena_read  # noqa                     # utilities for reading Athena++ data
+athena_read.check_nan_flag = True
+logger = logging.getLogger('athena' + __name__[7:])  # set logger name based on module
+
+_mag_list = [str(i) for i in [1e-5, 1e-4, 1e-3]]
+_mhd_tests = _tests + len(_mag_list) * [_tests[0]]
+_mhd_thresh = _thresh + len(_mag_list) * [_thresh[0]]
+_mhd_states = _states + len(_mag_list) * [_states[0]]
 
 
 def prepare(**kwargs):
+    logger.debug('Running test ' + __name__)
     athena.configure('b',
                      prob='shock_tube',
                      coord='cartesian',
@@ -23,28 +32,40 @@ def prepare(**kwargs):
 
 
 def run(**kwargs):
+    # run same tests as in eos_riemann.py
     for n, test in enumerate(_tests):
         args = [i + '={0:}'.format(test[i]) for i in test]
-        args += ['job/problem_id=eos_riemann_{0:02d}'.format(n), 'time/ncycle_out=100']
-        athena.run('mhd/athinput.sod_general_H', args)
-
+        args += ['job/problem_id=eos_mhd_{0:02d}'.format(n), 'time/ncycle_out=0']
+        athena.run('hydro/athinput.sod_general_H', args)
+    # run first Riemann problem but with different none zero Bx
+    for mag in _mag_list:
+        n += 1
+        args = ['problem/bxl=' + mag, 'problem/bxr=' + mag]
+        args += ['job/problem_id=eos_mhd_{0:02d}'.format(n), 'time/ncycle_out=0']
+        athena.run('hydro/athinput.sod_general_H', args)
+    # run RJ2a test for hydrogen EOS
+    args = ['hydro/eos_rho_unit=1e-7', 'hydro/eos_egas_unit=1e-8', 'time/ncycle_out=0']
+    athena.run('mhd/athinput.rj2a', args)
 
 def analyze():
     analyze_status = True
-    for n, state in enumerate(_states):
+    for n, state in enumerate(_mhd_states):
         t = 1
         x_ref, _, _, data_ref = athena_read.vtk(
-            'bin/eos_riemann_{0:02d}.block0.out1.{1:05d}.vtk'.format(n, t))
-        xi = (.5 * x_ref[:-1] + .5 * x_ref[1:]) / _tests[n]['time/tlim']
+            'bin/eos_mhd_{0:02d}.block0.out1.{1:05d}.vtk'.format(n, t))
+        xi = (.5 * x_ref[:-1] + .5 * x_ref[1:]) / _mhd_tests[n]['time/tlim']
         exact = riemann_problem(state, 'H').data_array(xi)
         for var in ['rho', 'press', 'vel']:
             data = data_ref[var][0, 0, :]
             if var == 'vel':
                 data = data_ref[var][0, 0, :, 0]
             diff = comparison.l1_norm(x_ref, data - exact[var])
-            if diff > _thresh[n][var]:
-                print(' '.join(
-                    map(str, ['EOS Riemann fail. Test#, var, diff, thresh =', n,
-                        var, diff, _thresh[n][var]])))
+            msg = ['EOS Riemann', 'fail:', 'Test#, var, diff, thresh =', n, var, diff,
+                   _mhd_thresh[n][var]]
+            if diff > _mhd_thresh[n][var]:
+                logger.warning(' '.join(map(str, msg)))
                 analyze_status = False
+            else:
+                msg[1] = 'pass:'
+                logger.debug(' '.join(map(str, msg)))
     return analyze_status
