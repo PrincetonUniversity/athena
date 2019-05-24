PrincetonUniversity/athena

commit 9c6bf10acc56ba370b7a5c38e61192773082d5f8

Do not assume dt factor in AddFluxDivergence() and CT() fns
Make client pass dt in first parameter, wght. For consistency with
AddCoordTermsDivergence() behavior.

4th order integrator call was missing dt factor in call to
AddCoordTermsDivergence. Credit to @pdmullen

src/task_list/time_integrator.cpp
@@ -794,11 +794,10 @@ TaskStatus TimeIntegratorTaskList::IntegrateHydro(MeshBlock *pmb, int stage) {
     else
       pmb->WeightedAve(ph->u, ph->u1, ph->u2, ave_wghts);
 
-    const Real wght = stage_wghts[stage-1].beta;
+    const Real wght = stage_wghts[stage-1].beta*pmb->pmy_mesh->dt;
     ph->AddFluxDivergence(wght, ph->u);
     // add coordinate (geometric) source terms
-    pmb->pcoord->AddCoordTermsDivergence(wght*pmb->pmy_mesh->dt, ph->flux, ph->w,
-                                         pf->bcc, ph->u);
+    pmb->pcoord->AddCoordTermsDivergence(wght, ph->flux, ph->w, pf->bcc, ph->u);
 
     // Hardcode an additional flux divergence weighted average for the penultimate
     // stage of SSPRK(5,4) since it cannot be expressed in a 3S* framework
@@ -807,13 +806,14 @@ TaskStatus TimeIntegratorTaskList::IntegrateHydro(MeshBlock *pmb, int stage) {
       ave_wghts[0] = -1.0; // -u^(n) coeff.
       ave_wghts[1] = 0.0;
       ave_wghts[2] = 0.0;
-      Real beta = 0.063692468666290; // F(u^(3)) coeff.
+      const Real beta = 0.063692468666290; // F(u^(3)) coeff.
+      const Real wght = beta*pmb->pmy_mesh->dt;
       // writing out to u2 register
       pmb->WeightedAve(ph->u2, ph->u1, ph->u2, ave_wghts);
 
-       ph->AddFluxDivergence(beta, ph->u2);
+       ph->AddFluxDivergence(wght, ph->u2);
       // add coordinate (geometric) source terms
-      pmb->pcoord->AddCoordTermsDivergence(beta, ph->flux, ph->w, pf->bcc, ph->u2);
+      pmb->pcoord->AddCoordTermsDivergence(wght, ph->flux, ph->w, pf->bcc, ph->u2);
     }
     return TaskStatus::next;
   }
@@ -845,7 +845,7 @@ TaskStatus TimeIntegratorTaskList::IntegrateField(MeshBlock *pmb, int stage) {
       pmb->WeightedAve(pf->b, pf->b1, pf->b2, ave_wghts);
     }
 
-    pf->CT(stage_wghts[stage-1].beta, pf->b);
+    pf->CT(stage_wghts[stage-1].beta*pmb->pmy_mesh->dt, pf->b);
 
     return TaskStatus::next;
   }
@@ -1241,7 +1241,7 @@ TaskStatus TimeIntegratorTaskList::IntegrateScalars(MeshBlock *pmb, int stage) {
     else
       pmb->WeightedAve(ps->s, ps->s1, ps->s2, ave_wghts);
 
-    const Real wght = stage_wghts[stage-1].beta;
+    const Real wght = stage_wghts[stage-1].beta*pmb->pmy_mesh->dt;
     ps->AddFluxDivergence(wght, ps->s);
 
     // Hardcode an additional flux divergence weighted average for the penultimate
@@ -1251,7 +1251,8 @@ TaskStatus TimeIntegratorTaskList::IntegrateScalars(MeshBlock *pmb, int stage) {
       ave_wghts[0] = -1.0; // -u^(n) coeff.
       ave_wghts[1] = 0.0;
       ave_wghts[2] = 0.0;
-      Real beta = 0.063692468666290; // F(u^(3)) coeff.
+      const Real beta = 0.063692468666290; // F(u^(3)) coeff.
+      const Real wght = beta*pmb->pmy_mesh->dt;
       // writing out to s2 register
       pmb->WeightedAve(ps->s2, ps->s1, ps->s2, ave_wghts);
       ps->AddFluxDivergence(beta, ps->s2);

src/field/ct.cpp
@@ -47,8 +47,7 @@ void Field::CT(const Real wght, FaceField &b_out) {
 #pragma omp simd
         for (int i=is; i<=ie+1; ++i) {
           b_out.x1f(k,j,i) -=
-              wght*
-              ((pmb->pmy_mesh->dt)/area(i))*(len_p1(i)*e3(k,j+1,i) - len(i)*e3(k,j,i));
+              (wght/area(i))*(len_p1(i)*e3(k,j+1,i) - len(i)*e3(k,j,i));
         }
 
         if (pmb->block_size.nx3 > 1) {
@@ -57,8 +56,7 @@ void Field::CT(const Real wght, FaceField &b_out) {
 #pragma omp simd
           for (int i=is; i<=ie+1; ++i) {
             b_out.x1f(k,j,i) +=
-                wght*
-                ((pmb->pmy_mesh->dt)/area(i))*(len_p1(i)*e2(k+1,j,i) -len(i)*e2(k,j,i));
+                (wght/area(i))*(len_p1(i)*e2(k+1,j,i) - len(i)*e2(k,j,i));
           }
         }
       }
@@ -80,8 +78,7 @@ void Field::CT(const Real wght, FaceField &b_out) {
       pmb->pcoord->Edge3Length(k,j,is,ie+1,len);
 #pragma omp simd
       for (int i=is; i<=ie; ++i) {
-        b_out.x2f(k,j,i) += (wght*(pmb->pmy_mesh->dt)/area(i))*(len(i+1)*e3(k,j,i+1)
-                                                                - len(i)*e3(k,j,i));
+        b_out.x2f(k,j,i) += (wght/area(i))*(len(i+1)*e3(k,j,i+1) - len(i)*e3(k,j,i));
       }
       if (pmb->block_size.nx3 > 1) {
         pmb->pcoord->Edge1Length(k  ,j,is,ie,len);
@@ -89,8 +86,7 @@ void Field::CT(const Real wght, FaceField &b_out) {
 #pragma omp simd
         for (int i=is; i<=ie; ++i) {
           b_out.x2f(k,j,i) -=
-              wght*
-              ((pmb->pmy_mesh->dt)/area(i))*(len_p1(i)*e1(k+1,j,i) - len(i)*e1(k,j,i));
+              (wght/area(i))*(len_p1(i)*e1(k+1,j,i) - len(i)*e1(k,j,i));
         }
       }
     }
@@ -103,8 +99,7 @@ void Field::CT(const Real wght, FaceField &b_out) {
       pmb->pcoord->Edge2Length(k,j,is,ie+1,len);
 #pragma omp simd
       for (int i=is; i<=ie; ++i) {
-        b_out.x3f(k,j,i) -= (wght*(pmb->pmy_mesh->dt)/area(i))*(len(i+1)*e2(k,j,i+1) -
-                                                                len(i)*e2(k,j,i));
+        b_out.x3f(k,j,i) -= (wght/area(i))*(len(i+1)*e2(k,j,i+1) - len(i)*e2(k,j,i));
       }
       if (pmb->block_size.nx2 > 1) {
         pmb->pcoord->Edge1Length(k,j  ,is,ie,len);
@@ -112,8 +107,7 @@ void Field::CT(const Real wght, FaceField &b_out) {
 #pragma omp simd
         for (int i=is; i<=ie; ++i) {
           b_out.x3f(k,j,i) +=
-              wght*
-              ((pmb->pmy_mesh->dt)/area(i))*(len_p1(i)*e1(k,j+1,i) - len(i)*e1(k,j,i));
+              (wght/area(i))*(len_p1(i)*e1(k,j+1,i) - len(i)*e1(k,j,i));
         }
       }
     }

src/hydro/add_flux_divergence.cpp
@@ -47,7 +47,7 @@ void Hydro::AddFluxDivergence(const Real wght, AthenaArray<Real> &u_out) {
   for (int k=ks; k<=ke; ++k) {
     for (int j=js; j<=je; ++j) {
       // calculate x1-flux divergence
-      pmb->pcoord->Face1Area(k,j,is,ie+1,x1area);
+      pmb->pcoord->Face1Area(k, j, is, ie+1, x1area);
       for (int n=0; n<NHYDRO; ++n) {
 #pragma omp simd
         for (int i=is; i<=ie; ++i) {
@@ -57,8 +57,8 @@ void Hydro::AddFluxDivergence(const Real wght, AthenaArray<Real> &u_out) {
 
       // calculate x2-flux divergence
       if (pmb->block_size.nx2 > 1) {
-        pmb->pcoord->Face2Area(k,j  ,is,ie,x2area   );
-        pmb->pcoord->Face2Area(k,j+1,is,ie,x2area_p1);
+        pmb->pcoord->Face2Area(k, j  , is, ie, x2area   );
+        pmb->pcoord->Face2Area(k, j+1, is, ie, x2area_p1);
         for (int n=0; n<NHYDRO; ++n) {
 #pragma omp simd
           for (int i=is; i<=ie; ++i) {
@@ -69,8 +69,8 @@ void Hydro::AddFluxDivergence(const Real wght, AthenaArray<Real> &u_out) {
 
       // calculate x3-flux divergence
       if (pmb->block_size.nx3 > 1) {
-        pmb->pcoord->Face3Area(k  ,j,is,ie,x3area   );
-        pmb->pcoord->Face3Area(k+1,j,is,ie,x3area_p1);
+        pmb->pcoord->Face3Area(k  , j, is, ie, x3area   );
+        pmb->pcoord->Face3Area(k+1, j, is, ie, x3area_p1);
         for (int n=0; n<NHYDRO; ++n) {
 #pragma omp simd
           for (int i=is; i<=ie; ++i) {
@@ -80,11 +80,11 @@ void Hydro::AddFluxDivergence(const Real wght, AthenaArray<Real> &u_out) {
       }
 
       // update conserved variables
-      pmb->pcoord->CellVolume(k,j,is,ie,vol);
+      pmb->pcoord->CellVolume(k, j, is, ie, vol);
       for (int n=0; n<NHYDRO; ++n) {
 #pragma omp simd
         for (int i=is; i<=ie; ++i) {
-          u_out(n,k,j,i) -= wght*(pmb->pmy_mesh->dt)*dflx(n,i)/vol(i);
+          u_out(n,k,j,i) -= wght*dflx(n,i)/vol(i);
         }
       }
     }

src/scalars/add_scalar_flux_divergence.cpp
@@ -91,7 +91,7 @@ void PassiveScalars::AddFluxDivergence(const Real wght, AthenaArray<Real> &s_out
       for (int n=0; n<NSCALARS; ++n) {
 #pragma omp simd
         for (int i=is; i<=ie; ++i) {
-          s_out(n,k,j,i) -= wght*(pmb->pmy_mesh->dt)*dflx(n,i)/vol(i);
+          s_out(n,k,j,i) -= wght*dflx(n,i)/vol(i);
         }
       }
     }

src/task_list/sts_task_list.cpp
@@ -405,7 +405,7 @@ TaskStatus SuperTimeStepTaskList::IntegrateHydro_STS(MeshBlock *pmb, int stage) 
     ave_wghts[1] = pmb->pmy_mesh->muj;
     ave_wghts[2] = pmb->pmy_mesh->nuj;
     pmb->WeightedAve(ph->u, ph->u1, ph->u2, ave_wghts);
-    Real wght = pmb->pmy_mesh->muj_tilde;
+    const Real wght = pmb->pmy_mesh->muj_tilde*pmb->pmy_mesh->dt;
     ph->AddFluxDivergence(wght, ph->u);
     // TODO(pdmullen): check this after disabling ATHENA_ERROR for src terms
     pmb->pcoord->AddCoordTermsDivergence(wght, ph->flux, ph->w, pf->bcc, ph->u);
@@ -428,7 +428,7 @@ TaskStatus SuperTimeStepTaskList::IntegrateScalars_STS(MeshBlock *pmb, int stage
     ave_wghts[1] = pmb->pmy_mesh->muj;
     ave_wghts[2] = pmb->pmy_mesh->nuj;
     pmb->WeightedAve(ps->s, ps->s1, ps->s2, ave_wghts);
-    Real wght = pmb->pmy_mesh->muj_tilde;
+    const Real wght = pmb->pmy_mesh->muj_tilde*pmb->pmy_mesh->dt;
     ps->AddFluxDivergence(wght, ps->s);
 
     return TaskStatus::next;
@@ -448,7 +448,7 @@ TaskStatus SuperTimeStepTaskList::IntegrateField_STS(MeshBlock *pmb, int stage) 
     ave_wghts[1] = pmb->pmy_mesh->muj;
     ave_wghts[2] = pmb->pmy_mesh->nuj;
     pmb->WeightedAve(pf->b, pf->b1, pf->b2, ave_wghts);
-    pf->CT(pmb->pmy_mesh->muj_tilde, pf->b);
+    pf->CT(pmb->pmy_mesh->muj_tilde*pmb->pmy_mesh->dt, pf->b);
     return TaskStatus::next;
   }
   return TaskStatus::fail;

commit 723b51bbc1983e53034e56450baaec22588f85b2

Fix bug with passive scalar integrator + SSPRK(5,4)
Recently introduced by 9c6bf10acc56ba370b7a5c38e61192773082d5f8

Do not shadow "wghts" variable with declaration inside inner block

src/task_list/time_integrator.cpp
@@ -807,12 +807,12 @@ TaskStatus TimeIntegratorTaskList::IntegrateHydro(MeshBlock *pmb, int stage) {
       ave_wghts[1] = 0.0;
       ave_wghts[2] = 0.0;
       const Real beta = 0.063692468666290; // F(u^(3)) coeff.
-      const Real wght = beta*pmb->pmy_mesh->dt;
+      const Real wght_ssp = beta*pmb->pmy_mesh->dt;
       // writing out to u2 register
       pmb->WeightedAve(ph->u2, ph->u1, ph->u2, ave_wghts);
-      ph->AddFluxDivergence(wght, ph->u2);
+      ph->AddFluxDivergence(wght_ssp, ph->u2);
       // add coordinate (geometric) source terms
-      pmb->pcoord->AddCoordTermsDivergence(wght, ph->flux, ph->w, pf->bcc, ph->u2);
+      pmb->pcoord->AddCoordTermsDivergence(wght_ssp, ph->flux, ph->w, pf->bcc, ph->u2);
     }
     return TaskStatus::next;
   }
@@ -1254,10 +1254,10 @@ TaskStatus TimeIntegratorTaskList::IntegrateScalars(MeshBlock *pmb, int stage) {
       ave_wghts[1] = 0.0;
       ave_wghts[2] = 0.0;
       const Real beta = 0.063692468666290; // F(u^(3)) coeff.
-      const Real wght = beta*pmb->pmy_mesh->dt;
+      const Real wght_ssp = beta*pmb->pmy_mesh->dt;
       // writing out to s2 register
       pmb->WeightedAve(ps->s2, ps->s1, ps->s2, ave_wghts);
-      ps->AddFluxDivergence(beta, ps->s2);
+      ps->AddFluxDivergence(wght_ssp, ps->s2);
     }
     return TaskStatus::next;
   }