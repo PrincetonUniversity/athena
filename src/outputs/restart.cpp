d5ed65dc22fe833ca3833aff94ae7997bfe14151
Matt Coleman <mcoleman@ias.edu>
2019-06-16 17:23:53 -0400

    Only save scalars if they exist.

--- src/outputs/restart.cpp
+++ src/outputs/restart.cpp
@@ -173,9 +173,11 @@ void RestartOutput::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool force_wr
     }
 
     // (conserved variable) Passive scalars:
-    AthenaArray<Real> &s = pmb->pscalars->s;
-    std::memcpy(pdata, s.data(), s.GetSizeInBytes());
-    pdata += s.GetSizeInBytes();
+    if (NSCALARS > 0) {
+      AthenaArray<Real> &s = pmb->pscalars->s;
+      std::memcpy(pdata, s.data(), s.GetSizeInBytes());
+      pdata += s.GetSizeInBytes();
+    }
     // (primitive variable) density-normalized passive scalar concentrations
     // if ???
     // for (int n=0; n<NSCALARS; n++) {
