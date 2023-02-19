74e4014fdeff45620a19d417e11d5726ef2c9140
Kengo Tomida <tomida@astr.tohoku.ac.jp>
2023-02-19 15:10:16 +0900

    Release Candidate of the Multigrid Poisson Solver
    This commit contains the release candidate of the Multigrid Poisson Solver which is used in the method paper (Tomida & Stone 2023). The input parameters are slightly changed from the previous versions, and the convergence threshold is now defect itself, not multiplied by dx^2. Other than those, there are no major changes in the user interface. Additional test problems are provided.

--- src/pgen/binary_gravity.cpp
+++ src/pgen/binary_gravity.cpp
@@ -165,28 +165,6 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
       }
     }
   }
-  for (int k = ks; k <= ke; ++k) {
-    Real z = pcoord->x3v(k);
-    for (int j = js; j <= je; ++j) {
-     Real y = pcoord->x2v(j);
-      for (int i = is; i <= ie; ++i) {
-        Real x = pcoord->x1v(i);
-        Real r1 = std::sqrt(SQR(x-x1)+SQR(y-y1)+SQR(z-z1));
-        Real r2 = std::sqrt(SQR(x-x2)+SQR(y-y2)+SQR(z-z2));
-        Real p1, p2, pot0;
-        if (r1 > r)
-          p1 = -G*m1/r1;
-        else
-          p1 = -G*PI*2.0/3.0*den1*(3.0*r*r-r1*r1);
-        if (r2 > r)
-          p2 = -G*m2/r2;
-        else
-          p2 = -G*PI*2.0/3.0*den2*(3.0*r*r-r2*r2);
-        pot0 = p1 + p2;
-        phydro->u(IM2,k,j,i) = pot0;
-      }
-    }
-  }
 }
 
 
@@ -196,7 +174,7 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
 //========================================================================================
 
 void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
-  Real x1 = 8.0/1024.0, x2 = -16.0/1024.0, y1 = 0.0, y2 = 0.0, z1 = 0.0, z2 = 0.0;
+  Real x1 = 6.0/1024.0, x2 = -12.0/1024.0, y1 = 0.0, y2 = 0.0, z1 = 0.0, z2 = 0.0;
   Real r = 6.0/1024.0;
   Real m1 = 2.0, m2 = 1.0;
   Real G = four_pi_G / (4.0 * PI);
@@ -210,96 +188,179 @@ void Mesh::UserWorkAfterLoop(ParameterInput *pin) {
 
   Real err1 = 0.0, err2 = 0.0;
 
-  for (int b=0; b<nblocal; ++b) {
-    pmb = my_blocks(b);
-    pmb->pgrav->phi.ZeroClear();
-  }
-  if (SELF_GRAVITY_ENABLED == 1) pfgrd->Solve(1,1);
-  else if (SELF_GRAVITY_ENABLED == 2) pmgrd->Solve(1);
+  if (nlim == 0) {
+    // timing measure after loop
+    int ncycle = pin->GetOrAddInteger("problem", "ncycle", 1);
+    if (Globals::my_rank == 0) {
+      std::cout << "=====================================================" << std::endl;
+      std::cout << "Call Poisson Solver  " << ncycle << " times          " << std::endl;
+      std::cout << "=====================================================" << std::endl;
+    }
 
-  for (int b = 0; b < nblocal; ++b) {
-    pmb = my_blocks(b);
-    Hydro *phydro = pmb->phydro;
-    Gravity *pgrav = pmb->pgrav;
-    Coordinates *pcoord = pmb->pcoord;
-    Real vol = pcoord->dx1f(is)*pcoord->dx2f(js)*pcoord->dx3f(ks);
-    for (int k = ks; k <= ke; ++k) {
-      for (int j = js; j <= je; ++j) {
-        for (int i = is; i <= ie; ++i) {
-          Real x = pcoord->x1v(i);
-          Real y = pcoord->x2v(j);
-          Real z = pcoord->x3v(k);
-          Real dx = pcoord->dx1f(i);
-          Real dy = pcoord->dx2f(j);
-          Real dz = pcoord->dx3f(k);
+#ifdef MPI_PARALLEL
+    MPI_Barrier(MPI_COMM_WORLD);
+#endif
+#ifdef OPENMP_PARALLEL
+    double omp_start_time = omp_get_wtime();
+#endif
+    clock_t tstart = clock();
 
-          Real r1 = sqrt(SQR(x-x1)+SQR(y-y1)+SQR(z-z1));
-          Real r2 = sqrt(SQR(x-x2)+SQR(y-y2)+SQR(z-z2));
-
-          Real ax = -(pgrav->phi(k,j,i+1)-pgrav->phi(k,j,i-1))/(2.0*dx);
-          Real ay = -(pgrav->phi(k,j+1,i)-pgrav->phi(k,j-1,i))/(2.0*dy);
-          Real az = -(pgrav->phi(k+1,j,i)-pgrav->phi(k-1,j,i))/(2.0*dz);
-
-          Real p1, p2, pot0, ax1, ay1, az1, ax2, ay2, az2, ax0, ay0, az0;
-          if (r1 > r) {
-            p1 = -G*m1/r1;
-            ax1 = -G*m1/(r1*r1*r1)*(x-x1);
-            ay1 = -G*m1/(r1*r1*r1)*(y-y1);
-            az1 = -G*m1/(r1*r1*r1)*(z-z1);
-          } else {
-            p1 = -G*PI*2.0/3.0*den1*(3.0*r*r-r1*r1);
-            ax1 = -G*PI*4.0/3.0*den1*(x-x1);
-            ay1 = -G*PI*4.0/3.0*den1*(y-y1);
-            az1 = -G*PI*4.0/3.0*den1*(z-z1);
-          }
-          if (r2 > r) {
-            p2 = -G*m2/r2;
-            ax2 = -G*m2/(r2*r2*r2)*(x-x2);
-            ay2 = -G*m2/(r2*r2*r2)*(y-y2);
-            az2 = -G*m2/(r2*r2*r2)*(z-z2);
-          } else {
-            p2 = -G*PI*2.0/3.0*den2*(3.0*r*r-r2*r2);
-            ax2 = -G*PI*4.0/3.0*den2*(x-x2);
-            ay2 = -G*PI*4.0/3.0*den2*(y-y2);
-            az2 = -G*PI*4.0/3.0*den2*(z-z2);
-          }
-          pot0 = p1 + p2;
-          ax0 = ax1 + ax2;
-          ay0 = ay1 + ay2;
-          az0 = az1 + az2;
-
-          Real perr = SQR((pot0 - pgrav->phi(k,j,i))/pot0);
-          Real aerr = (SQR(ax-ax0)+SQR(ay-ay0)+SQR(az-az0))
-                    / (SQR(ax0)+SQR(ay0)+SQR(az0));
-          phydro->u(IM1,k,j,i) = perr;
-          phydro->u(IM2,k,j,i) = pot0;
-          phydro->u(IM3,k,j,i) = aerr;
-          err1 += perr * vol;
-          err2 += aerr * vol;
+    AthenaArray<Real> nsol;
+    nsol.NewAthenaArray(nblocal,pmb->ncells3,pmb->ncells2,pmb->ncells1);
+    for (int b=0; b<nblocal; ++b) {
+      pmb = my_blocks(b);
+      for (int k=0; k< pmb->ncells3; ++k) {
+        for (int j=0; j< pmb->ncells2; ++j) {
+          for (int i=0; i< pmb->ncells1; ++i)
+            nsol(b,k,j,i)=pmb->pgrav->phi(k,j,i);
         }
       }
     }
-  }
+    for (int n=0; n < ncycle; n++) {
+      for (int b=0; b<nblocal; ++b) {
+        pmb = my_blocks(b);
+        pmb->pgrav->phi.ZeroClear();
+      }
+      if (SELF_GRAVITY_ENABLED == 1) pfgrd->Solve(1,1);
+      else if (SELF_GRAVITY_ENABLED == 2) pmgrd->Solve(1);
+    }
+
+#ifdef OPENMP_PARALLEL
+    double omp_time = omp_get_wtime() - omp_start_time;;
+#endif
+    clock_t tstop = clock();
+    float cpu_time = (tstop>tstart ? static_cast<Real>(tstop-tstart) : 1.0) /
+                     static_cast<Real>(CLOCKS_PER_SEC);
+    std::int64_t zones = GetTotalCells();
+    std::int64_t mb_zones = GetTotalCells()/nbtotal*nblist[Globals::my_rank];
+    double zc_cpus = static_cast<double>(zones*ncycle)/cpu_time;
+
+    if (Globals::my_rank == 0) {
+      std::cout << "Timing Possison Solver                               " << std::endl;
+      std::cout << "number of zones in Mesh = " << zones << std::endl;
+      std::cout << "Mesh configuration = " << mesh_size.nx1 << "x"
+                << mesh_size.nx2 << "x" << mesh_size.nx3 << std::endl;
+      std::cout << "number of zones in MeshBlock = " << mb_zones << std::endl;
+      std::cout << "MeshBlock configuration = " << pmb->block_size.nx1 << "x"
+                << pmb->block_size.nx2 << "x" << pmb->block_size.nx3 << std::endl;
+      std::cout << "number of processors = " << Globals::nranks << std::endl;
+      std::cout << "processor configuration = "
+                << nrbx1 << "x" << nrbx2 << "x" << nrbx3 << std::endl;
+      std::cout << "cpu time used  = " << cpu_time << std::endl;
+      std::cout << "cpu time used/cycle  = " << cpu_time/static_cast<Real>(ncycle)
+                << std::endl;
+      std::cout << "zone-cycles/cpu_second = " << zc_cpus << std::endl;
+#ifdef OPENMP_PARALLEL
+      std::cout << "=====================================================" << std::endl;
+      double zc_omps = static_cast<Real>(zones*ncycle)/omp_time;
+      std::cout << "omp number of threads = " << GetNumMeshThreads() << std::endl;
+      std::cout << "omp wtime used = " << omp_time << std::endl;
+      std::cout << "zone-cycles/omp_wsecond = " << zc_omps << std::endl;
+#endif
+      std::cout << "=====================================================" << std::endl;
+    }
+
+    Real temax = 0.0, nemax = 0.0, defmax = 0.0;
+    for (int b = 0; b < nblocal; ++b) {
+      pmb = my_blocks(b);
+      Hydro *phydro = pmb->phydro;
+      Gravity *pgrav = pmb->pgrav;
+      Coordinates *pcoord = pmb->pcoord;
+      Real vol = pcoord->dx1f(is)*pcoord->dx2f(js)*pcoord->dx3f(ks);
+      for (int k = ks; k <= ke; ++k) {
+        for (int j = js; j <= je; ++j) {
+          for (int i = is; i <= ie; ++i) {
+            Real x = pcoord->x1v(i);
+            Real y = pcoord->x2v(j);
+            Real z = pcoord->x3v(k);
+            Real dx = pcoord->dx1f(i);
+            Real dy = pcoord->dx2f(j);
+            Real dz = pcoord->dx3f(k);
+
+            Real r1 = sqrt(SQR(x-x1)+SQR(y-y1)+SQR(z-z1));
+            Real r2 = sqrt(SQR(x-x2)+SQR(y-y2)+SQR(z-z2));
+
+            Real ax = -(pgrav->phi(k,j,i+1)-pgrav->phi(k,j,i-1))/(2.0*dx);
+            Real ay = -(pgrav->phi(k,j+1,i)-pgrav->phi(k,j-1,i))/(2.0*dy);
+            Real az = -(pgrav->phi(k+1,j,i)-pgrav->phi(k-1,j,i))/(2.0*dz);
+
+            Real p1, p2, pot0, ax1, ay1, az1, ax2, ay2, az2, ax0, ay0, az0;
+            if (r1 > r) {
+              p1 = -G*m1/r1;
+              ax1 = -G*m1/(r1*r1*r1)*(x-x1);
+              ay1 = -G*m1/(r1*r1*r1)*(y-y1);
+              az1 = -G*m1/(r1*r1*r1)*(z-z1);
+            } else {
+              p1 = -G*PI*2.0/3.0*den1*(3.0*r*r-r1*r1);
+              ax1 = -G*PI*4.0/3.0*den1*(x-x1);
+              ay1 = -G*PI*4.0/3.0*den1*(y-y1);
+              az1 = -G*PI*4.0/3.0*den1*(z-z1);
+            }
+            if (r2 > r) {
+              p2 = -G*m2/r2;
+              ax2 = -G*m2/(r2*r2*r2)*(x-x2);
+              ay2 = -G*m2/(r2*r2*r2)*(y-y2);
+              az2 = -G*m2/(r2*r2*r2)*(z-z2);
+            } else {
+              p2 = -G*PI*2.0/3.0*den2*(3.0*r*r-r2*r2);
+              ax2 = -G*PI*4.0/3.0*den2*(x-x2);
+              ay2 = -G*PI*4.0/3.0*den2*(y-y2);
+              az2 = -G*PI*4.0/3.0*den2*(z-z2);
+            }
+            pot0 = p1 + p2;
+            ax0 = ax1 + ax2;
+            ay0 = ay1 + ay2;
+            az0 = az1 + az2;
+
+            Real perr = (pot0 - pgrav->phi(k,j,i))/pot0;
+            Real perr2 = (nsol(b,k,j,i) - pgrav->phi(k,j,i))/pot0;
+            Real aerr = std::sqrt((SQR(ax-ax0)+SQR(ay-ay0)+SQR(az-az0))
+                      / (SQR(ax0)+SQR(ay0)+SQR(az0)));
+            phydro->u(IM1,k,j,i) = perr;
+            phydro->u(IM2,k,j,i) = perr2;
+            phydro->u(IM3,k,j,i) = aerr;
+            phydro->u(IEN,k,j,i) = pot0;
+            err1 += std::abs(perr) * vol;
+            err2 += aerr * vol;
+            if (std::abs(z) < dz) {
+              if (std::abs(perr) > temax)
+                temax = std::abs(perr);
+              if (std::abs(perr2) > nemax)
+                nemax = std::abs(perr2);
+              if (std::abs(pgrav->def(k,j,i)) > defmax)
+                defmax = std::abs(pgrav->def(k,j,i));
+            }
+          }
+        }
+      }
+    }
 #ifdef MPI_PARALLEL
-  MPI_Allreduce(MPI_IN_PLACE, &err1, 1, MPI_ATHENA_REAL, MPI_SUM, MPI_COMM_WORLD);
-  MPI_Allreduce(MPI_IN_PLACE, &err2, 1, MPI_ATHENA_REAL, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(MPI_IN_PLACE, &err1, 1, MPI_ATHENA_REAL, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(MPI_IN_PLACE, &err2, 1, MPI_ATHENA_REAL, MPI_SUM, MPI_COMM_WORLD);
+    MPI_Allreduce(MPI_IN_PLACE, &temax, 1, MPI_ATHENA_REAL, MPI_MAX, MPI_COMM_WORLD);
+    MPI_Allreduce(MPI_IN_PLACE, &nemax, 1, MPI_ATHENA_REAL, MPI_MAX, MPI_COMM_WORLD);
+    MPI_Allreduce(MPI_IN_PLACE, &defmax, 1, MPI_ATHENA_REAL, MPI_MAX, MPI_COMM_WORLD);
 #endif
 
-  Real x1size = mesh_size.x1max - mesh_size.x1min;
-  Real x2size = mesh_size.x2max - mesh_size.x2min;
-  Real x3size = mesh_size.x3max - mesh_size.x3min;
-  Real tvol = x1size * x2size * x3size;
-  err1 /= tvol;
-  err2 /= tvol;
-  err1 = std::sqrt(err1);
-  err2 = std::sqrt(err2);
-  if (Globals::my_rank == 0) {
-    std::cout << std::scientific
-              << std::setprecision(std::numeric_limits<Real>::max_digits10 - 1);
-    std::cout << "=====================================================" << std::endl;
-    std::cout << "Potential    L2 : " << err1 << std::endl;
-    std::cout << "Acceleration L2 : " << err2 << std::endl;
-    std::cout << "=====================================================" << std::endl;
+    Real x1size = mesh_size.x1max - mesh_size.x1min;
+    Real x2size = mesh_size.x2max - mesh_size.x2min;
+    Real x3size = mesh_size.x3max - mesh_size.x3min;
+    Real tvol = x1size * x2size * x3size;
+    err1 /= tvol;
+    err2 /= tvol;
+    err1 = std::sqrt(err1);
+    err2 = std::sqrt(err2);
+    if (Globals::my_rank == 0) {
+      std::cout << std::scientific
+                << std::setprecision(std::numeric_limits<Real>::max_digits10 - 1);
+      std::cout << "=====================================================" << std::endl;
+      std::cout << "Potential    L2       : " << err1 << std::endl;
+      std::cout << "Acceleration L2       : " << err2 << std::endl;
+      std::cout << "Max True Error        : " << temax << std::endl;
+      std::cout << "Max Discretized Error : " << nemax << std::endl;
+      std::cout << "Max Defect            : " << defmax << std::endl;
+      std::cout << "=====================================================" << std::endl;
+    }
   }
 
   return;
--- src/pgen/collapse.cpp
+++ src/pgen/collapse.cpp
@@ -32,6 +32,10 @@
 #error "This problem generator requires Multigrid gravity solver."
 #endif
 
+#ifdef MPI_PARALLEL
+#include <mpi.h>
+#endif
+
 
 namespace {
 
@@ -52,14 +56,15 @@ constexpr Real yr   = 3.15569e7;    // year, s
 constexpr Real G    = 6.67259e-8;   // gravitational constant, dyn cm^2 g^-2
 
 // units in cgs
-Real m0, v0, t0, l0, rho0;
+Real m0, v0, t0, l0, rho0, gauss;
 
 // parameters and derivatives
-Real mass, temp, f, rhocrit;
+Real mass, temp, f, rhocrit, omega, bz, mu, amp;
 
 // AMR parameter
 Real njeans; // Real is used intentionally 
 
+Real totalm;
 }
 
 // Mask the density outside the initial sphere
@@ -94,15 +99,17 @@ int JeansCondition(MeshBlock *pmb) {
         for (int j = pmb->js-NGHOST; j<=pmb->je+NGHOST; ++j) {
           for (int i = pmb->is-NGHOST; i<=pmb->ie+NGHOST; ++i) {
             Real v = std::sqrt(gamma * pmb->phydro->w(IPR,k,j,i)
-                             +(SQR(pmb->pfield->bcc(IB1,k,j,i))
-                             + SQR(pmb->pfield->bcc(IB2,k,j,i))
-                             + SQR(pmb->pfield->bcc(IB3,k,j,i)))
-                             / pmb->phydro->w(IDN,k,j,i));
-            Real nj = fac * v / std::sqrt(pmb->phydro->w(IDN,k,j,i));
+                              / pmb->phydro->w(IDN,k,j,i))+
+                   + std::sqrt((SQR(pmb->pfield->bcc(IB1,k,j,i))
+                              + SQR(pmb->pfield->bcc(IB2,k,j,i))
+                              + SQR(pmb->pfield->bcc(IB3,k,j,i)))
+                              / pmb->phydro->w(IDN,k,j,i));
+            Real nj = v / std::sqrt(pmb->phydro->w(IDN,k,j,i));
             njmin = std::min(njmin, nj);
           }
         }
       }
+      njmin *= fac;
     } else {
       const Real cs = pmb->peos->GetIsoSoundSpeed();
       const Real fac = 2.0 * pi / dx;
@@ -113,11 +120,12 @@ int JeansCondition(MeshBlock *pmb) {
                                    + SQR(pmb->pfield->bcc(IB2,k,j,i))
                                    + SQR(pmb->pfield->bcc(IB3,k,j,i)))
                                    / pmb->phydro->w(IDN,k,j,i));
-            Real nj = fac * v / std::sqrt(pmb->phydro->w(IDN,k,j,i));
+            Real nj = v / std::sqrt(pmb->phydro->w(IDN,k,j,i));
             njmin = std::min(njmin, nj);
           }
         }
       }
+      njmin *= fac;
     }
   } else {
     if (NON_BAROTROPIC_EOS) {
@@ -126,23 +134,25 @@ int JeansCondition(MeshBlock *pmb) {
       for (int k = pmb->ks-NGHOST; k<=pmb->ke+NGHOST; ++k) {
         for (int j = pmb->js-NGHOST; j<=pmb->je+NGHOST; ++j) {
           for (int i = pmb->is-NGHOST; i<=pmb->ie+NGHOST; ++i) {
-            Real nj = fac * std::sqrt(pmb->phydro->w(IPR,k,j,i))
+            Real nj = std::sqrt(pmb->phydro->w(IPR,k,j,i))
                               / pmb->phydro->w(IDN,k,j,i);
             njmin = std::min(njmin, nj);
           }
         }
       }
+      njmin *= fac;
     } else {
       const Real cs = pmb->peos->GetIsoSoundSpeed();
       const Real fac = 2.0 * pi / dx;
       for (int k = pmb->ks-NGHOST; k<=pmb->ke+NGHOST; ++k) {
         for (int j = pmb->js-NGHOST; j<=pmb->je+NGHOST; ++j) {
           for (int i = pmb->is-NGHOST; i<=pmb->ie+NGHOST; ++i) {
-            Real nj = fac * cs / std::sqrt(pmb->phydro->w(IDN,k,j,i));
+            Real nj = cs / std::sqrt(pmb->phydro->w(IDN,k,j,i));
             njmin = std::min(njmin, nj);
           }
         }
       }
+      njmin *= fac;
     }
   }
   if (njmin < njeans)
@@ -165,6 +175,25 @@ void Cooling(MeshBlock *pmb, const Real time, const Real dt,
              AthenaArray<Real> &cons_scalar) {
   const Real gm1 = pmb->peos->GetGamma() - 1.0;
   const Real igm1 = 1.0 / gm1;
+
+  // Fixed boundary condition outside the initial core
+  for (int k=pmb->ks; k<=pmb->ke; ++k) {
+    Real z = pmb->pcoord->x3v(k);
+    for (int j=pmb->js; j<=pmb->je; ++j) {
+      Real y = pmb->pcoord->x2v(j);
+      for (int i=pmb->is; i<=pmb->ie; ++i) {
+        Real x = pmb->pcoord->x1v(i);
+        Real r = std::sqrt(SQR(x) + SQR(y) + SQR(z));
+        if (r > rc) {
+          cons(IM1,k,j,i) = 0.0;
+          cons(IM2,k,j,i) = 0.0;
+          cons(IM3,k,j,i) = 0.0;
+        }
+      }
+    }
+  }
+
+  // Set the internal energy to follow the barotropic relation
   if (MAGNETIC_FIELDS_ENABLED) {
     for (int k=pmb->ks; k<=pmb->ke; ++k) {
       for (int j=pmb->js; j<=pmb->je; ++j) {
@@ -173,7 +202,7 @@ void Cooling(MeshBlock *pmb, const Real time, const Real dt,
                   * (SQR(cons(IM1,k,j,i)) + SQR(cons(IM2,k,j,i)) + SQR(cons(IM3,k,j,i)));
           Real me = 0.5*(SQR(bcc(IB1,k,j,i)) + SQR(bcc(IB2,k,j,i)) + SQR(bcc(IB3,k,j,i)));
           Real te = igm1 * cons(IDN,k,j,i)
-                  * std::max(1.0, std::pow(cons(IDN,k,j,i)/rhocrit, gm1));
+                  * std::sqrt(1.0+std::pow(cons(IDN,k,j,i)/rhocrit, 2.0*gm1));
           cons(IEN,k,j,i) = te + ke + me;
         }
       }
@@ -185,7 +214,7 @@ void Cooling(MeshBlock *pmb, const Real time, const Real dt,
           Real ke = 0.5 / cons(IDN,k,j,i)
                   * (SQR(cons(IM1,k,j,i)) + SQR(cons(IM2,k,j,i)) + SQR(cons(IM3,k,j,i)));
           Real te = igm1 * cons(IDN,k,j,i)
-                  * std::max(1.0, std::pow(cons(IDN,k,j,i)/rhocrit, gm1));
+                  * std::sqrt(1.0+std::pow(cons(IDN,k,j,i)/rhocrit, 2.0*gm1));
           cons(IEN,k,j,i) = te + ke;
         }
       }
@@ -205,25 +234,38 @@ void Cooling(MeshBlock *pmb, const Real time, const Real dt,
 
 void Mesh::InitUserMeshData(ParameterInput *pin) {
   SetFourPiG(four_pi_G); // 4piG = 1.0
-  mass = pin->GetReal("problem", "mass");
+  mass = pin->GetReal("problem", "mass"); // solar mass
   temp = pin->GetReal("problem", "temperature");
   f = pin->GetReal("problem", "f"); // Density enhancement factor; f = 1 is critical
-  m0 = mass * msun / (bemass*f);
-  v0 = cs10 * std::sqrt(temp/10.0);
+  amp = pin->GetOrAddReal("problem", "amp", 0.0); // perturbation amplitude
+  mu = pin->GetOrAddReal("problem", "mu", 0.0); // micro gauss
+  m0 = mass * msun / (bemass*f); // total mass = 1.0
+  v0 = cs10 * std::sqrt(temp/10.0); // cs at 10K = 1.0
   rho0 = (v0*v0*v0*v0*v0*v0) / (m0*m0) /(64.0*pi*pi*pi*G*G*G);
-  t0 = 1.0/std::sqrt(4.0*pi*G*rho0);
+  t0 = 1.0/std::sqrt(4.0*pi*G*rho0); // sqrt(1/4pi G rho0) = 1.0
   l0 = v0 * t0;
+  gauss = std::sqrt(rho0*v0*v0*4.0*pi);
   rhocrit = pin->GetReal("problem", "rhocrit") / rho0;
-  Real tff = sqrt(3.0/8.0)*pi;
+  totalm = 0.0;
+  Real tff = std::sqrt(3.0/8.0/f)*pi;
+  Real omegatff = pin->GetOrAddReal("problem", "omegatff", 0.0);
+  omega = omegatff/tff;
+  Real mucrit1 = 0.53/(3.0*pi)*std::sqrt(5.0/G);
+  Real mucrit2 = 1.0/(2.0*pi*std::sqrt(G));
+  bz = mass*msun/mucrit1/mu/pi/SQR(rc*l0)/gauss;
+  Real bzug = bz*gauss*1e6;
   if (Globals::my_rank == 0 && ncycle == 0) {
     std::cout << std::endl
       << "---  Dimensional parameters of the simulation  ---" << std::endl
       << "Total mass          : " << mass      << " \t\t[Msun]" << std::endl
       << "Initial temperature : " << temp      << " \t\t[K]" << std::endl
       << "Sound speed         : " << v0        << " \t\t[cm s^-1]" << std::endl 
-      << "Central density     : " << rho0      << " \t[g cm^-3]" << std::endl
+      << "Central density     : " << rho0*f    << " \t[g cm^-3]" << std::endl
       << "Cloud radius        : " << rc*l0/au  << " \t\t[au]" << std::endl
       << "Free fall time      : " << tff*t0/yr << " \t\t[yr]" << std::endl
+      << "Angular velocity    : " << omega/t0  << " \t[s^-1]" << std::endl
+      << "Angular velocity    : " << omega/t0*yr << " \t[yr^-1]" << std::endl
+      << "Magnetic field      : " << bzug      << " \t\t[uGauss]" << std::endl
       << "Density Enhancement : " << f         << std::endl << std::endl
       << "---   Normalization Units of the simulation    ---" << std::endl
       << "Mass                : " << m0        << " \t[g]" << std::endl
@@ -234,13 +276,17 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
       << "Time                : " << t0        << " \t[s]" << std::endl
       << "Time                : " << t0/yr     << " \t\t[yr]" << std::endl
       << "Velocity            : " << v0        << " \t\t[cm s^-1]" << std::endl
-      << "Density             : " << rho0      << " \t[g cm^-3]" << std::endl << std::endl
+      << "Density             : " << rho0      << " \t[g cm^-3]" << std::endl
+      << "Magnetic field      : " << gauss     << " \t[Gauss]" << std::endl << std::endl
       << "--- Dimensionless parameters of the simulation ---" << std::endl
       << "Total mass          : " << bemass*f  << std::endl
       << "Sound speed at " << temp << " K : "  << 1.0 << std::endl
       << "Central density     : " << 1.0       << std::endl
       << "Cloud radius        : " << rc        << std::endl
-      << "Free fall time      : " << tff       << std::endl << std::endl;
+      << "Free fall time      : " << tff       << std::endl
+      << "m=2 perturbation    : " << amp       << std::endl
+      << "Omega * tff         : " << omegatff << std::endl
+      << "Mass-to-flux ratio  : " << mu        << std::endl << std::endl;
   }
 
   EnrollUserMGGravitySourceMaskFunction(SourceMask);
@@ -263,6 +309,7 @@ void Mesh::InitUserMeshData(ParameterInput *pin) {
 
 void MeshBlock::ProblemGenerator(ParameterInput *pin) {
   Real igm1 = 1.0 / (peos->GetGamma() - 1.0);
+  Real dx = pcoord->dx1f(is);
 
   for (int k=ks; k<=ke; ++k) {
     Real z = pcoord->x3v(k);
@@ -272,12 +319,54 @@ void MeshBlock::ProblemGenerator(ParameterInput *pin) {
         Real x = pcoord->x1v(i);
         Real r = std::sqrt(SQR(x) + SQR(y) + SQR(z));
         r = std::min(r, rc); // pressure confinement - constant beyond the cloud radius
-        phydro->u(IDN,k,j,i) = BEProfile(r);
-        phydro->u(IM1,k,j,i) = 0.0;
-        phydro->u(IM2,k,j,i) = 0.0;
-        phydro->u(IM3,k,j,i) = 0.0;
+        phydro->u(IDN,k,j,i) = f * BEProfile(r)
+                             * (1.0+amp*SQR(r)/SQR(rc)*cos(2.0*std::atan2(y,x)));
+        if (r < rc) {
+          phydro->u(IM1,k,j,i) =  phydro->u(IDN,k,j,i)*omega*y;
+          phydro->u(IM2,k,j,i) = -phydro->u(IDN,k,j,i)*omega*x;
+          phydro->u(IM3,k,j,i) = 0.0;
+        } else {
+          phydro->u(IM1,k,j,i) = 0.0;
+          phydro->u(IM2,k,j,i) = 0.0;
+          phydro->u(IM3,k,j,i) = 0.0;
+        }
         if (NON_BAROTROPIC_EOS)
-          phydro->u(IEN,k,j,i) = igm1 * phydro->u(IDN,k,j,i); // c_s = 1
+          phydro->u(IEN,k,j,i) = igm1 * phydro->u(IDN,k,j,i) // c_s = 1
+                               + 0.5*(SQR(phydro->u(IM1,k,j,i))+SQR(phydro->u(IM2,k,j,i))
+                                     +SQR(phydro->u(IM3,k,j,i)))/SQR(phydro->u(IDN,k,j,i));
+      }
+    }
+  }
+  // initialize interface B, uniform
+  if (MAGNETIC_FIELDS_ENABLED) {
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=js; j<=je; ++j) {
+        for (int i=is; i<=ie+1; ++i) {
+          pfield->b.x1f(k,j,i) = 0.0;
+        }
+      }
+    }
+    for (int k=ks; k<=ke; ++k) {
+      for (int j=js; j<=je+1; ++j) {
+        for (int i=is; i<=ie; ++i) {
+          pfield->b.x2f(k,j,i) = 0.0;
+        }
+      }
+    }
+    for (int k=ks; k<=ke+1; ++k) {
+      for (int j=js; j<=je; ++j) {
+        for (int i=is; i<=ie; ++i) {
+          pfield->b.x3f(k,j,i) = bz;
+        }
+      }
+    }
+    if (NON_BAROTROPIC_EOS) {
+      for (int k=ks; k<=ke; ++k) {
+        for (int j=js; j<=je; ++j) {
+          for (int i=is; i<=ie; ++i) {
+            phydro->u(IEN,k,j,i) += 0.5*SQR(bz);
+          }
+        }
       }
     }
   }
