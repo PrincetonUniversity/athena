946ad458f66c59547d4181995eb30382fa71414b
Munan Gong <munan@princeton.edu>
2023-06-29 19:52:55 +0200

    changed filename to be consistent with other chemistry problem generators

--- src/pgen/chem_turb.cpp
+++ src/pgen/chem_turb.cpp
@@ -3,7 +3,7 @@
 // Copyright(C) 2014 James M. Stone <jmstone@princeton.edu> and other code contributors
 // Licensed under the 3-clause BSD License, see LICENSE file for details
 //========================================================================================
-//! \file turb_chem.cpp
+//! \file chem_turb.cpp
 //! \brief Problem generator for turbulence driver with chemistry
 
 // C headers
