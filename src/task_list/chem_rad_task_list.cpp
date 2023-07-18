471e8bb46d1318af5886c9fb734966a7a86960a1
Kyle Gerard Felker <felker@anl.gov>
2023-07-18 16:25:16 -0400

    Header alpha include

--- src/task_list/chem_rad_task_list.cpp
+++ src/task_list/chem_rad_task_list.cpp
@@ -14,8 +14,8 @@
 
 // Athena++ classes headers
 #include "../athena.hpp"
-#include "../chem_rad/integrators/rad_integrators.hpp"
 #include "../chem_rad/chem_rad.hpp"
+#include "../chem_rad/integrators/rad_integrators.hpp"
 #include "../defs.hpp"
 #include "../mesh/mesh.hpp"
 #include "chem_rad_task_list.hpp"
@@ -273,4 +273,3 @@ TaskStatus ChemRadiationIntegratorTaskList::ConstRadiation(MeshBlock *pmb, int s
   }
   return TaskStatus::success;
 }
-
