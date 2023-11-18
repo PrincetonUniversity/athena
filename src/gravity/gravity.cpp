PrincetonUniversity/athena

commit 73bf8182ace1d54f347ed2d5746a1a482613272f

Fix error: ‘nbx’ may be used uninitialized in this function, etc.

src/multigrid/multigrid.cpp
@@ -57,20 +57,24 @@ Multigrid::Multigrid(MultigridDriver *pmd, MeshBlock *pmb, int invar, int nghost
     for (int i = 0; i < 6; ++i)
       mg_block_bcs_[i] = pmy_driver_->mg_mesh_bcs_[i];
   }
-  rdx_=(size_.x1max-size_.x1min)/static_cast<Real>(size_.nx1);
-  rdy_=(size_.x2max-size_.x2min)/static_cast<Real>(size_.nx2);
-  rdz_=(size_.x3max-size_.x3min)/static_cast<Real>(size_.nx3);
+  rdx_ = (size_.x1max-size_.x1min)/static_cast<Real>(size_.nx1);
+  rdy_ = (size_.x2max-size_.x2min)/static_cast<Real>(size_.nx2);
+  rdz_ = (size_.x3max-size_.x3min)/static_cast<Real>(size_.nx3);
 
   nlevel_=0;
   if (pmy_block_ == nullptr) { // root
     int nbx, nby, nbz;
-    for (int l = 0; l < 20; l++) {
+    //nbx = nby = nbz = 1;
+    nbx = size_.nx1;
+    nby = size_.nx2;
+    nbz = size_.nx3;
+    for (int l = 1; l < 20; l++) {
       if (size_.nx1%(1<<l) == 0 && size_.nx2%(1<<l) == 0 && size_.nx3%(1<<l) == 0) {
-        nbx=size_.nx1/(1<<l), nby=size_.nx2/(1<<l), nbz=size_.nx3/(1<<l);
-        nlevel_=l+1;
+        nbx = size_.nx1/(1<<l), nby = size_.nx2/(1<<l), nbz = size_.nx3/(1<<l);
+        nlevel_ = l+1;
       }
     }
-    int nmaxr=std::max(nbx, std::max(nby, nbz));
+    int nmaxr = std::max(nbx, std::max(nby, nbz));
     // int nminr=std::min(nbx, std::min(nby, nbz)); // unused variable
     if (nmaxr != 1 && Globals::my_rank == 0) {
       std::cout
@@ -1344,5 +1348,3 @@ void MGCoordinates::CalculateMGCoordinates(const RegionSize &size, int ll, int n
   for (int k = 0; k < ncz+2*ngh; ++k)
     x3v(k) = 0.5*(x3f(k)+x3f(k+1));
 }
-
-

commit c18b02e87937bb1649f5a9915a08c028ffa117b6

Fixed a bug in Multigrid introduced in 73bf8182ace1d54f347ed2d5746a1a482613272f
Also moved MGGravity objects from MeshBlock to Gravity.

src/gravity/gravity.cpp
@@ -52,6 +52,7 @@ Gravity::Gravity(MeshBlock *pmb, ParameterInput *pin) :
   }
 
   if (SELF_GRAVITY_ENABLED == 2) {
+    pmg = new MGGravity(pmb->pmy_mesh->pmgrd, pmb);
     output_defect = pin->GetOrAddBoolean("gravity", "output_defect", false);
     if (output_defect)
       def.NewAthenaArray(pmb->ncells3, pmb->ncells2, pmb->ncells1);
@@ -82,6 +83,14 @@ Gravity::Gravity(MeshBlock *pmb, ParameterInput *pin) :
   pmb->pbval->pgbvar = &gbvar;
 }
 
+
+//----------------------------------------------------------------------------------------
+//! \fn Gravity::~Gravity()
+//! \brief Gravity destructor
+Gravity::~Gravity() {
+  if (SELF_GRAVITY_ENABLED == 2)
+    delete pmg;
+}
 
 //----------------------------------------------------------------------------------------
 //! \fn Gravity::SaveFaceBoundaries()

src/gravity/gravity.hpp
@@ -32,6 +32,7 @@ class MGGravityDriver;
 class Gravity {
  public:
   Gravity(MeshBlock *pmb, ParameterInput *pin);
+  ~Gravity();
 
   MeshBlock* pmy_block;  // ptr to MeshBlock containing this Field
   AthenaArray<Real> phi, coarse_phi;   // gravitational potential
@@ -52,6 +53,7 @@ class Gravity {
   friend class MGGravityDriver;
 
  private:
+  MGGravity *pmg;
   bool gravity_tensor_momentum_;
   bool gravity_tensor_energy_;
   AthenaArray<Real> fbuf_[6];

src/gravity/mg_gravity.cpp
@@ -168,7 +168,7 @@ void MGGravityDriver::Solve(int stage) {
   // Construct the Multigrid array
   vmg_.clear();
   for (int i = 0; i < pmy_mesh_->nblocal; ++i)
-    vmg_.push_back(pmy_mesh_->my_blocks(i)->pmg);
+    vmg_.push_back(pmy_mesh_->my_blocks(i)->pgrav->pmg);
 
   // load the source
 #pragma omp parallel for num_threads(nthreads_)

src/mesh/mesh.hpp
@@ -52,7 +52,6 @@ class Field;
 class Particles;
 class PassiveScalars;
 class Gravity;
-class MGGravity;
 class MGGravityDriver;
 class EquationOfState;
 class FFTDriver;
@@ -123,7 +122,6 @@ class MeshBlock {
   CosmicRay *pcr;
   Field *pfield;
   Gravity *pgrav;
-  MGGravity* pmg;
   PassiveScalars *pscalars;
   EquationOfState *peos;
   OrbitalAdvection *porb;

src/mesh/meshblock.cpp
@@ -218,8 +218,6 @@ MeshBlock::MeshBlock(int igid, int ilid, LogicalLocation iloc, RegionSize input_
     // if (this->grav_block)
     pgrav = new Gravity(this, pin);
     pbval->AdvanceCounterPhysID(CellCenteredBoundaryVariable::max_phys_id);
-    if (SELF_GRAVITY_ENABLED == 2)
-      pmg = new MGGravity(pmy_mesh->pmgrd, this);
   }
   if (NSCALARS > 0) {
     // if (this->scalars_block)
@@ -418,8 +416,6 @@ MeshBlock::MeshBlock(int igid, int ilid, Mesh *pm, ParameterInput *pin,
     // if (this->grav_block)
     pgrav = new Gravity(this, pin);
     pbval->AdvanceCounterPhysID(CellCenteredBoundaryVariable::max_phys_id);
-    if (SELF_GRAVITY_ENABLED == 2)
-      pmg = new MGGravity(pmy_mesh->pmgrd, this);
   }
 
   if (NSCALARS > 0) {
@@ -552,7 +548,6 @@ MeshBlock::~MeshBlock() {
   delete peos;
   delete porb;
   if (SELF_GRAVITY_ENABLED) delete pgrav;
-  if (SELF_GRAVITY_ENABLED == 2) delete pmg;
   if (NSCALARS > 0) delete pscalars;
 
   if (NR_RADIATION_ENABLED || IM_RADIATION_ENABLED) delete pnrrad;

src/multigrid/multigrid.cpp
@@ -61,14 +61,10 @@ Multigrid::Multigrid(MultigridDriver *pmd, MeshBlock *pmb, int invar, int nghost
   rdy_ = (size_.x2max-size_.x2min)/static_cast<Real>(size_.nx2);
   rdz_ = (size_.x3max-size_.x3min)/static_cast<Real>(size_.nx3);
 
-  nlevel_=0;
+  nlevel_ = 0;
   if (pmy_block_ == nullptr) { // root
-    int nbx, nby, nbz;
-    //nbx = nby = nbz = 1;
-    nbx = size_.nx1;
-    nby = size_.nx2;
-    nbz = size_.nx3;
-    for (int l = 1; l < 20; l++) {
+    int nbx = 0, nby = 0, nbz = 0;
+    for (int l = 0; l < 20; l++) {
       if (size_.nx1%(1<<l) == 0 && size_.nx2%(1<<l) == 0 && size_.nx3%(1<<l) == 0) {
         nbx = size_.nx1/(1<<l), nby = size_.nx2/(1<<l), nbz = size_.nx3/(1<<l);
         nlevel_ = l+1;