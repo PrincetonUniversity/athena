3a22cffd9b23ff18ee3393cbbca4a971f934ff3d
mariareneemf <mrmeza@astr-pella.astro.virginia.edu>
2024-04-25 22:37:13 -0400

    Add area-averaged coordinates for MHD with AMR

--- src/coordinates/spherical_polar.cpp
+++ src/coordinates/spherical_polar.cpp
@@ -131,14 +131,16 @@ SphericalPolar::SphericalPolar(MeshBlock *pmb, ParameterInput *pin, bool flag)
       x2s1(jl) = x2s3(jl) = x2v(jl);
     } else {
       for (int j=jl-ng; j<=ju+ng; ++j) {
-        x2s1(j) = x2s3(j) = x2v(j);
+        x2s1(j) = (std::sin(x2f(j+1)) - x2f(j+1)*std::cos(x2f(j+1)) - std::sin(x2f(j)) 
+                  + x2f(j)*std::cos(x2f(j)))/(std::cos(x2f(j)) - std::cos(x2f(j+1)));
+	x2s3(j) = 0.5*(x2f(j+1) + x2f(j));
       }
     }
     if (pmb->block_size.nx3 == 1) {
       x3s1(kl) = x3s2(kl) = x3v(kl);
     } else {
       for (int k=kl-ng; k<=ku+ng; ++k) {
-        x3s1(k) = x3s2(k) = x3v(k);
+        x3s1(k) = x3s2(k) = 0.5*(x3f(k+1) + x3f(k));
       }
     }
   }
