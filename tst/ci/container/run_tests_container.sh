8eebdabbfb53522f814acb91d8554e503e97f747
Kyle Gerard Felker <felker@anl.gov>
2024-06-06 11:29:15 -0500

    Remove explicit Travis mentions

--- tst/ci/container/run_tests_container.sh
+++ tst/ci/container/run_tests_container.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
 
+# Script originally used for testing on Travis CI osx and Ubuntu containers;
+# could be modified to be useful for Docker, Apptainer, etc. containers for testing
+OS_NAME=osx   #linux, ubuntu
+BUILD_DIR=$HOME/build
+
 # Terminate script at first cmd w/ non-zero exit status & echo commands before executing (-v) as lines are read
 set -ev # -x # for tracing/debugging commands after variable expansion
 # Do not use "python run_tests.py" to run all tests, so that:
-# - Each set/directory of tests are timed separately (although entire script is timed as one unit in Travis CI)
+# - Each set/directory of tests are timed separately
 # - Script fails after first broken set of tests
 # (Could alternatively group sets of tests with && operator)
 
@@ -11,12 +16,12 @@ set -ev # -x # for tracing/debugging commands after variable expansion
 cd tst/regression/
 
 if [ "$MPI_CHOICE" == "openmpi" ]; then
-    PATH=$TRAVIS_BUILD_DIR/openmpi/bin/:$PATH
+    PATH=$BUILD_DIR/openmpi/bin/:$PATH
     MPI_OPTS=--oversubscribe
     # Disable OpenMPI 3.1 vader CMA due to namespace permission issues on Travis CI / Docker containers
     export OMPI_MCA_btl_vader_single_copy_mechanism=none
 else
-    PATH=$TRAVIS_BUILD_DIR/mpich/bin/:$PATH
+    PATH=$BUILD_DIR/mpich/bin/:$PATH
 fi
 
 # --silent option refers only to stdout of Makefile calls for condensed build logs. Don't use with pgen_compile.py
@@ -25,17 +30,18 @@ time python3 run_tests.py pgen/pgen_compile --config=--cxx=$TEMP_CXX -c=--ccmd=$
 time python3 run_tests.py pgen/hdf5_reader_serial --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD -v
 
 # need to switch serial compiler to Homebrew's GCC instead of /usr/bin/gcc -> Apple Clang for OpenMP functionality
-if [ "$TRAVIS_OS_NAME" == "osx" ]; then
+if [ "$OS_NAME" == "osx" ]; then
     time python3 run_tests.py mpi --config=--cxx=$TEMP_CXX -c=--ccmd=$TEMP_CCMD --mpirun_opts=$MPI_OPTS --silent
-    # TODO(felker): improve selection of 'gcc-9' so when 'brew install gcc' formula instead installs gcc-10, this won't break
-    export OMPI_CC=/usr/local/bin/gcc-9
-    export OMPI_CXX=/usr/local/bin/g++-9
-    export MPICH_CC=/usr/local/bin/gcc-9
-    export MPICH_CXX=/usr/local/bin/g++-9
-    time python3 run_tests.py hybrid --config=--cxx=g++ --config=--ccmd=/usr/local/bin/g++-9 \
+    # TODO(felker): improve selection of 'gcc-14' so when 'brew install gcc' formula instead installs gcc-10, this won't break
+    # TODO(felker): note, these hardcoded paths were only true for x86_64 macOS systems; arm64 uses /opt/homebrew/bin/gcc-14
+    export OMPI_CC=/usr/local/bin/gcc-14
+    export OMPI_CXX=/usr/local/bin/g++-14
+    export MPICH_CC=/usr/local/bin/gcc-14
+    export MPICH_CXX=/usr/local/bin/g++-14
+    time python3 run_tests.py hybrid --config=--cxx=g++ --config=--ccmd=/usr/local/bin/g++-14 \
 	 --config=--mpiccmd='mpicxx -DMPICH_SKIP_MPICXX -DOMPI_SKIP_MPICXX' --mpirun_opts=$MPI_OPTS --silent
-    time python3 run_tests.py omp --config=--cxx=g++ --config=--ccmd=/usr/local/bin/g++-9 --silent
-    time python3 run_tests.py grav --config=--cxx=g++ --config=--ccmd=/usr/local/bin/g++-9 \
+    time python3 run_tests.py omp --config=--cxx=g++ --config=--ccmd=/usr/local/bin/g++-14 --silent
+    time python3 run_tests.py grav --config=--cxx=g++ --config=--ccmd=/usr/local/bin/g++-14 \
 	 --config=--mpiccmd='mpicxx -DMPICH_SKIP_MPICXX -DOMPI_SKIP_MPICXX' --mpirun_opts=$MPI_OPTS --silent # requires FFTW library
 else
     export OMPI_CC=$TEMP_CCMD
