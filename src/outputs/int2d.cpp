696284cb096e5c2a2001fcad86140db8689ab757
Chao-Chin Yang <ccyang@ua.edu>
2026-06-03 21:49:43 -0500

    Implement the integral over X1 and X2.

--- src/outputs/int2d.cpp
+++ src/outputs/int2d.cpp
@@ -14,6 +14,7 @@
 #include <vector>
 
 // Athena++ headers
+#include "../coordinates/coordinates.hpp"  // VolCenterFace3Area
 #include "../mesh/mesh.hpp"  // time, my_blocks
 #include "outputs.hpp"
 
@@ -225,17 +226,35 @@ void IntX1X2Output::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag) {
   fout.write(reinterpret_cast<const char*>(&pm->time), sizeof(pm->time));
 
   // Allocate arrays for 2D integrals.
+  AthenaArray<Real> area(pm->my_blocks(0)->ncells3);
   AthenaArray<Real> integral(num_vars_, nx);
   integral.ZeroClear();
 
   // Loop over meshblocks.
-  for (int i = 0; i < pm->nblocal; ++i) {
-    MeshBlock *pmb = pm->my_blocks(i);
+  for (int b = 0; b < pm->nblocal; ++b) {
+    MeshBlock *pmb = pm->my_blocks(b);
     LoadOutputData(pmb);
 
-    // Loop over data in each meshblock.
+    // Determine where the meshblock fits.
+    const int offset = pmb->loc.lx3 * pmb->block_size.nx3 - pmb->ks;
+
+    // Integrate each data field.
+    int ii = 0;
     OutputData *pdata = pfirst_data_;
     while (pdata != nullptr) {
+      const int nc = (pdata->type == "VECTORS") ? 3 : 1;
+      for (int c = 0; c < nc; ++c) {
+        for (int k = pmb->ks; k <= pmb->ke; ++k) {
+          Real sum = 0;
+          for (int j = pmb->js; j <= pmb->je; ++j) {
+            pmb->pcoord->VolCenterFace3Area(k, j, pmb->is, pmb->ie, area);
+            for (int i = pmb->is; i <= pmb->ie; ++i)
+              sum += pdata->data(c,k,j,i) * area(i);
+          }
+          integral(ii, k + offset) += sum;
+        }
+        ++ii;
+      }
       pdata = pdata->pnext;
     }
 
@@ -248,6 +267,7 @@ void IntX1X2Output::WriteOutputFile(Mesh *pm, ParameterInput *pin, bool flag) {
   // Close and clean up.
   fout.close();
   integral.DeleteAthenaArray();
+  area.DeleteAthenaArray();
 
   // Update output parameters.
   output_params.next_time += output_params.dt;
